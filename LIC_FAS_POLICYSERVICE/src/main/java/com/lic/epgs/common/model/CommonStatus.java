a8f9dbbc95b74d48c4fcd7aa1fcaf65cec26d78c
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-04 18:38:24 +0530

    initial commit

---
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/common/model/CommonStatus.java
@@ -0,0 +1,50 @@
+// Entity/Model Class
+package com.lic.epgs.common.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class CommonStatus {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String code;
+    private String type;
+    private boolean isActive;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+}

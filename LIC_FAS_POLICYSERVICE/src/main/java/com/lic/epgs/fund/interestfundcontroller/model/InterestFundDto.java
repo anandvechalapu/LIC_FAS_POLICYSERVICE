25d9c66ba731bae6d272fee3bb9e05a76bdebb7a
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-04 19:06:00 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/fund/interestfundcontroller/model/InterestFundDto.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/fund/interestfundcontroller/model/InterestFundDto.java
@@ -1,78 +1,46 @@
 package com.lic.epgs.fund.interestfundcontroller.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
-@Entity
-@Table(name = "InterestFund")
 public class InterestFundDto {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    private String policyNumber;
-    private Integer creditAmount;
-    private Integer creditTerm;
-    private Double interestRate;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getPolicyNumber() {
-        return policyNumber;
-    }
-
-    public void setPolicyNumber(String policyNumber) {
-        this.policyNumber = policyNumber;
-    }
-
-    public Integer getCreditAmount() {
-        return creditAmount;
-    }
-
-    public void setCreditAmount(Integer creditAmount) {
-        this.creditAmount = creditAmount;
-    }
-
-    public Integer getCreditTerm() {
-        return creditTerm;
-    }
-
-    public void setCreditTerm(Integer creditTerm) {
-        this.creditTerm = creditTerm;
-    }
-
-    public Double getInterestRate() {
-        return interestRate;
-    }
-
-    public void setInterestRate(Double interestRate) {
-        this.interestRate = interestRate;
-    }
-
+  private String bankName;
+  private Integer amount;
+  private Double interestRate;
+  
+  public String getBankName() {
+    return bankName;
+  }
+  
+  public void setBankName(String bankName) {
+    this.bankName = bankName;
+  }
+  
+  public Integer getAmount() {
+    return amount;
+  }
+  
+  public void setAmount(Integer amount) {
+    this.amount = amount;
+  }
+  
+  public Double getInterestRate() {
+    return interestRate;
+  }
+  
+  public void setInterestRate(Double interestRate) {
+    this.interestRate = interestRate;
+  }
 }
 
-package com.lic.epgs.fund.interestfundcontroller.model;
-
 public class InterestRateResponseDto {
-    
-    private Double interestRate;
 
-    public Double getInterestRate() {
-        return interestRate;
-    }
+  private String status;
 
-    public void setInterestRate(Double interestRate) {
-        this.interestRate = interestRate;
-    }
+  public String getStatus() {
+    return status;
+  }
+
+  public void setStatus(String status) {
+    this.status = status;
+  }
 
 }

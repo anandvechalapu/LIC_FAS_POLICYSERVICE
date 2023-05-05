34f97db1b49a3d41b65d3aa02b756cca0ae19205
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-05 11:55:22 +0530

    initial commit

---
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/integration/controller/.java
@@ -0,0 +1,153 @@
+String getMphAddress1() {
+        return mphAddress1;
+    }
+
+    public void setMphAddress1(String mphAddress1) {
+        this.mphAddress1 = mphAddress1;
+    }
+
+    public String getMphAddress2() {
+        return mphAddress2;
+    }
+
+    public void setMphAddress2(String mphAddress2) {
+        this.mphAddress2 = mphAddress2;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public Long getPinCode() {
+        return pinCode;
+    }
+
+    public void setPinCode(Long pinCode) {
+        this.pinCode = pinCode;
+    }
+
+    public String getServicingUnitName() {
+        return servicingUnitName;
+    }
+
+    public void setServicingUnitName(String servicingUnitName) {
+        this.servicingUnitName = servicingUnitName;
+    }
+
+    public String getServicingUnitAddress1() {
+        return servicingUnitAddress1;
+    }
+
+    public void setServicingUnitAddress1(String servicingUnitAddress1) {
+        this.servicingUnitAddress1 = servicingUnitAddress1;
+    }
+
+    public String getServicingUnitAddress2() {
+        return servicingUnitAddress2;
+    }
+
+    public void setServicingUnitAddress2(String servicingUnitAddress2) {
+        this.servicingUnitAddress2 = servicingUnitAddress2;
+    }
+
+    public String getServicingUnitAddress3() {
+        return servicingUnitAddress3;
+    }
+
+    public void setServicingUnitAddress3(String servicingUnitAddress3) {
+        this.servicingUnitAddress3 = servicingUnitAddress3;
+    }
+
+    public String getServicingUnitAddress4() {
+        return servicingUnitAddress4;
+    }
+
+    public void setServicingUnitAddress4(String servicingUnitAddress4) {
+        this.servicingUnitAddress4 = servicingUnitAddress4;
+    }
+
+    public String getServicingUnitCity() {
+        return servicingUnitCity;
+    }
+
+    public void setServicingUnitCity(String servicingUnitCity) {
+        this.servicingUnitCity = servicingUnitCity;
+    }
+
+    public Long getServicingUnitPincode() {
+        return servicingUnitPincode;
+    }
+
+    public void setServicingUnitPincode(Long servicingUnitPincode) {
+        this.servicingUnitPincode = servicingUnitPincode;
+    }
+
+    public String getServicingUnitEmail() {
+        return servicingUnitEmail;
+    }
+
+    public void setServicingUnitEmail(String servicingUnitEmail) {
+        this.servicingUnitEmail = servicingUnitEmail;
+    }
+
+    public Long getServicingUnitPhoneNo() {
+        return servicingUnitPhoneNo;
+    }
+
+    public void setServicingUnitPhoneNo(Long servicingUnitPhoneNo) {
+        this.servicingUnitPhoneNo = servicingUnitPhoneNo;
+    }
+
+    public String getOperatingUnitType() {
+        return operatingUnitType;
+    }
+
+    public void setOperatingUnitType(String operatingUnitType) {
+        this.operatingUnitType = operatingUnitType;
+    }
+
+    public String getUnitCode() {
+        return unitCode;
+    }
+
+    public void setUnitCode(String unitCode) {
+        this.unitCode = unitCode;
+    }
+
+    public String getICodeForLob() {
+        return ICodeForLob;
+    }
+
+    public void setICodeForLob(String iCodeForLob) {
+        ICodeForLob = iCodeForLob;
+    }
+
+    public String getICodeForProductLine() {
+        return ICodeForProductLine;
+    }
+
+    public void setICodeForProductLine(String iCodeForProductLine) {
+        ICodeForProductLine = iCodeForProductLine;
+    }
+
+    public String getICodeForVariant() {
+        return ICodeForVariant;
+    }
+
+    public void setICodeForVariant(String iCodeForVariant) {
+        ICodeForVariant = iCodeForVariant;
+    }
+
+    public String

0322fd85afa4b7e57955c01fbff532ee49320546
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-05 13:22:55 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policyservicingcontroller/repository/SavePolicyServiceRepository.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policyservicingcontroller/repository/SavePolicyServiceRepository.java
@@ -1,10 +1,15 @@
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.lic.epgs.policyservicing.common.policyservicingcontroller.model.PolicyServiceModuleDto;
-import com.lic.epgs.policyservicing.common.policyservicingcontroller.model.CommonResponseDto;
-import com.lic.epgs.policyservicing.common.policyservicingcontroller.model.PolicyServiceDto;
-
 @Repository
-public interface SavePolicyServiceRepository extends JpaRepository<PolicyServiceModuleDto, Long> {
-    CommonResponseDto<PolicyServiceDto> savePolicyService(PolicyServiceModuleDto policyServiceModuleDto);
+public interface SavePolicyServiceRepository {
+  
+  // Method to save policy service
+  CommonResponseDto<PolicyServiceDto> savePolicyService(PolicyServiceModuleDto policyServiceModuleDto) throws ApplicationException; 
+  
+  // Method to save temp conversion
+  CommonResponseDto<PolicyServiceDto> saveTempConversion() throws ApplicationException;
+  
+  // Method to save temp policy module
+  CommonResponseDto<PolicyServiceDto> saveTempPolicyModule() throws ApplicationException;
+  
+  // Method to log error
+  void logError(String errorMessage);
 }

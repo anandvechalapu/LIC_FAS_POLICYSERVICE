8c553a21cfceadd61c798a3a195f98ded4fd87ef
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-05 17:26:27 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/memberlvl/service/CommissionService.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/memberlvl/service/CommissionService.java
@@ -1,25 +1,49 @@
+//Spring Boot Service Class
+
 package com.lic.epgs.policyservicing.common.memberlvl.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.lic.epgs.policyservicing.common.memberlvl.entity.CommissionDetailsEntity;
 import com.lic.epgs.policyservicing.common.memberlvl.entity.CommissionEntity;
 import com.lic.epgs.policyservicing.common.memberlvl.repository.CommissionRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class CommissionService {
 
     @Autowired
-    private CommissionRepository commissionRepository;
+    CommissionRepository commissionRepository;
 
-    public List<CommissionEntity> getInprogressLoad(String unitCode) {
-        return commissionRepository.getInprogressLoad(unitCode);
+    public List<CommissionEntity> getInprogressLoad(String role, String unitCode) {
+        return commissionRepository.getInprogressLoad(role, unitCode);
     }
 
-    public List<CommissionEntity> getInprogressMakerLoad(String unitCode) {
-        return commissionRepository.getInprogressMakerLoad(unitCode);
+    public List<CommissionEntity> getInprogressMakerLoad(String role, String unitCode) {
+        return commissionRepository.getInprogressMakerLoad(role, unitCode);
+    }
+
+    public Optional<CommissionEntity> findById(Long id) {
+        return commissionRepository.findById(id);
+    }
+
+    public CommissionEntity save(CommissionEntity commissionEntity) {
+        return commissionRepository.save(commissionEntity);
+    }
+
+    public void delete(CommissionEntity commissionEntity) {
+        commissionRepository.delete(commissionEntity);
+    }
+
+    public CommissionDetailsEntity saveCommissionDetails(CommissionDetailsEntity commissionDetailsEntity) {
+        return commissionRepository.saveCommissionDetails(commissionDetailsEntity);
+    }
+
+    public void deleteCommissionDetails(CommissionDetailsEntity commissionDetailsEntity) {
+        commissionRepository.deleteCommissionDetails(commissionDetailsEntity);
     }
 
 }

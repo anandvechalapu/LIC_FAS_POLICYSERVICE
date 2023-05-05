d5519b09b9b018ca2a2403b22b61e09cfd17704f
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-05 12:34:15 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/commoncontroller/service/PolicyServicingCommonService.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/commoncontroller/service/PolicyServicingCommonService.java
@@ -1,19 +1,24 @@
 package com.lic.epgs.policyservicing.common.commoncontroller.service;
 
+import com.lic.epgs.policyservicing.common.commoncontroller.dto.PolicyServiceCommonResponseDto;
+import com.lic.epgs.policyservicing.common.commoncontroller.dto.PolicyServiceDto;
+import com.lic.epgs.policyservicing.common.commoncontroller.entity.PolicyServiceEntity;
 import com.lic.epgs.policyservicing.common.commoncontroller.repository.PolicyServicingCommonRepository;
-import com.lic.epgs.policyservicing.common.commoncontroller.model.ServiceDetails;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
 @Service
 public class PolicyServicingCommonService {
- 
-    @Autowired
-    private PolicyServicingCommonRepository policyServicingCommonRepository;
- 
-    public Optional<ServiceDetails> getServiceDetailsById(Long serviceId) {
-        return policyServicingCommonRepository.findById(serviceId);
-    }
+
+  @Autowired
+  private PolicyServicingCommonRepository policyServicingCommonRepository;
+
+  public PolicyServiceEntity findByPolicyId(String policyId) {
+    return policyServicingCommonRepository.findByPolicyId(policyId);
+  }
+
+  public PolicyServiceCommonResponseDto startService(PolicyServiceDto policyServiceDto, String serviceType) {
+    return policyServicingCommonRepository.startService(policyServiceDto, serviceType);
+  }
+
 }

cba3cb571958a70939bbed1831644e6b990c5751
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-05 11:46:10 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/fund/policyinterestratefundcontroller/service/PolicyInterestRateFundControllerService.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/fund/policyinterestratefundcontroller/service/PolicyInterestRateFundControllerService.java
@@ -2,17 +2,19 @@ package com.lic.epgs.fund.policyinterestratefundcontroller.service;
 
 import com.lic.epgs.fund.policyinterestratefundcontroller.model.InterestRateResponseDto;
 import com.lic.epgs.fund.policyinterestratefundcontroller.repository.PolicyInterestRateFundControllerRepository;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PolicyInterestRateFundControllerService {
 
-    @Autowired
-    private PolicyInterestRateFundControllerRepository policyInterestRateFundControllerRepository;
+    private final PolicyInterestRateFundControllerRepository policyInterestRateFundControllerRepository;
 
-    public InterestRateResponseDto creditPolicy(String policyNo) throws ApplicationException {
-        return policyInterestRateFundControllerRepository.creditPolicy(policyNo);
+    public PolicyInterestRateFundControllerService(PolicyInterestRateFundControllerRepository policyInterestRateFundControllerRepository) {
+        this.policyInterestRateFundControllerRepository = policyInterestRateFundControllerRepository;
     }
+
+    public InterestRateResponseDto getInterestRateByPolicyNumber(String policyNo) {
+        return policyInterestRateFundControllerRepository.debitMembersByPolicy(policyNo);
+    }
+
 }

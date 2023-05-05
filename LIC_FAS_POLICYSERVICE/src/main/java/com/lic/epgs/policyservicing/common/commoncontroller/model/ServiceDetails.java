8deaf2a915148a7c39158cb8a0462c6fbfa1c930
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-05 12:25:44 +0530

    initial commit

---
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/commoncontroller/model/ServiceDetails.java
@@ -0,0 +1,65 @@
+package com.lic.epgs.policyservicing.common.commoncontroller.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class ServiceDetails {
+
+    @Id
+    private Long serviceId;
+    private String serviceName;
+    private String serviceDescription;
+    private String serviceType;
+    private String serviceStatus;
+    private Long serviceFee;
+
+    public Long getServiceId() {
+        return serviceId;
+    }
+
+    public void setServiceId(Long serviceId) {
+        this.serviceId = serviceId;
+    }
+
+    public String getServiceName() {
+        return serviceName;
+    }
+
+    public void setServiceName(String serviceName) {
+        this.serviceName = serviceName;
+    }
+
+    public String getServiceDescription() {
+        return serviceDescription;
+    }
+
+    public void setServiceDescription(String serviceDescription) {
+        this.serviceDescription = serviceDescription;
+    }
+
+    public String getServiceType() {
+        return serviceType;
+    }
+
+    public void setServiceType(String serviceType) {
+        this.serviceType = serviceType;
+    }
+
+    public String getServiceStatus() {
+        return serviceStatus;
+    }
+
+    public void setServiceStatus(String serviceStatus) {
+        this.serviceStatus = serviceStatus;
+    }
+
+    public Long getServiceFee() {
+        return serviceFee;
+    }
+
+    public void setServiceFee(Long serviceFee) {
+        this.serviceFee = serviceFee;
+    }
+
+}

4240c94325a54d8d23751b389ca7ffed03716da6
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-05 17:21:51 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/memberlvl/controller/CommissionController.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/memberlvl/controller/CommissionController.java
@@ -1,57 +1,45 @@
 package com.lic.epgs.policyservicing.common.memberlvl.controller;
 
-import java.util.List;
-
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.lic.epgs.policyservicing.common.memberlvl.model.CommissionModel;
+import com.lic.epgs.policyservicing.common.memberlvl.entity.CommissionEntity;
+import com.lic.epgs.policyservicing.common.memberlvl.entity.CommissionDetailsEntity;
 import com.lic.epgs.policyservicing.common.memberlvl.service.CommissionService;
 
 @RestController
-@RequestMapping("/commission")
 public class CommissionController {
-
-    @Autowired
-    private CommissionService commissionService;
-    
-    @GetMapping("/{unitCode}/{role}")
-    public CommissionModel getCommissionByUnitCodeAndRole(@PathVariable String unitCode, @PathVariable String role) {
-        return commissionService.findByUnitCodeAndRole(unitCode, role);
-    }
-    
-    @GetMapping("/{commissionId}")
-    public CommissionModel getCommissionDetailsByCommissionId(@PathVariable Long commissionId) {
-        return commissionService.findByCommissionId(commissionId);
-    }
-    
-    @PostMapping
-    public CommissionModel createCommission(@Valid @RequestBody CommissionModel commissionModel) {
-              return commissionService.save(commissionModel);
-    }
-    
-    @PutMapping
-    public CommissionModel updateCommissionDetails(@Valid @RequestBody CommissionModel commissionModel) {
-              return commissionService.save(commissionModel);
-    }
-    
-    @DeleteMapping("/{commissionId}")
-    public void deleteCommission(@PathVariable Long commissionId) {
-              commissionService.deleteByCommissionId(commissionId);
-    }
-    
-    @GetMapping
-    public List<CommissionModel> getAllCommissions() {
-        return commissionService.findAllCommissions();
-    }
-
+	
+	@Autowired
+	private CommissionService commissionService;
+	
+	@GetMapping("/commission/inprogress")
+	public CommissionEntity getInprogressLoad(@RequestParam String role, @RequestParam String unitCode) {
+		return commissionService.getInprogressLoad(role, unitCode);
+	}
+	
+	@GetMapping("/commission/inprogressmaker")
+	public CommissionDetailsEntity getInprogressMakerLoad(@RequestParam String role, @RequestParam String unitCode) {
+		return commissionService.getInprogressMakerLoad(role, unitCode);
+	}
+	
+	@GetMapping("/commissions")
+	public List<CommissionEntity> getAllCommissions() {
+		return commissionService.getAllCommissions();
+	}
+	
+	@PostMapping("/commission")
+	public CommissionEntity saveCommission(@RequestBody CommissionEntity commission) {
+		return commissionService.saveCommission(commission);
+	}
+	
+	@PostMapping("/commissiondetails")
+	public CommissionDetailsEntity saveCommissionDetails(@RequestBody CommissionDetailsEntity commissionDetails) {
+		return commissionService.saveCommissionDetails(commissionDetails);
+	}
+	
 }

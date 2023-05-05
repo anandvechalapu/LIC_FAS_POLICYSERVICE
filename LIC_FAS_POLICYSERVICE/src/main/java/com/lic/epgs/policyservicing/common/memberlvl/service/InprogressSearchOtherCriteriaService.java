1ae2886090ddc34bf2bfb4db4461c61fa2c25808
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-05 16:47:45 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/memberlvl/service/InprogressSearchOtherCriteriaService.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/memberlvl/service/InprogressSearchOtherCriteriaService.java
@@ -1,7 +1,7 @@
 package com.lic.epgs.policyservicing.common.memberlvl.service;
 
-import com.lic.epgs.policyservicing.common.memberlvl.dto.CommissionSearchDto;
 import com.lic.epgs.policyservicing.common.memberlvl.dto.CommonCommissionDto;
+import com.lic.epgs.policyservicing.common.memberlvl.dto.CommissionSearchDto;
 import com.lic.epgs.policyservicing.common.memberlvl.repository.InprogressSearchOtherCriteriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,8 +13,7 @@ public class InprogressSearchOtherCriteriaService {
     @Autowired
     private InprogressSearchOtherCriteriaRepository inprogressSearchOtherCriteriaRepository;
 
-    public ResponseEntity<CommonCommissionDto> findBySearchParamsAndRole(String role, CommissionSearchDto searchParams) {
-        return inprogressSearchOtherCriteriaRepository.findBySearchParamsAndRole(role, searchParams);
+    public ResponseEntity<CommonCommissionDto> inprogressSearchOtherCriteria(CommissionSearchDto commissionSearchDto, String role){
+        return inprogressSearchOtherCriteriaRepository.inprogressSearchOtherCriteria(commissionSearchDto, role);
     }
-    
 }

3e9f8b41e203f4e3bc48c0c3c32a978bdbeb5730
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-05 18:22:56 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/memberlvl/policylevelconversioncheckercontroller/service/PolicyLevelConversionCheckerControllerService.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/memberlvl/policylevelconversioncheckercontroller/service/PolicyLevelConversionCheckerControllerService.java
@@ -1,44 +1,56 @@
 package com.lic.epgs.policyservicing.memberlvl.policylevelconversioncheckercontroller.service;
 
-import com.lic.epgs.policyservicing.memberlvl.policylevelconversioncheckercontroller.entity.PolicyLevelConversionEntity;
-import com.lic.epgs.policyservicing.memberlvl.policylevelconversioncheckercontroller.entity.PolicyLevelConversionTempEntity;
+import com.lic.epgs.policyservicing.memberlvl.policylevelconversioncheckercontroller.model.PolicyConversion;
 import com.lic.epgs.policyservicing.memberlvl.policylevelconversioncheckercontroller.repository.PolicyLevelConversionCheckerControllerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
+import java.util.List;
 
 @Service
 public class PolicyLevelConversionCheckerControllerService {
-
+    
     @Autowired
     private PolicyLevelConversionCheckerControllerRepository policyLevelConversionCheckerControllerRepository;
 
-    public void updateConversionStatus(Long conversionId) {
-        policyLevelConversionCheckerControllerRepository.updateConversionStatus(conversionId);
+    // Method to perform existing common search using various search parameters
+    public List<PolicyConversion> existingCommonSearch(String mphCode, String mphName, String status, String product, String unitCode, int pageCount, int limit){
+        return policyLevelConversionCheckerControllerRepository.existingCommonSearch(mphCode, mphName, status, product, unitCode, pageCount, limit);
     }
 
-    public void updateConversionWorkflowStatus(Long conversionId, String modifiedBy) {
-        policyLevelConversionCheckerControllerRepository.updateConversionWorkflowStatus(conversionId, modifiedBy);
+    // Method to set transaction status
+    public void setStatus(String status){
+        policyLevelConversionCheckerControllerRepository.setStatus(status);
     }
 
-    public PolicyLevelConversionEntity checkIfExists(Long prevPolicyId) {
-        return policyLevelConversionCheckerControllerRepository.checkIfExists(prevPolicyId);
+    // Method to set transaction message
+    public void setMessage(String message){
+        policyLevelConversionCheckerControllerRepository.setMessage(message);
     }
 
-    @Transactional
-    public void saveNewPolicyLevelConversionEntity(Long conversionId) {
-        policyLevelConversionCheckerControllerRepository.saveNewPolicyLevelConversionEntity(conversionId);
+    // Method to set default limit of 5000 records
+    public void setDefaultLimit(int limit){
+        policyLevelConversionCheckerControllerRepository.setDefaultLimit(limit);
     }
 
-    @Transactional
-    public void saveNewPolicyServiceDocumentEntity(Long conversionId) {
-        policyLevelConversionCheckerControllerRepository.saveNewPolicyServiceDocumentEntity(conversionId);
+    // Method to set default page count of 0
+    public void setDefaultPageCount(int pageCount){
+        policyLevelConversionCheckerControllerRepository.setDefaultPageCount(pageCount);
     }
 
-    @Transactional
-    public void saveNewPolicyServiceNotesEntity(Long serviceId) {
-        policyLevelConversionCheckerControllerRepository.saveNewPolicyServiceNotesEntity(serviceId);
+    // Method to sort the results in descending order by the createdOn field
+    public void sortByCreatedOn(){
+        policyLevelConversionCheckerControllerRepository.sortByCreatedOn();
+    }
+
+    // Method to ignore blank search parameters
+    public void ignoreBlankSearchParameters(){
+        policyLevelConversionCheckerControllerRepository.ignoreBlankSearchParameters();
+    }
+
+    // Method to return error transaction status and message
+    public void returnErrorTransactionStatusAndMessage(){
+        policyLevelConversionCheckerControllerRepository.returnErrorTransactionStatusAndMessage();
     }
 
 }

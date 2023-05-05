27c1a4715da4ad12beb4a25b783b1b3ef0a4e304
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-05 16:51:47 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/memberlvl/controller/InprogressSearchOtherCriteriaController.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/memberlvl/controller/InprogressSearchOtherCriteriaController.java
@@ -1,22 +1,23 @@
 package com.lic.epgs.policyservicing.common.memberlvl.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.lic.epgs.policyservicing.common.memberlvl.dto.CommonCommissionDto;
 import com.lic.epgs.policyservicing.common.memberlvl.dto.CommissionSearchDto;
 import com.lic.epgs.policyservicing.common.memberlvl.service.InprogressSearchOtherCriteriaService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/inprogress/search/other")
 public class InprogressSearchOtherCriteriaController {
-
+    
     @Autowired
-    private InprogressSearchOtherCriteriaService inprogressSearchOtherCriteriaService;
-
-    @PostMapping("/criteria")
-    public ResponseEntity<CommonCommissionDto> inprogressSearchOtherCriteria(@RequestBody CommissionSearchDto commissionSearchDto, @RequestParam("role") String role){
-        return inprogressSearchOtherCriteriaService.inprogressSearchOtherCriteria(commissionSearchDto, role);
+    private InprogressSearchOtherCriteriaService service;
+    
+    @PostMapping("/inprogressSearchOtherCriteria")
+    public CommonCommissionDto inprogressSearchOtherCriteria(@RequestBody CommissionSearchDto searchCriteria) {
+        return service.inprogressSearchOtherCriteria(searchCriteria.getRole(), searchCriteria);
     }
-
+    
 }

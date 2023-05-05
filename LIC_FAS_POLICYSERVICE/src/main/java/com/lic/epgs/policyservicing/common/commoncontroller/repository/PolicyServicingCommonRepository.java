05381d6aee5540c7c2d135812df3ab5bb08a38cb
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-05 12:33:40 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/commoncontroller/repository/PolicyServicingCommonRepository.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/commoncontroller/repository/PolicyServicingCommonRepository.java
@@ -1,6 +1,15 @@
+package com.lic.epgs.policyservicing.common.commoncontroller.repository;
+
+import com.lic.epgs.policyservicing.common.commoncontroller.dto.PolicyServiceCommonResponseDto;
+import com.lic.epgs.policyservicing.common.commoncontroller.dto.PolicyServiceDto;
+import com.lic.epgs.policyservicing.common.commoncontroller.entity.PolicyServiceEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 @Repository
-public interface PolicyServicingCommonRepository extends JpaRepository<ServiceDetails, Long> {
+public interface PolicyServicingCommonRepository extends JpaRepository<PolicyServiceEntity, Long> {
 
-    ServiceDetails findById(Long serviceId);
+  PolicyServiceEntity findByPolicyId(String policyId);
 
+  PolicyServiceCommonResponseDto startService(PolicyServiceDto policyServiceDto, String serviceType);
 }

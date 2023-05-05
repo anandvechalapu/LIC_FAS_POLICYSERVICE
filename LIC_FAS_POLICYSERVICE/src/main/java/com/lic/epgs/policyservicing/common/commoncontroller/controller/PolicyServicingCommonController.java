f77b8d5e619a75c84a6898858856a5115dca1c00
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-05 12:28:32 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/commoncontroller/controller/PolicyServicingCommonController.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/commoncontroller/controller/PolicyServicingCommonController.java
@@ -1,38 +1,36 @@
 package com.lic.epgs.policyservicing.common.commoncontroller.repository;
 
-import com.lic.epgs.policyservicing.common.commoncontroller.dto.PolicyServiceCommonResponseDto;
+import com.lic.epgs.policyservicing.common.commoncontroller.model.ServiceDetails;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PolicyServicingCommonRepository extends JpaRepository<PolicyServiceCommonResponseDto, Long> {
-
-    @Query("SELECT p FROM PolicyServiceCommonResponseDto p WHERE p.policyId = ?1")
-    PolicyServiceCommonResponseDto getServiceDetailsByPolicyId(Long policyId);
-
+public interface PolicyServicingCommonRepository extends JpaRepository<ServiceDetails, Long> {
 }
 
 package com.lic.epgs.policyservicing.common.commoncontroller.controller;
 
-import com.lic.epgs.policyservicing.common.commoncontroller.dto.PolicyServiceCommonResponseDto;
+import com.lic.epgs.policyservicing.common.commoncontroller.model.ServiceDetails;
 import com.lic.epgs.policyservicing.common.commoncontroller.service.PolicyServicingCommonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/policyServicing")
 public class PolicyServicingCommonController {
 
     @Autowired
     private PolicyServicingCommonService policyServicingCommonService;
 
-    @GetMapping("/{policyId}")
-    public PolicyServiceCommonResponseDto getServiceDetailsByPolicyId(@PathVariable Long policyId) {
-        return policyServicingCommonService.getServiceDetailsByPolicyId(policyId);
+    @GetMapping("/services/{serviceId}")
+    public ResponseEntity<ServiceDetails> getServiceDetailsById(@PathVariable("serviceId") Long serviceId) {
+        ServiceDetails serviceDetails = policyServicingCommonService.getServiceDetailsById(serviceId).orElse(null);
+        if (serviceDetails == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(serviceDetails, HttpStatus.OK);
     }
-
 }

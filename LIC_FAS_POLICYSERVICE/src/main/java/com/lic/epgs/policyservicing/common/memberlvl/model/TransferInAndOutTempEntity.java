08ec11e8cecb0813feb048998fa3efa76e218d59
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-05 17:43:08 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/memberlvl/model/TransferInAndOutTempEntity.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/memberlvl/model/TransferInAndOutTempEntity.java
@@ -2,19 +2,14 @@ package com.lic.epgs.policyservicing.common.memberlvl.model;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.Table;
 
 @Entity
-@Table(name = "transfer_in_and_out_temp")
 public class TransferInAndOutTempEntity {
-	
+
 	@Id
 	private Long transferId;
 	private String unitCode;
-	private String transferType;
-	private String transferDate;
 	private String transferStatus;
-	private String rejectReason;
 	
 	public Long getTransferId() {
 		return transferId;
@@ -28,29 +23,11 @@ public class TransferInAndOutTempEntity {
 	public void setUnitCode(String unitCode) {
 		this.unitCode = unitCode;
 	}
-	public String getTransferType() {
-		return transferType;
-	}
-	public void setTransferType(String transferType) {
-		this.transferType = transferType;
-	}
-	public String getTransferDate() {
-		return transferDate;
-	}
-	public void setTransferDate(String transferDate) {
-		this.transferDate = transferDate;
-	}
 	public String getTransferStatus() {
 		return transferStatus;
 	}
 	public void setTransferStatus(String transferStatus) {
 		this.transferStatus = transferStatus;
-	}
-	public String getRejectReason() {
-		return rejectReason;
-	}
-	public void setRejectReason(String rejectReason) {
-		this.rejectReason = rejectReason;
 	}
 	
 	

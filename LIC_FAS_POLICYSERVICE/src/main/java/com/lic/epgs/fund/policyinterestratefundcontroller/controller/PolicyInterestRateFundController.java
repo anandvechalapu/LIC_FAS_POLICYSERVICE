1213d01c301b61b664ff8d1bfbfe4fa4f65261e0
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-05 11:46:38 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/fund/policyinterestratefundcontroller/controller/PolicyInterestRateFundController.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/fund/policyinterestratefundcontroller/controller/PolicyInterestRateFundController.java
@@ -1,10 +1,23 @@
+package com.lic.epgs.fund.policyinterestratefundcontroller.repository;
+
+import com.lic.epgs.fund.policyinterestratefundcontroller.model.InterestRateResponseDto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PolicyInterestRateFundControllerRepository extends JpaRepository<InterestRateResponseDto, String> {
+
+    InterestRateResponseDto debitMembersByPolicy(String policyNo);
+}
+
 package com.lic.epgs.fund.policyinterestratefundcontroller.controller;
 
 import com.lic.epgs.fund.policyinterestratefundcontroller.model.InterestRateResponseDto;
 import com.lic.epgs.fund.policyinterestratefundcontroller.service.PolicyInterestRateFundControllerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,9 +26,10 @@ public class PolicyInterestRateFundController {
     @Autowired
     private PolicyInterestRateFundControllerService policyInterestRateFundControllerService;
 
-    @GetMapping("/policies/{policyNo}/interestrate")
-    public InterestRateResponseDto getInterestRateForPolicy(@PathVariable String policyNo) {
-        return policyInterestRateFundControllerService.creditPolicy(policyNo);
+    @GetMapping("/interestrate")
+    public ResponseEntity<InterestRateResponseDto> getInterestRateByPolicyNumber(@RequestParam("policyNo") String policyNo) {
+        InterestRateResponseDto interestRateResponseDto = policyInterestRateFundControllerService.getInterestRateByPolicyNumber(policyNo);
+        return ResponseEntity.ok(interestRateResponseDto);
     }
 
 }

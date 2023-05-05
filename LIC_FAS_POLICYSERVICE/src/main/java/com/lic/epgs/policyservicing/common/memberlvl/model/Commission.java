8af255f004aabf2ec2c3ff25aab16cae13fdc6c5
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-05 17:24:48 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/memberlvl/model/Commission.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/memberlvl/model/Commission.java
@@ -1,24 +1,30 @@
 package com.lic.epgs.policyservicing.common.memberlvl.model;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.util.List;
 
 @Entity
-@Table(name="commission")
+@Table(name = "commission")
 public class Commission {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
     private Long id;
+
+    @Column(name = "role")
+    private String role;
 
     @Column(name = "unit_code")
     private String unitCode;
 
-    @Column(name = "is_inprogress")
-    private Boolean isInprogress;
-
-    @OneToMany(mappedBy = "commission", cascade = CascadeType.ALL)
-    private CommissionDetails commissionDetails;
+    @OneToMany(mappedBy = "commission")
+    private List<CommissionDetails> commissionDetails;
 
     public Long getId() {
         return id;
@@ -26,6 +32,14 @@ public class Commission {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
     }
 
     public String getUnitCode() {
@@ -36,19 +50,11 @@ public class Commission {
         this.unitCode = unitCode;
     }
 
-    public Boolean getIsInprogress() {
-        return isInprogress;
-    }
-
-    public void setIsInprogress(Boolean isInprogress) {
-        this.isInprogress = isInprogress;
-    }
-
-    public CommissionDetails getCommissionDetails() {
+    public List<CommissionDetails> getCommissionDetails() {
         return commissionDetails;
     }
 
-    public void setCommissionDetails(CommissionDetails commissionDetails) {
+    public void setCommissionDetails(List<CommissionDetails> commissionDetails) {
         this.commissionDetails = commissionDetails;
     }
 }

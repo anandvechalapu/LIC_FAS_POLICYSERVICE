0c2ad0a68bdc83f37c2b67abbfa4e95e276977d2
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-05 12:17:25 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/commoncontroller/repository/PolicyServicingCommonRepository.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/commoncontroller/repository/PolicyServicingCommonRepository.java
@@ -1,7 +1,6 @@
 @Repository
-public interface PolicyServicingCommonRepository extends JpaRepository<PolicyServiceEntity, Long> {
-    Long getSequence();
-	
-	@Query("SELECT ps FROM PolicyServiceEntity ps WHERE ps.policyId = :policyId AND ps.serviceType = :serviceType")
-    PolicyServiceEntity findByPolicyIdAndServiceType(@Param("policyId") String policyId, @Param("serviceType") String serviceType); 
+public interface PolicyServicingCommonRepository {
+
+    PolicyServiceCommonResponseDto getServiceDetailsByPolicyId(Long policyId);
+
 }

3d428913b9bfa08ce7efc00cf72ada0030f65389
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-05 11:50:22 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/fund/policyinterestratefundcontroller/controller/PolicyInterestRateFundController.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/fund/policyinterestratefundcontroller/controller/PolicyInterestRateFundController.java
@@ -1,35 +1,45 @@
-package com.lic.epgs.fund.policyinterestratefundcontroller.repository;
-
-import com.lic.epgs.fund.policyinterestratefundcontroller.model.InterestRateResponseDto;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PolicyInterestRateFundControllerRepository extends JpaRepository<InterestRateResponseDto, String> {
-
-    InterestRateResponseDto debitMembersByPolicy(String policyNo);
-}
-
 package com.lic.epgs.fund.policyinterestratefundcontroller.controller;
 
-import com.lic.epgs.fund.policyinterestratefundcontroller.model.InterestRateResponseDto;
+import com.lic.epgs.fund.policyinterestratefundcontroller.dto.InterestRateResponseDto;
+import com.lic.epgs.fund.policyinterestratefundcontroller.model.PolicyInterestRateFundControllerModel;
 import com.lic.epgs.fund.policyinterestratefundcontroller.service.PolicyInterestRateFundControllerService;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1/fund")
 public class PolicyInterestRateFundController {
 
     @Autowired
     private PolicyInterestRateFundControllerService policyInterestRateFundControllerService;
 
-    @GetMapping("/interestrate")
-    public ResponseEntity<InterestRateResponseDto> getInterestRateByPolicyNumber(@RequestParam("policyNo") String policyNo) {
-        InterestRateResponseDto interestRateResponseDto = policyInterestRateFundControllerService.getInterestRateByPolicyNumber(policyNo);
-        return ResponseEntity.ok(interestRateResponseDto);
+    @PostMapping("/credit")
+    public InterestRateResponseDto creditPolicyMemberByPolicyNo(@RequestBody PolicyInterestRateFundControllerModel model) {
+        return policyInterestRateFundControllerService.creditPolicyMemberByPolicyNo(model.getPolicyNo());
+    }
+
+    @PostMapping("/credit-bulk")
+    public InterestRateResponseDto creditBulkPolicyMembersByPolicyNo(@RequestBody PolicyInterestRateFundControllerModel model) {
+        return policyInterestRateFundControllerService.creditBulkPolicyMembersByPolicyNo(model.getPolicyNo());
+    }
+
+    @PostMapping("/debit")
+    public InterestRateResponseDto debitPolicyMemberByPolicyNoAndMemberId(@RequestBody PolicyInterestRateFundControllerModel model) {
+        return policyInterestRateFundControllerService.debitPolicyMemberByPolicyNoAndMemberId(model.getPolicyNo(), model.getMemberId());
+    }
+
+    @PostMapping("/debit-bulk")
+    public InterestRateResponseDto debitPolicyMembersByPolicyNo(@RequestBody PolicyInterestRateFundControllerModel model) {
+        return policyInterestRateFundControllerService.debitPolicyMembersByPolicyNo(model.getPolicyNo());
+    }
+
+    @PostMapping("/debit-bulk-members")
+    public InterestRateResponseDto debitBulkPolicyMembersByPolicyNo(@RequestBody PolicyInterestRateFundControllerModel model) {
+        return policyInterestRateFundControllerService.debitBulkPolicyMembersByPolicyNo(model.getPolicyNo());
     }
 
 }

c674c17093d163a71b3780cc50b910de51329802
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-05 11:49:38 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/fund/policyinterestratefundcontroller/service/PolicyInterestRateFundControllerService.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/fund/policyinterestratefundcontroller/service/PolicyInterestRateFundControllerService.java
@@ -1,20 +1,35 @@
 package com.lic.epgs.fund.policyinterestratefundcontroller.service;
 
-import com.lic.epgs.fund.policyinterestratefundcontroller.model.InterestRateResponseDto;
+import com.lic.epgs.fund.policyinterestratefundcontroller.dto.InterestRateResponseDto;
 import com.lic.epgs.fund.policyinterestratefundcontroller.repository.PolicyInterestRateFundControllerRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PolicyInterestRateFundControllerService {
 
-    private final PolicyInterestRateFundControllerRepository policyInterestRateFundControllerRepository;
+    @Autowired
+    private PolicyInterestRateFundControllerRepository policyInterestRateFundControllerRepository;
 
-    public PolicyInterestRateFundControllerService(PolicyInterestRateFundControllerRepository policyInterestRateFundControllerRepository) {
-        this.policyInterestRateFundControllerRepository = policyInterestRateFundControllerRepository;
+    public InterestRateResponseDto creditPolicyMemberByPolicyNo(String policyNo) {
+        return policyInterestRateFundControllerRepository.creditPolicyMemberByPolicyNo(policyNo);
     }
 
-    public InterestRateResponseDto getInterestRateByPolicyNumber(String policyNo) {
-        return policyInterestRateFundControllerRepository.debitMembersByPolicy(policyNo);
+    public InterestRateResponseDto creditBulkPolicyMembersByPolicyNo(String policyNo) {
+        return policyInterestRateFundControllerRepository.creditBulkPolicyMembersByPolicyNo(policyNo);
+    }
+
+    public InterestRateResponseDto debitPolicyMemberByPolicyNoAndMemberId(String policyNo, String memberId) {
+        return policyInterestRateFundControllerRepository.debitPolicyMemberByPolicyNoAndMemberId(policyNo, memberId);
+    }
+
+    public InterestRateResponseDto debitPolicyMembersByPolicyNo(String policyNo) {
+        return policyInterestRateFundControllerRepository.debitPolicyMembersByPolicyNo(policyNo);
+    }
+
+    public InterestRateResponseDto debitBulkPolicyMembersByPolicyNo(String policyNo) {
+        return policyInterestRateFundControllerRepository.debitBulkPolicyMembersByPolicyNo(policyNo);
     }
 
 }

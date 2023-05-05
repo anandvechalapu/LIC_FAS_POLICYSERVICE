218feeb606435e6694a7ff4323b5855590a587e6
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-05 16:49:55 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/memberlvl/controller/InprogressSearchOtherCriteriaController.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/memberlvl/controller/InprogressSearchOtherCriteriaController.java
@@ -1,21 +1,22 @@
 package com.lic.epgs.policyservicing.common.memberlvl.controller;
 
-import com.lic.epgs.policyservicing.common.memberlvl.dto.CommissionSearchDto;
 import com.lic.epgs.policyservicing.common.memberlvl.dto.CommonCommissionDto;
+import com.lic.epgs.policyservicing.common.memberlvl.dto.CommissionSearchDto;
 import com.lic.epgs.policyservicing.common.memberlvl.service.InprogressSearchOtherCriteriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/common/memberlvl/inprogress")
+@RequestMapping("/inprogress/search/other")
 public class InprogressSearchOtherCriteriaController {
 
     @Autowired
     private InprogressSearchOtherCriteriaService inprogressSearchOtherCriteriaService;
 
-    @GetMapping
-    public ResponseEntity<CommonCommissionDto> findBySearchParamsAndRole(@RequestParam String role, @RequestBody CommissionSearchDto searchParams) {
-        return inprogressSearchOtherCriteriaService.findBySearchParamsAndRole(role, searchParams);
+    @PostMapping("/criteria")
+    public ResponseEntity<CommonCommissionDto> inprogressSearchOtherCriteria(@RequestBody CommissionSearchDto commissionSearchDto, @RequestParam("role") String role){
+        return inprogressSearchOtherCriteriaService.inprogressSearchOtherCriteria(commissionSearchDto, role);
     }
+
 }

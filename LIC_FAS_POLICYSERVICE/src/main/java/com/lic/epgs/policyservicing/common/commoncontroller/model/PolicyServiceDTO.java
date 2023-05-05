5b74fa6532fd768d82d88431e3436864e6089c22
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-05 12:04:41 +0530

    initial commit

---
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/commoncontroller/model/PolicyServiceDTO.java
@@ -0,0 +1,132 @@
+package com.lic.epgs.policyservicing.common.commoncontroller.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="policy_service_details")
+public class PolicyServiceDTO implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+	
+	@Column(name = "policy_id")
+	private String policyId;
+	
+	@Column(name = "service_id")
+	private String serviceId;
+	
+	@Column(name = "service_type")
+	private String serviceType;
+	
+	@Column(name = "service_status")
+	private boolean serviceStatus;
+	
+	@Column(name = "workflow_status")
+	private String workflowStatus;
+	
+	@Column(name = "created_by")
+	private String createdBy;
+	
+	@Column(name = "created_on")
+	private String createdOn;
+	
+	@Column(name = "modified_by")
+	private String modifiedBy;
+	
+	@Column(name = "modified_on")
+	private String modifiedOn;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getPolicyId() {
+		return policyId;
+	}
+
+	public void setPolicyId(String policyId) {
+		this.policyId = policyId;
+	}
+
+	public String getServiceId() {
+		return serviceId;
+	}
+
+	public void setServiceId(String serviceId) {
+		this.serviceId = serviceId;
+	}
+
+	public String getServiceType() {
+		return serviceType;
+	}
+
+	public void setServiceType(String serviceType) {
+		this.serviceType = serviceType;
+	}
+
+	public boolean isServiceStatus() {
+		return serviceStatus;
+	}
+
+	public void setServiceStatus(boolean serviceStatus) {
+		this.serviceStatus = serviceStatus;
+	}
+
+	public String getWorkflowStatus() {
+		return workflowStatus;
+	}
+
+	public void setWorkflowStatus(String workflowStatus) {
+		this.workflowStatus = workflowStatus;
+	}
+
+	public String getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public String getCreatedOn() {
+		return createdOn;
+	}
+
+	public void setCreatedOn(String createdOn) {
+		this.createdOn = createdOn;
+	}
+
+	public String getModifiedBy() {
+		return modifiedBy;
+	}
+
+	public void setModifiedBy(String modifiedBy) {
+		this.modifiedBy = modifiedBy;
+	}
+
+	public String getModifiedOn() {
+		return modifiedOn;
+	}
+
+	public void setModifiedOn(String modifiedOn) {
+		this.modifiedOn = modifiedOn;
+	}
+	
+	
+
+}

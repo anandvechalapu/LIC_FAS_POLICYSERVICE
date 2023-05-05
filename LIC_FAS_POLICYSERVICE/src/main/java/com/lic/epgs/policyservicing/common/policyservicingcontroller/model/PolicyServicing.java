99f904492114ac156d928cc1e8a0002cf0e70d60
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-05 12:41:06 +0530

    initial commit

---
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policyservicingcontroller/model/PolicyServicing.java
@@ -0,0 +1,59 @@
+package com.lic.epgs.policyservicing.common.policyservicingcontroller.model;
+
+public class PolicyServicing {
+
+    private Long id;
+    private String policyId;
+    private String servicingType;
+    private String status;
+    private String createdBy;
+    private String updatedBy;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPolicyId() {
+        return policyId;
+    }
+
+    public void setPolicyId(String policyId) {
+        this.policyId = policyId;
+    }
+
+    public String getServicingType() {
+        return servicingType;
+    }
+
+    public void setServicingType(String servicingType) {
+        this.servicingType = servicingType;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public String getUpdatedBy() {
+        return updatedBy;
+    }
+
+    public void setUpdatedBy(String updatedBy) {
+        this.updatedBy = updatedBy;
+    }
+}

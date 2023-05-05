71de302cd0e9e5228171de05841d887b83fc5d84
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-05 10:38:55 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/fund/interestratelogcontroller/model/InterestRateLog.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/fund/interestratelogcontroller/model/InterestRateLog.java
@@ -4,66 +4,50 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "interest_rate_logs")
 public class InterestRateLog {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
-    private String policyNo;
-    private String referenceNo;
-    private Double interestRate;
-    private Boolean isFail;
-    private String description;
- 
+
+    private Long memberId;
+
+    private String logData;
+
+    public InterestRateLog() {
+    }
+
+    public InterestRateLog(Long memberId, String logData) {
+        this.memberId = memberId;
+        this.logData = logData;
+    }
+
     public Long getId() {
         return id;
     }
-    
+
     public void setId(Long id) {
         this.id = id;
     }
-    
-    public String getPolicyNo() {
-        return policyNo;
+
+    public Long getMemberId() {
+        return memberId;
     }
-    
-    public void setPolicyNo(String policyNo) {
-        this.policyNo = policyNo;
+
+    public void setMemberId(Long memberId) {
+        this.memberId = memberId;
     }
-    
-    public String getReferenceNo() {
-        return referenceNo;
+
+    public String getLogData() {
+        return logData;
     }
-    
-    public void setReferenceNo(String referenceNo) {
-        this.referenceNo = referenceNo;
+
+    public void setLogData(String logData) {
+        this.logData = logData;
     }
-    
-    public Double getInterestRate() {
-        return interestRate;
-    }
-    
-    public void setInterestRate(Double interestRate) {
-        this.interestRate = interestRate;
-    }
-    
-    public Boolean getIsFail() {
-        return isFail;
-    }
-    
-    public void setIsFail(Boolean isFail) {
-        this.isFail = isFail;
-    }
-    
-    public String getDescription() {
-        return description;
-    }
-    
-    public void setDescription(String description) {
-        this.description = description;
-    }
-    
+
 }

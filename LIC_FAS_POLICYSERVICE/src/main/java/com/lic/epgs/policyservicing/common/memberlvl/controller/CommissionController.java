254340c493147c937d233b0ef533a7131ed0e56f
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-05 17:29:11 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/memberlvl/controller/CommissionController.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/memberlvl/controller/CommissionController.java
@@ -1,34 +1,63 @@
+//Spring Boot Controller Class
+
 package com.lic.epgs.policyservicing.common.memberlvl.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lic.epgs.policyservicing.common.memberlvl.entity.CommissionDetailsEntity;
 import com.lic.epgs.policyservicing.common.memberlvl.entity.CommissionEntity;
 import com.lic.epgs.policyservicing.common.memberlvl.service.CommissionService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
-
-@Controller
-@RequestMapping("/api/commission")
+@RestController
+@RequestMapping("/commission")
 public class CommissionController {
 
     @Autowired
-    private CommissionService commissionService;
+    CommissionService commissionService;
 
-    @GetMapping("/inprogress")
-    public ResponseEntity<List<CommissionEntity>> getInprogressLoad(@RequestParam(name = "unitCode") String unitCode) {
-        List<CommissionEntity> commissionEntities = commissionService.getInprogressLoad(unitCode);
-        return new ResponseEntity<>(commissionEntities, HttpStatus.OK);
+    @GetMapping("/inprogress-load/{role}/{unitCode}")
+    public List<CommissionEntity> getInprogressLoad(@PathVariable String role, @PathVariable String unitCode) {
+        return commissionService.getInprogressLoad(role, unitCode);
     }
 
-    @GetMapping("/inprogress/maker")
-    public ResponseEntity<List<CommissionEntity>> getInprogressMakerLoad(@RequestParam(name = "unitCode") String unitCode) {
-        List<CommissionEntity> commissionEntities = commissionService.getInprogressMakerLoad(unitCode);
-        return new ResponseEntity<>(commissionEntities, HttpStatus.OK);
+    @GetMapping("/inprogress-maker-load/{role}/{unitCode}")
+    public List<CommissionEntity> getInprogressMakerLoad(@PathVariable String role, @PathVariable String unitCode) {
+        return commissionService.getInprogressMakerLoad(role, unitCode);
     }
 
+    @GetMapping("/{id}")
+    public Optional<CommissionEntity> findById(@PathVariable Long id) {
+        return commissionService.findById(id);
+    }
+
+    @PostMapping
+    public CommissionEntity save(@RequestBody CommissionEntity commissionEntity) {
+        return commissionService.save(commissionEntity);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        commissionService.delete(commissionService.findById(id).get());
+    }
+
+    @PutMapping
+    public CommissionDetailsEntity saveCommissionDetails(@RequestBody CommissionDetailsEntity commissionDetailsEntity) {
+        return commissionService.saveCommissionDetails(commissionDetailsEntity);
+    }
+
+    @DeleteMapping
+    public void deleteCommissionDetails(@RequestBody CommissionDetailsEntity commissionDetailsEntity) {
+        commissionService.deleteCommissionDetails(commissionDetailsEntity);
+    }
 }

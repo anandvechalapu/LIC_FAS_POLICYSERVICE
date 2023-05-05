293958e8a3c47da035b0ec628f718e3826a543bc
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-05 17:23:17 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/memberlvl/model/Commission.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/memberlvl/model/Commission.java
@@ -1,48 +1,54 @@
 package com.lic.epgs.policyservicing.common.memberlvl.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
-@Table(name = "commission")
+@Table(name="commission")
 public class Commission {
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "id")
-	private Long id;
-	
-	@Column(name = "role")
-	private String role;
-	
-	@Column(name = "unit_code")
-	private String unitCode;
-	
-	public Long getId() {
-		return id;
-	}
-	
-	public void setId(Long id) {
-		this.id = id;
-	}
-	
-	public String getRole() {
-		return role;
-	}
-	
-	public void setRole(String role) {
-		this.role = role;
-	}
-	
-	public String getUnitCode() {
-		return unitCode;
-	}
-	
-	public void setUnitCode(String unitCode) {
-		this.unitCode = unitCode;
-	}
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "unit_code")
+    private String unitCode;
+
+    @Column(name = "is_inprogress")
+    private Boolean isInprogress;
+
+    @OneToMany(mappedBy = "commission", cascade = CascadeType.ALL)
+    private CommissionDetails commissionDetails;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUnitCode() {
+        return unitCode;
+    }
+
+    public void setUnitCode(String unitCode) {
+        this.unitCode = unitCode;
+    }
+
+    public Boolean getIsInprogress() {
+        return isInprogress;
+    }
+
+    public void setIsInprogress(Boolean isInprogress) {
+        this.isInprogress = isInprogress;
+    }
+
+    public CommissionDetails getCommissionDetails() {
+        return commissionDetails;
+    }
+
+    public void setCommissionDetails(CommissionDetails commissionDetails) {
+        this.commissionDetails = commissionDetails;
+    }
 }

8efffab8433f2b37ddc1d6cce150860dc2c1a757
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-05 15:05:59 +0530

    initial commit

---
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policyservicingcontroller/model/Member.java
@@ -0,0 +1,60 @@
+package com.lic.epgs.policyservicing.common.policyservicingcontroller.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Member {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String name;
+	private String address;
+	private String contactNumber;
+	
+	public Member() {
+		
+	}
+	
+	public Member(String name, String address, String contactNumber) {
+		this.name = name;
+		this.address = address;
+		this.contactNumber = contactNumber;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getAddress() {
+		return address;
+	}
+	
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	public String getContactNumber() {
+		return contactNumber;
+	}
+	
+	public void setContactNumber(String contactNumber) {
+		this.contactNumber = contactNumber;
+	}
+	
+}

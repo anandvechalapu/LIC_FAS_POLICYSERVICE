ee14c7bfa6c0941909ea28a6f67652aa15d19abe
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-05 17:18:27 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/memberlvl/service/CommissionService.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/memberlvl/service/CommissionService.java
@@ -4,41 +4,35 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.lic.epgs.policyservicing.common.memberlvl.entity.CommissionEntity;
 import com.lic.epgs.policyservicing.common.memberlvl.entity.CommissionDetailsEntity;
 import com.lic.epgs.policyservicing.common.memberlvl.repository.CommissionRepository;
 
 @Service
-@Transactional
 public class CommissionService {
+	
+	@Autowired
+	private CommissionRepository commissionRepository;
+	
+	public CommissionEntity getInprogressLoad(String role, String unitCode) {
+		return commissionRepository.getInprogressLoad(role, unitCode);
+	}
+	
+	public CommissionDetailsEntity getInprogressMakerLoad(String role, String unitCode) {
+		return commissionRepository.getInprogressMakerLoad(role, unitCode);
+	}
+	
+	public List<CommissionEntity> getAllCommissions() {
+		return commissionRepository.findAll();
+	}
+	
+	public CommissionEntity saveCommission(CommissionEntity commission) {
+		return commissionRepository.save(commission);
+	}
+	
+	public CommissionDetailsEntity saveCommissionDetails(CommissionDetailsEntity commissionDetails) {
+		return commissionRepository.save(commissionDetails);
+	}
 
-    @Autowired
-    private CommissionRepository commissionRepository;
-
-    public CommissionEntity findByUnitCodeAndRole(String unitCode, String role) {
-        return commissionRepository.findByUnitCodeAndRole(unitCode, role);
-    }
-    
-    public CommissionDetailsEntity findByCommissionId(Long commissionId) {
-        return commissionRepository.findByCommissionId(commissionId);
-    }
-    
-    public void deleteByCommissionId(Long commissionId) {
-        commissionRepository.deleteByCommissionId(commissionId);
-    }
-    
-    public CommissionEntity save(CommissionEntity commissionEntity) {
-        return commissionRepository.save(commissionEntity);
-    }
-    
-    public CommissionDetailsEntity save(CommissionDetailsEntity commissionDetailsEntity) {
-        return commissionRepository.save(commissionDetailsEntity);
-    }
-    
-    public List<CommissionEntity> findAllCommissions() {
-        return commissionRepository.findAll();
-    }
-    
 }

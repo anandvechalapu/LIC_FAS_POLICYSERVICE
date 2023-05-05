27be1c7256bcdf1547dff17a7436b5820bd2aa11
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-05 17:14:56 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/memberlvl/repository/CommissionRepository.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/memberlvl/repository/CommissionRepository.java
@@ -1,22 +1,17 @@
 package com.lic.epgs.policyservicing.common.memberlvl.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.lic.epgs.policyservicing.common.memberlvl.entity.CommissionEntity;
 import com.lic.epgs.policyservicing.common.memberlvl.entity.CommissionDetailsEntity;
 
-@Repository
 public interface CommissionRepository extends JpaRepository<CommissionEntity, Long> {
+	
+	@Query("SELECT c FROM CommissionEntity c WHERE c.role = :role AND c.unitCode = :unitCode")
+	public CommissionEntity getInprogressLoad(String role, String unitCode);
+	
+	@Query("SELECT c FROM CommissionDetailsEntity c WHERE c.maker = :role AND c.unitCode = :unitCode")
+	public CommissionDetailsEntity getInprogressMakerLoad(String role, String unitCode);
 
-    public CommissionEntity findByUnitCodeAndRole(String unitCode, String role);
-    
-    public CommissionDetailsEntity findByCommissionId(Long commissionId);
-    
-    public void deleteByCommissionId(Long commissionId);
-    
-    public CommissionEntity save(CommissionEntity commissionEntity);
-    
-    public CommissionDetailsEntity save(CommissionDetailsEntity commissionDetailsEntity);
-    
 }

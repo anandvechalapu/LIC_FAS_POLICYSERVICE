69dacbf9a0cb3310f30fcece024c80312a85a275
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-07 18:17:19 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/memberlvl/policylevelconversioncheckercontroller/model/PolicyLevelConversionTempEntity.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/memberlvl/policylevelconversioncheckercontroller/model/PolicyLevelConversionTempEntity.java
@@ -1,27 +1,124 @@
-@Entity
-@Table(name = "policy_level_conversion_temp")
 public class PolicyLevelConversionTempEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long conversionId;
-
-    private Long prevPolicyId;
-
-    private String conversionStatus;
-
-    private String conversionWorkflowStatus;
-
-    @Column(name = "created_by")
+    private String conversionId;
+    private String policyNumber;
+    private String existingLevel;
+    private String requestedLevel;
+    private String requestedBy;
+    private String requestedOn;
+    private String status;
+    private String rejectionReasonCode;
+    private String remarks;
     private String createdBy;
-
-    @Column(name = "created_on")
-    private Date createdOn;
-
-    @Column(name = "modified_by")
+    private String createdOn;
     private String modifiedBy;
+    private String modifiedOn;
 
-    @Column(name = "modified_on")
-    private Date modifiedOn;
+    public PolicyLevelConversionTempEntity() {
+    }
+
+    public String getConversionId() {
+        return conversionId;
+    }
+
+    public void setConversionId(String conversionId) {
+        this.conversionId = conversionId;
+    }
+
+    public String getPolicyNumber() {
+        return policyNumber;
+    }
+
+    public void setPolicyNumber(String policyNumber) {
+        this.policyNumber = policyNumber;
+    }
+
+    public String getExistingLevel() {
+        return existingLevel;
+    }
+
+    public void setExistingLevel(String existingLevel) {
+        this.existingLevel = existingLevel;
+    }
+
+    public String getRequestedLevel() {
+        return requestedLevel;
+    }
+
+    public void setRequestedLevel(String requestedLevel) {
+        this.requestedLevel = requestedLevel;
+    }
+
+    public String getRequestedBy() {
+        return requestedBy;
+    }
+
+    public void setRequestedBy(String requestedBy) {
+        this.requestedBy = requestedBy;
+    }
+
+    public String getRequestedOn() {
+        return requestedOn;
+    }
+
+    public void setRequestedOn(String requestedOn) {
+        this.requestedOn = requestedOn;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getRejectionReasonCode() {
+        return rejectionReasonCode;
+    }
+
+    public void setRejectionReasonCode(String rejectionReasonCode) {
+        this.rejectionReasonCode = rejectionReasonCode;
+    }
+
+    public String getRemarks() {
+        return remarks;
+    }
+
+    public void setRemarks(String remarks) {
+        this.remarks = remarks;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public String getCreatedOn() {
+        return createdOn;
+    }
+
+    public void setCreatedOn(String createdOn) {
+        this.createdOn = createdOn;
+    }
+
+    public String getModifiedBy() {
+        return modifiedBy;
+    }
+
+    public void setModifiedBy(String modifiedBy) {
+        this.modifiedBy = modifiedBy;
+    }
+
+    public String getModifiedOn() {
+        return modifiedOn;
+    }
+
+    public void setModifiedOn(String modifiedOn) {
+        this.modifiedOn = modifiedOn;
+    }
 
 }

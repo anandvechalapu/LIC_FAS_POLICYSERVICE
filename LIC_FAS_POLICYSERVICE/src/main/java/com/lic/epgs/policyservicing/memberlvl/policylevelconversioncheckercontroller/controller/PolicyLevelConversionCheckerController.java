d30ee848f53cd7304f1619ade6ce26fa8be8d83e
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-07 18:18:12 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/memberlvl/policylevelconversioncheckercontroller/controller/PolicyLevelConversionCheckerController.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/memberlvl/policylevelconversioncheckercontroller/controller/PolicyLevelConversionCheckerController.java
@@ -1,27 +1,26 @@
 package com.lic.epgs.policyservicing.memberlvl.policylevelconversioncheckercontroller.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.lic.epgs.policyservicing.memberlvl.policylevelconversioncheckercontroller.model.CommonResponseDto;
-import com.lic.epgs.policyservicing.memberlvl.policylevelconversioncheckercontroller.model.PolicyLevelConversionDto;
-import com.lic.epgs.policyservicing.memberlvl.policylevelconversioncheckercontroller.service.PolicyLevelConversionCheckerControllerService;
+import com.lic.epgs.policyservicing.memberlvl.policylevelconversioncheckercontroller.model.PolicyLevelConversionTempEntity;
+import com.lic.epgs.policyservicing.memberlvl.policylevelconversioncheckercontroller.service.PolicyConversionRejection_PolicyLevelConversionCheckerControllerService;
 
 @RestController
 public class PolicyLevelConversionCheckerController {
 
     @Autowired
-    private PolicyLevelConversionCheckerControllerService policyLevelConversionCheckerControllerService;
+    private PolicyConversionRejection_PolicyLevelConversionCheckerControllerService policyConversionRejection_PolicyLevelConversionCheckerControllerService;
 
-    @PostMapping("/policyLevelConversionChecker")
-    public ResponseEntity<CommonResponseDto> getExistingDetailsByNewPolicyNo(
-            @RequestBody PolicyLevelConversionDto policyLevelConversionDto) {
-
-        CommonResponseDto responseDto = policyLevelConversionCheckerControllerService.getExistingDetailsByNewPolicyNo(policyLevelConversionDto.getNewPolicyNo());
-        return ResponseEntity.ok(responseDto);
+    @PostMapping("/save/policyLevelConversion")
+    public PolicyLevelConversionTempEntity save(@RequestBody PolicyLevelConversionTempEntity policyLevelConversionTempEntity) {
+        return policyConversionRejection_PolicyLevelConversionCheckerControllerService.save(policyLevelConversionTempEntity);
     }
 
+    @PostMapping("/update/policyLevelConversion")
+    public int updatePolicyLevelConversionTempEntity(@RequestBody PolicyLevelConversionTempEntity policyLevelConversionTempEntity) {
+        return policyConversionRejection_PolicyLevelConversionCheckerControllerService.updatePolicyLevelConversionTempEntity(policyLevelConversionTempEntity.getConversionId(), policyLevelConversionTempEntity.getRejectionReasonCode(), policyLevelConversionTempEntity.getRemarks(), policyLevelConversionTempEntity.getModifiedBy(), policyLevelConversionTempEntity.getModifiedOn());
+    }
 }

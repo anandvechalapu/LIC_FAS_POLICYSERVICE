4f1283c66c5e0f6cad0d63b90f15ed820c6bfe0d
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-07 13:08:02 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/surrender/controller/PolicySurrenderController.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/surrender/controller/PolicySurrenderController.java
@@ -1,27 +1,30 @@
 package com.lic.epgs.surrender.controller;
 
-import com.lic.epgs.surrender.model.PolicySurrenderApiResponse;
-import com.lic.epgs.surrender.service.GetExistingSurrenderOverallDetails_PolicySurrenderService;
+import com.lic.epgs.model.MphBankDto;
+import com.lic.epgs.model.PolicySurrenderApiResponse;
+import com.lic.epgs.surrender.service.PolicySurrenderService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
+import java.util.List;
 
 @RestController
-@RequestMapping("/surrender")
 public class PolicySurrenderController {
 
+    private static final Logger logger = LoggerFactory.getLogger(PolicySurrenderController.class);
+
     @Autowired
-    private GetExistingSurrenderOverallDetails_PolicySurrenderService getExistingSurrenderOverallDetails_PolicySurrenderService;
+    private PolicySurrenderService policySurrenderService;
 
-    @GetMapping("/{surrenderId}")
-    public Optional<PolicySurrenderApiResponse> findBySurrenderId(@PathVariable Long surrenderId) {
-        return getExistingSurrenderOverallDetails_PolicySurrenderService.findBySurrenderId(surrenderId);
-    }
-
-    @PostMapping
-    public PolicySurrenderApiResponse getExistingSurrenderOverallDetails_PolicySurrender(@RequestBody Long surrenderId) {
-        return getExistingSurrenderOverallDetails_PolicySurrenderService.getExistingSurrenderOverallDetails_PolicySurrender(surrenderId);
+    // Controller method to call service method
+    @GetMapping(value = "/policy/bank/details")
+    public PolicySurrenderApiResponse getPolicyBankDetails(@RequestParam("mphId") String mphId) {
+        logger.info("Calling policy surrender service to get policy bank details for mphId: {}", mphId);
+        return policySurrenderService.getPolicyBankDetails(mphId);
     }
 
 }

83ea02997e13b97a9f7e1602626e050e6d3f9e7d
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-07 18:55:05 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/memberlvl/policylevelconversioncontroller/controller/PolicyLevelConversionController.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/memberlvl/policylevelconversioncontroller/controller/PolicyLevelConversionController.java
@@ -1,26 +1,52 @@
+package com.lic.epgs.policyservicing.memberlvl.policylevelconversioncontroller.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.lic.epgs.policyservicing.memberlvl.policylevelconversioncontroller.model.ExistingPolicyConversionDetails;
+
+public interface GetExistingPolicyConversionDetailsList_PolicyLevelConversionControllerRepository extends JpaRepository<ExistingPolicyConversionDetails, Long> {
+    List<ExistingPolicyConversionDetails> findByRoleAndUnitCode(String role, String unitCode);
+}
+
 package com.lic.epgs.policyservicing.memberlvl.policylevelconversioncontroller.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.lic.epgs.policyservicing.memberlvl.policylevelconversioncontroller.entity.PolicyLevelConversionDetails;
-import com.lic.epgs.policyservicing.memberlvl.policylevelconversioncontroller.service.GetInprogressPolicyConversionDetailsList_PolicyLevelConversionControllerService;
+import com.lic.epgs.policyservicing.memberlvl.policylevelconversioncontroller.model.ExistingPolicyConversionDetails;
+import com.lic.epgs.policyservicing.memberlvl.policylevelconversioncontroller.service.GetExistingPolicyConversionDetailsList_PolicyLevelConversionControllerService;
 
 @RestController
-@RequestMapping("/policylevelconversion")
+@RequestMapping("/api")
 public class PolicyLevelConversionController {
 
     @Autowired
-    private GetInprogressPolicyConversionDetailsList_PolicyLevelConversionControllerService service;
+    GetExistingPolicyConversionDetailsList_PolicyLevelConversionControllerService existingPolicyConversionDetailsListService;
 
-    @GetMapping("/getconversiondetails")
-    public List<PolicyLevelConversionDetails> getInProgressPolicyConversionDetails(@RequestParam("role") String role, @RequestParam("unitCode") String unitCode) {
-        return service.getInProgressPolicyConversionDetails(role, unitCode);
+    @PostMapping("/existingpolicyconversiondetails")
+    public ExistingPolicyConversionDetails createExistingPolicyConversionDetails(@Valid @RequestBody ExistingPolicyConversionDetails existingPolicyConversionDetails) {
+        return existingPolicyConversionDetailsListService.save(existingPolicyConversionDetails);
     }
 
+    @GetMapping("/existingpolicyconversiondetails/{role}/{unitCode}")
+    public ResponseEntity<List<ExistingPolicyConversionDetails>> getExistingPolicyConversionDetailsListByRoleAndUnitCode(@PathVariable(value = "role") String role,
+                                                                                                                     @PathVariable(value = "unitCode") String unitCode) {
+        List<ExistingPolicyConversionDetails> existingPolicyConversionDetailsList = existingPolicyConversionDetailsListService.GetExistingPolicyConversionDetailsList_PolicyLevelConversionController(role, unitCode);
+        if (existingPolicyConversionDetailsList == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok().body(existingPolicyConversionDetailsList);
+    }
 }

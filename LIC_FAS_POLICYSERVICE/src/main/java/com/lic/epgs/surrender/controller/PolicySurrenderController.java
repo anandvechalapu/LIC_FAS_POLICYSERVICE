cc3ca899684a26e2af22d7effd2a2e37b1091d7e
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-07 13:23:19 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/surrender/controller/PolicySurrenderController.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/surrender/controller/PolicySurrenderController.java
@@ -1,36 +1,26 @@
-package com.lic.epgs.surrender.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.lic.epgs.surrender.model.PolicySurrender;
-
-@Repository
-public interface PolicySurrenderRepository extends JpaRepository<PolicySurrender, Long> {
-
-    public PolicySurrenderApiResponse getPolicySurrenderbySurrenderId(Long surrenderId);
-
-}
-
 package com.lic.epgs.surrender.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.lic.epgs.surrender.model.PolicySurrenderApiResponse;
-import com.lic.epgs.surrender.service.PolicySurrenderService;
+import com.lic.epgs.surrender.dto.CommonNotesDto;
+import com.lic.epgs.surrender.service.PolicySurrenderServiceImpl;
 
 @RestController
+@RequestMapping("/api/surrender")
 public class PolicySurrenderController {
     
     @Autowired
-    private PolicySurrenderService policySurrenderService;
-    
-    @GetMapping("/surrender/{surrenderId}")
-    public PolicySurrenderApiResponse getPolicySurrenderbySurrenderId(@PathVariable("surrenderId") Long surrenderId) {
-        return policySurrenderService.getPolicySurrenderbySurrenderId(surrenderId);
-    }
+    private PolicySurrenderServiceImpl policySurrenderService;
 
+    @GetMapping("/{surrenderId}")
+    public ResponseEntity<List<CommonNotesDto>> getSurrenderNotesList(@PathVariable("surrenderId") Long surrenderId){
+        return policySurrenderService.getSurrenderNotesList(surrenderId);
+    }
 }

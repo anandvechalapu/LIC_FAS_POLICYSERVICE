634e3f1a052e6b49b26400d118dd859868cedd9b
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-07 14:29:11 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/topupda/controller/TopupdaController.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/topupda/controller/TopupdaController.java
@@ -1,23 +1,42 @@
 package com.lic.epgs.topupda.controller;
 
-import com.lic.epgs.topupda.dto.TopupdaApiResponseDto;
-import com.lic.epgs.topupda.dto.TopupdaNotesDto;
-import com.lic.epgs.topupda.service.SaveNotesTopupdaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.lic.epgs.topupda.model.TopupdaTempEntity;
+import com.lic.epgs.topupda.model.TopupdaEntity;
+import com.lic.epgs.topupda.service.TopupdaService;
+
 @RestController
 @RequestMapping("/topupda")
 public class TopupdaController {
 
-    @Autowired
-    private SaveNotesTopupdaService saveNotesTopupdaService;
+	@Autowired
+	private TopupdaService topupdaService;
 
-    @PostMapping("/saveNotes")
-    public TopupdaApiResponseDto saveNotes(@RequestBody TopupdaNotesDto topupdaNotesDto) {
-        return saveNotesTopupdaService.saveNotes(topupdaNotesDto);
-    }
+	@GetMapping("/temp/{topupId}")
+	public TopupdaTempEntity findTopupdaTempByTopupId(@PathVariable Long topupId) {
+		return topupdaService.findTopupdaTempByTopupId(topupId);
+	}
+
+	@PostMapping("/temp")
+	public TopupdaTempEntity saveTopupdaTemp(@RequestBody TopupdaTempEntity topupdaTempEntity) {
+		return topupdaService.saveTopupdaTemp(topupdaTempEntity);
+	}
+
+	@GetMapping("/{topupId}")
+	public TopupdaEntity findTopupdaByTopupId(@PathVariable Long topupId) {
+		return topupdaService.findTopupdaByTopupId(topupId);
+	}
+
+	@PostMapping
+	public TopupdaEntity saveTopupda(@RequestBody TopupdaEntity topupdaEntity) {
+		return topupdaService.saveTopupda(topupdaEntity);
+	}
+
 }

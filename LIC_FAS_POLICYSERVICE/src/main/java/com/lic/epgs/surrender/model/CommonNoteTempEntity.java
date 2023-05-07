24a1ce318d262ca510b839f9650941a54cefebf7
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-07 13:45:49 +0530

    initial commit

---
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/surrender/model/CommonNoteTempEntity.java
@@ -0,0 +1,58 @@
+package com.lic.epgs.surrender.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "commonNoteTemp")
+public class CommonNoteTempEntity {
+
+    @Id
+    private Long id;
+    private String note;
+    private Long userId;
+    private String createdAt;
+    private String updatedAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+    
+}

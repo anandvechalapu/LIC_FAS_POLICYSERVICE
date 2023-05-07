5a059c0d6231d1949299b0bd751a43d7af983d67
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-07 13:59:53 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/surrender/repository/.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/surrender/repository/.java
@@ -1,46 +1,47 @@
-package com.lic.epgs.surrender.repository;
-
-import com.lic.epgs.model.MphBankDto;
-import com.lic.epgs.model.PolicySurrenderApiResponse;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
-
-import java.util.List;
-
 @Repository
-public class PolicySurrenderRepository {
-
-    private static final Logger logger = LoggerFactory.getLogger(PolicySurrenderRepository.class);
+public class UpdatePolicyBankDetailsRepository {
 
     @Autowired
-    private MphBankRepository mphBankRepository;
+    private EntityManager em;
 
-    // Repository method to get policy bank details
-    public PolicySurrenderApiResponse getPolicyBankDetails(String mphId) {
-        logger.info("Fetching policy bank details for mphId: {}", mphId);
-
-        PolicySurrenderApiResponse policySurrenderApiResponse = new PolicySurrenderApiResponse();
-
+    public PolicySurrenderApiResponse updatePolicyBankDetails(MphBankDto mphBankDto) {
+        PolicySurrenderApiResponse response = new PolicySurrenderApiResponse();
         try {
-            List<MphBankDto> mphBankDtos = mphBankRepository.getMphBankEntityByMphId(mphId);
-            if (mphBankDtos.size() > 0) {
-                policySurrenderApiResponse.setPolicyBanks(mphBankDtos);
-                policySurrenderApiResponse.setTransactionStatus("SUCCESS");
-                policySurrenderApiResponse.setTransactionMessage("FETCH_MESSAGE");
-            } else {
-                policySurrenderApiResponse.setTransactionStatus("FAIL");
-                policySurrenderApiResponse.setTransactionMessage("NO_RECORD_FOUND");
+            // Fetch MPH ID from MphBankDto object
+            Long mphId = mphBankDto.getMphId();
+            // Fetch MPH Master record from database
+            MphMaster mphMaster = em.find(MphMaster.class, mphId);
+            if (mphMaster == null || !mphMaster.getActive()) {
+                response.setTransactionStatus(TransactionStatus.FAILURE);
+                response.setTransactionMessage("Invalid or inactive MPH ID");
+                return response;
             }
-        } catch (IllegalArgumentException e) {
-            policySurrenderApiResponse.setTransactionStatus("FAIL");
-            policySurrenderApiResponse.setTransactionMessage(e.getMessage());
+            // Fetch MPH Master Temp record from database
+            MphMasterTemp mphMasterTemp = em.find(MphMasterTemp.class, mphId);
+            if (mphMasterTemp == null || !mphMasterTemp.getActive()) {
+                response.setTransactionStatus(TransactionStatus.FAILURE);
+                response.setTransactionMessage("Invalid or inactive MPH Master Temp record");
+                return response;
+            }
+            // Fetch MPH Bank Temp record from database
+            MphBankTemp mphBankTemp = em.find(MphBankTemp.class, mphId);
+            // Set MPH Bank Temp ID to null to create new record or update existing record
+            mphBankTemp.setId(null);
+            // Map MphBankDto object to MPH Bank Temp entity
+            ModelMapper modelMapper = new ModelMapper();
+            modelMapper.map(mphBankDto, mphBankTemp);
+            // Save MPH Bank Temp entity to database
+            em.persist(mphBankTemp);
+            em.flush();
+            // Prepare response
+            response.setTransactionStatus(TransactionStatus.SUCCESS);
+            response.setTransactionMessage("Mph Bank Details updated successfully");
+            response.setPolicyBank(mphBankTemp);
+        } catch (Exception e) {
+            response.setTransactionStatus(TransactionStatus.FAILURE);
+            response.setTransactionMessage("Error occurred while updating Mph Bank Details");
         }
-
-        logger.info("Finished fetching policy bank details for mphId: {}", mphId);
-
-        return policySurrenderApiResponse;
+        return response;
     }
 
 }

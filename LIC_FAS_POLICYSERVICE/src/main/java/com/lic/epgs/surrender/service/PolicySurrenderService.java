4abdcec4c5d8c24a3eed8c1913136050d4949915
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-07 13:15:04 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/surrender/service/PolicySurrenderService.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/surrender/service/PolicySurrenderService.java
@@ -1,28 +1,19 @@
 package com.lic.epgs.surrender.service;
 
-import com.lic.epgs.model.MphBankDto;
-import com.lic.epgs.model.PolicySurrenderApiResponse;
-import com.lic.epgs.surrender.repository.MphBankRepository;
-import com.lic.epgs.surrender.repository.PolicySurrenderRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import com.lic.epgs.surrender.model.PolicySurrenderApiResponse;
+import com.lic.epgs.surrender.repository.PolicySurrenderRepository;
 
 @Service
 public class PolicySurrenderService {
-
-    private static final Logger logger = LoggerFactory.getLogger(PolicySurrenderService.class);
-
+ 
     @Autowired
-    private PolicySurrenderRepository policySurrenderRepository;
-
-    // Service method to call repository method
-    public PolicySurrenderApiResponse getPolicyBankDetails(String mphId) {
-        logger.info("Calling policy surrender repository to get policy bank details for mphId: {}", mphId);
-        return policySurrenderRepository.getPolicyBankDetails(mphId);
+    PolicySurrenderRepository policySurrenderRepository;
+ 
+    public PolicySurrenderApiResponse getPolicySurrenderbySurrenderId(Long surrenderId) {
+        return policySurrenderRepository.getPolicySurrenderbySurrenderId(surrenderId);
     }
-
+ 
 }

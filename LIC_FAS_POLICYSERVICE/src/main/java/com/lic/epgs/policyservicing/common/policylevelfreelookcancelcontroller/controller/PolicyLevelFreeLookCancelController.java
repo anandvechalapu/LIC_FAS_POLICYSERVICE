25a0a77582a3d1a280333fd7054de5a360deb080
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-08 16:25:17 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelfreelookcancelcontroller/controller/PolicyLevelFreeLookCancelController.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelfreelookcancelcontroller/controller/PolicyLevelFreeLookCancelController.java
@@ -1,54 +1,35 @@
-OB) {
-        this.policyHolderDOB = policyHolderDOB;
-    }
-
-    public String getPolicyHolderAddress() {
-        return policyHolderAddress;
-    }
-
-    public void setPolicyHolderAddress(String policyHolderAddress) {
-        this.policyHolderAddress = policyHolderAddress;
-    }
-
-    public String getFreeLookCancellationDate() {
-        return freeLookCancellationDate;
-    }
-
-    public void setFreeLookCancellationDate(String freeLookCancellationDate) {
-        this.freeLookCancellationDate = freeLookCancellationDate;
-    }
-
-    public String getFreeLookCancellationReason() {
-        return freeLookCancellationReason;
-    }
-
-    public void setFreeLookCancellationReason(String freeLookCancellationReason) {
-        this.freeLookCancellationReason = freeLookCancellationReason;
-    }
-
-}
-
 package com.lic.epgs.policyservicing.common.policylevelfreelookcancelcontroller.controller;
 
-import org.springframework.beans.BeanUtils;
+import com.lic.epgs.policyservicing.common.policylevelfreelookcancelcontroller.dto.FreeLookCancellationDto;
+import com.lic.epgs.policyservicing.common.policylevelfreelookcancelcontroller.service.ExistingCitrieaSearch_PolicyLevelFreeLookCancelControllerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-import com.lic.epgs.policyservicing.common.policylevelfreelookcancelcontroller.dto.FreeLookCancellationResponseDto;
-import com.lic.epgs.policyservicing.common.policylevelfreelookcancelcontroller.service.PolicyLevelFreeLookCancelControllerService;
+import java.util.Date;
+import java.util.List;
 
 @RestController
-@RequestMapping("/freeLookCancellation")
+@RequestMapping("/policyLevelFreeLookCancel")
 public class PolicyLevelFreeLookCancelController {
-    
-    @Autowired
-    PolicyLevelFreeLookCancelControllerService policyLevelFreeLookCancelControllerService;
 
-    @GetMapping("/{freeLookCancellationId}")
-    public FreeLookCancellationResponseDto getFreeLookCancellationDetails(@PathVariable("freeLookCancellationId") Long freeLookCancellationId) {
-        return policyLevelFreeLookCancelControllerService.getFreeLookCancellationDetails(freeLookCancellationId);
+    @Autowired
+    ExistingCitrieaSearch_PolicyLevelFreeLookCancelControllerService existingCitrieaSearch_PolicyLevelFreeLookCancelControllerService;
+
+    @GetMapping("/{policyNumber}/{mphCode}/{mphName}/{from}/{to}/{product}/{lineOfBusiness}/{freeLookStatus}/{unitCode}")
+    public ResponseEntity<List<FreeLookCancellationDto>> getByAllParameters(@PathVariable String policyNumber, @PathVariable String mphCode, @PathVariable String mphName, @PathVariable Date from, @PathVariable Date to, @PathVariable String product, @PathVariable String lineOfBusiness, @PathVariable String freeLookStatus, @PathVariable String unitCode){
+        List<FreeLookCancellationDto> freeLookCancellationDtos =  existingCitrieaSearch_PolicyLevelFreeLookCancelControllerService.findAllByPolicyNumberAndMphCodeAndMphNameAndFromAndToAndProductAndLineOfBusinessAndFreeLookStatusAndUnitCode(policyNumber,mphCode,mphName,from,to,product,lineOfBusiness,freeLookStatus,unitCode);
+        return new ResponseEntity<>(freeLookCancellationDtos, HttpStatus.OK);
     }
-}
+
+    @GetMapping("/{mphCode}/{mphName}/{from}/{to}/{product}/{lineOfBusiness}/{freeLookStatus}/{unitCode}")
+    public ResponseEntity<List<FreeLookCancellationDto>> getByMphCodeMphNameFromToProductLineOfBusinessFreeLookStatusUnitCode(@PathVariable String mphCode, @PathVariable String mphName, @PathVariable Date from, @PathVariable Date to, @PathVariable String product, @PathVariable String lineOfBusiness, @PathVariable String freeLookStatus, @PathVariable String unitCode){
+        List<FreeLookCancellationDto> freeLookCancellationDtos = existingCitrieaSearch_PolicyLevelFreeLookCancelControllerService.findAllByMphCodeAndMphNameAndFromAndToAndProductAndLineOfBusinessAndFreeLookStatusAndUnitCode(mphCode,mphName,from,to,product,lineOfBusiness,freeLookStatus,unitCode);
+        return new ResponseEntity<>(freeLookCancellationDtos, HttpStatus.OK);
+    }
+
+    @GetMapping("/{policyNumber}/{mphName}/{from}/{to}/{product}/{lineOfBusiness}/{freeLookStatus}/{unitCode}")
+    public ResponseEntity<List<FreeLookCancellationDto>> getByPolicyNumberMphNameFromToProductLineOfBusinessFreeLookStatusUnitCode(@PathVariable String policyNumber, @PathVariable String mphName, @PathVariable Date from, @PathVariable Date to, @PathVariable String product, @PathVariable String lineOfBusiness, @PathVariable String freeLookStatus, @PathVariable String unitCode){
+        List<FreeLookCancellationDto> freeLookCancellationDtos = existingCitrieaSearch_PolicyLevelFreeLookCancelControllerService.findAllByPolicyNumberAndMphNameAndFromAndToAndProductAndLineOfBusinessAndFreeLookStatusAndUnitCode(policyNumber,mphName,from,to,product,lineOfBusiness,freeLookStatus,unitCode);
+        return new ResponseEntity<>(freeLookCancellationDtos

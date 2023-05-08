289516817bc123d114cb25cb4d8c7ed9e3a337a5
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-08 17:24:05 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelmemberadditioncontroller/repository/PolicyLevelMemberAdditionControllerRepository.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelmemberadditioncontroller/repository/PolicyLevelMemberAdditionControllerRepository.java
@@ -1,26 +1,16 @@
-import java.util.List;
+package com.lic.epgs.policyservicing.common.policylevelmemberadditioncontroller.repository;
+
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import com.lic.epgs.policyservicing.common.policylevelmemberadditioncontroller.dto.CommonResponseDto;
-import com.lic.epgs.policyservicing.common.policylevelmemberadditioncontroller.dto.PolicyAomDto;
-import com.lic.epgs.policyservicing.common.policylevelmemberadditioncontroller.dto.PolicyServiceMemberAdditionDto;
-import com.lic.epgs.policyservicing.common.policylevelmemberadditioncontroller.dto.PolicyServiceMbrDto;
+import com.lic.epgs.policyservicing.common.policylevelmemberadditioncontroller.model.PolicyLevelMemberAdditionController;
 
-public interface PolicyLevelMemberAdditionControllerRepository extends JpaRepository<CommonResponseDto<Object>, Long> {
+public interface PolicyLevelMemberAdditionControllerRepository extends JpaRepository<PolicyLevelMemberAdditionController, Long> {
+ 
+    @Query("SELECT MAX(p.licenseId) FROM PolicyLevelMemberAdditionController p")
+    Optional<Long> getMaxLicenseId();
 
-	@Query("SELECT new com.lic.epgs.policyservicing.common.policylevelmemberadditioncontroller.dto.PolicyAomDto(p.POLICY_NUMBER, p.MPH_NAME, p.MPH_CODE, p.LINE_OF_BUSINESS, p.PRODUCT_ID, p.VARIANT, p.NO_OF_CATEGORY, mbr, madd) "
-			+ "FROM PolicyAomDto p, PolicyServiceMbrDto mbr, PolicyServiceMemberAdditionDto madd "
-			+ "WHERE p.POLICY_NUMBER = mbr.POLICY_NUMBER AND p.POLICY_NUMBER = madd.POLICY_NUMBER")
-	public CommonResponseDto<Object> getAllPolicyLevelMemberAdditionController(PolicyServiceMemberAdditionDto policyServiceMemberAdditionDto);
-
-	@Query("SELECT mbr FROM PolicyServiceMbrDto mbr")
-	public List<PolicyServiceMbrDto> getMemberDetails();
-
-	@Query("SELECT madd FROM PolicyServiceMemberAdditionDto madd")
-	public List<PolicyServiceMemberAdditionDto> getMemberAdditionDetails();
-
-	public List<String> getNotes();
-
+    Optional<PolicyLevelMemberAdditionController> findByLicenseId(Long licenseId);
 }

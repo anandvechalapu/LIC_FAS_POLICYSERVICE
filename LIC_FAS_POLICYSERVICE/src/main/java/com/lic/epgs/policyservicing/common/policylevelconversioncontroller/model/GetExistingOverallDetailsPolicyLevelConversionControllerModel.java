825e253e31afdce97da25de85b7eb78b5c86d831
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-08 13:05:51 +0530

    initial commit

---
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelconversioncontroller/model/GetExistingOverallDetailsPolicyLevelConversionControllerModel.java
@@ -0,0 +1,14 @@
+package com.lic.epgs.policyservicing.common.policylevelconversioncontroller.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class GetExistingOverallDetailsPolicyLevelConversionControllerModel {
+
+    @Id
+    private Long serviceId;
+
+    // other fields
+
+}

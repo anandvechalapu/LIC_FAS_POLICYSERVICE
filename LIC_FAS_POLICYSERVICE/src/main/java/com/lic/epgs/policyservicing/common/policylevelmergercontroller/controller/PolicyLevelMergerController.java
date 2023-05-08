84c2c53ed1eab3b67f7e47d7a546f42e0bf8f0c7
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-08 19:05:30 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelmergercontroller/controller/PolicyLevelMergerController.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelmergercontroller/controller/PolicyLevelMergerController.java
@@ -1,32 +1,22 @@
 package com.lic.epgs.policyservicing.common.policylevelmergercontroller.controller;
 
-import com.lic.epgs.policyservicing.common.policylevelmergercontroller.dto.PolicyResponseDto;
-import com.lic.epgs.policyservicing.common.policylevelmergercontroller.dto.SearchParametersDto;
-import com.lic.epgs.policyservicing.common.policylevelmergercontroller.service.NewcitrieaSearchPolicyLevelMergerControllerService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
+import com.lic.epgs.policyservicing.common.policylevelmergercontroller.model.ExistingPolicy;
+import com.lic.epgs.policyservicing.common.policylevelmergercontroller.service.PolicyLevelMergerControllerService;
 
 @RestController
-@Api(value = "Policy Level Merger Controller")
 public class PolicyLevelMergerController {
-
-    @Autowired
-    NewcitrieaSearchPolicyLevelMergerControllerService newcitrieaSearchPolicyLevelMergerControllerService;
-
-    @ApiOperation(value = "Search for policy level", notes = "Search for policy level using citriea search.")
-    @PostMapping("/v1/search/policy-level")
-    public ResponseEntity<PolicyResponseDto> citrieaSearch(
-            @ApiParam(value = "Search parameters for citriea search.", required = true) @Valid @RequestBody SearchParametersDto searchParameters
-    ) {
-        return newcitrieaSearchPolicyLevelMergerControllerService.citrieaSearch(searchParameters);
-    }
+	
+	@Autowired
+	private PolicyLevelMergerControllerService policyLevelMergerControllerService;
+	
+	@GetMapping("/existingpolicies/{policyNumber}")
+	public ExistingPolicy getExistingPolicyByPolicyNumber(@PathVariable("policyNumber") String policyNumber) {
+		return policyLevelMergerControllerService.getExistingPolicyByPolicyNumber(policyNumber);
+	}
 
 }

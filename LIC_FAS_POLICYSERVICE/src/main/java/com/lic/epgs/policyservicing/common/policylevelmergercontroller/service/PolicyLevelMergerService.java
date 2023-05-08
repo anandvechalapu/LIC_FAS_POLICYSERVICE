c89b1ea7e7124b1111d49e1bbe3dae11409257b9
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-08 18:53:52 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelmergercontroller/service/PolicyLevelMergerService.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelmergercontroller/service/PolicyLevelMergerService.java
@@ -1,7 +1,8 @@
 package com.lic.epgs.policyservicing.common.policylevelmergercontroller.service;
 
-import com.lic.epgs.policyservicing.common.policylevelmergercontroller.dto.PolicyLevelMergerApiResponse;
-import com.lic.epgs.policyservicing.common.policylevelmergercontroller.dto.PolicyLevelMergerSearchDto;
+import com.lic.epgs.policyservicing.common.policylevelmergercontroller.dto.PolicyLevelMergerDto;
+import com.lic.epgs.policyservicing.common.policylevelmergercontroller.model.PolicyLevelMergerApiResponse;
+import com.lic.epgs.policyservicing.common.policylevelmergercontroller.model.PolicyLevelMergerTemp;
 import com.lic.epgs.policyservicing.common.policylevelmergercontroller.repository.PolicyLevelMergerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,14 +13,16 @@ public class PolicyLevelMergerService {
     @Autowired
     private PolicyLevelMergerRepository policyLevelMergerRepository;
 
-    public PolicyLevelMergerApiResponse getCriteriaSearchPolicy(String policyNumber,
-                                                               String product,
-                                                               String lineOfBusiness,
-                                                               String mergeStatus,
-                                                               String unitCode,
-                                                               String mphCode,
-                                                               String mphName) {
-        return policyLevelMergerRepository.getCriteriaSearchPolicy(policyNumber, product, lineOfBusiness, mergeStatus, unitCode, mphCode, mphName);
+    public void updateStatus(Boolean status, String modifiedBy, Long id) {
+        policyLevelMergerRepository.updateStatus(status, modifiedBy, id);
     }
 
+    public void saveAndUpdatePolicyLevelMerger(PolicyLevelMergerDto policyLevelMergerDto,
+                                               PolicyLevelMergerApiResponse response) {
+        policyLevelMergerRepository.saveAndUpdatePolicyLevelMerger(policyLevelMergerDto, response);
+    }
+
+    public boolean validPolicyNumberAndType(String policyNumber, String policyType) {
+        return policyLevelMergerRepository.validPolicyNumberAndType(policyNumber, policyType);
+    }
 }

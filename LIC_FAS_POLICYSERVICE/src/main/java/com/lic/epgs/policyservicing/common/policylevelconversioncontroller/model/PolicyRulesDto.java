9745a4afa0bdf895ba62a1ebd6dbe2543cedc22c
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-08 12:49:23 +0530

    initial commit

---
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelconversioncontroller/model/PolicyRulesDto.java
@@ -0,0 +1,19 @@
+public class PolicyRulesDto {
+    private String policyCode;
+    private String policyLevel;
+    private String benefitTypeCode;
+    private String benefitTypeDesc;
+    private String limitAmount;
+    private String limitType;
+
+    // Getters and setters
+    // ...
+}
+
+public class ResponseDto {
+    private String transactionStatus;
+    private String message;
+
+    // Getters and setters
+    // ...
+}

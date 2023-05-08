a5aa20e93a9d5ad77679cc84284983ab9c898d04
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-08 18:35:18 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelmergercontroller/repository/PolicyLevelMergerRepository.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelmergercontroller/repository/PolicyLevelMergerRepository.java
@@ -1,12 +1,12 @@
-package com.lic.epgs.policyservicing.common.policylevelmergercontroller.repository;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.lic.epgs.policyservicing.common.policylevelmergercontroller.model.PolicyLevelMergerApiResponse;
+
 import com.lic.epgs.policyservicing.common.policylevelmergercontroller.model.PolicyLevelMergerDto;
 import com.lic.epgs.policyservicing.common.policylevelmergercontroller.model.PolicyServiceNotesDto;
 
-public interface PolicyLevelMergerRepository extends JpaRepository<PolicyLevelMergerApiResponse, Long>{
-    PolicyLevelMergerApiResponse getMasterPolicyMergebyMergeId(Long mergeId);
-    PolicyLevelMergerDto findByMergeId(Long mergeId);
-    PolicyServiceNotesDto findByMergeIdAndIsActive(Long mergeId, Boolean isActive);
+public interface PolicyLevelMergerRepository extends JpaRepository<PolicyLevelMergerDto, Long> {
+	
+	public PolicyLevelMergerDto getMasterPolicyMergebyMergeId(Long mergeId);
+	
+	public List<PolicyServiceNotesDto> getNotesByMergeIdAndIsActive(Long mergeId, boolean isActive);
+	
 }

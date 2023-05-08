147e4aa32254301a44ad61a3f294b5b6b125d997
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-08 15:12:06 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policyleveldetailschangecontroller/model/PolicyServiceNotesTempEntity.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policyleveldetailschangecontroller/model/PolicyServiceNotesTempEntity.java
@@ -1,53 +1,32 @@
 package com.lic.epgs.policyservicing.common.policyleveldetailschangecontroller.model;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
-@Table(name="policyservicenotestemp")
+@Table(name = "policy_service_notes_temp")
 public class PolicyServiceNotesTempEntity {
 
-    @Id
-    @GeneratedValue(strategy= GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	private Integer policyId;
+	
+	private String notes;
 
-    @Column(name="policyid")
-    private Long policyId;
+	public Integer getPolicyId() {
+		return policyId;
+	}
 
-    @Column(name="notes")
-    private String notes;
+	public void setPolicyId(Integer policyId) {
+		this.policyId = policyId;
+	}
 
-    @Column(name="active")
-    private Boolean active;
+	public String getNotes() {
+		return notes;
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public void setNotes(String notes) {
+		this.notes = notes;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getPolicyId() {
-        return policyId;
-    }
-
-    public void setPolicyId(Long policyId) {
-        this.policyId = policyId;
-    }
-
-    public String getNotes() {
-        return notes;
-    }
-
-    public void setNotes(String notes) {
-        this.notes = notes;
-    }
-
-    public Boolean getActive() {
-        return active;
-    }
-
-    public void setActive(Boolean active) {
-        this.active = active;
-    }
 }

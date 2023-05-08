4f5a7bf60569d28c2ea0311e5e2af7d07ba7ee73
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-08 15:14:41 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policyleveldetailschangecontroller/controller/SaveNotesDetails_PolicyLevelDetailsChangeControllerController.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policyleveldetailschangecontroller/controller/SaveNotesDetails_PolicyLevelDetailsChangeControllerController.java
@@ -1,46 +1,29 @@
-package com.lic.epgs.policyservicing.common.policyleveldetailschangecontroller.repository;
+package com.lic.epgs.policyservicing.common.policyleveldetailschangecontroller.controller;
 
-import com.lic.epgs.policyservicing.common.policyleveldetailschangecontroller.model.PolicyServiceNotesTempEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
-
-import javax.transaction.Transactional;
-
-@Repository
-public interface SaveNotesDetails_PolicyLevelDetailsChangeControllerRepository extends JpaRepository<PolicyServiceNotesTempEntity, Long> {
-
-    @Transactional
-    @Modifying
-    @Query("UPDATE PolicyServiceNotesTempEntity t SET t.active = true where t.policyId = :policyId")
-    void setPolicyServiceNotesActive(@Param("policyId") Long policyId);
-
-}
-
-package com.lic.epgs.policyservicing.common.policyleveldetailschangecontroller.controller; 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.lic.epgs.policyservicing.common.policyleveldetailschangecontroller.model.PolicyServiceNotesTempEntity;
 import com.lic.epgs.policyservicing.common.policyleveldetailschangecontroller.service.SaveNotesDetails_PolicyLevelDetailsChangeControllerService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/policyLevelDetailsChangeController")
+@RequestMapping("/policyleveldetailschangecontroller")
 public class SaveNotesDetails_PolicyLevelDetailsChangeControllerController {
 
-    @Autowired
-    SaveNotesDetails_PolicyLevelDetailsChangeControllerService saveNotesDetails_PolicyLevelDetailsChangeControllerService;
-
-    @PostMapping("/saveNotesDetails")
-    public void saveNotesDetails(@RequestBody PolicyServiceNotesTempEntity policyServiceNotesTempEntity){
-        saveNotesDetails_PolicyLevelDetailsChangeControllerService.saveNotesDetails(policyServiceNotesTempEntity);
-    }
-
-    @PostMapping("/setPolicyServiceNotesActive")
-    public void setPolicyServiceNotesActive(@RequestParam Long policyId){
-        saveNotesDetails_PolicyLevelDetailsChangeControllerService.setPolicyServiceNotesActive(policyId);
-    }
-
+	@Autowired
+	private SaveNotesDetails_PolicyLevelDetailsChangeControllerService saveNotesDetails_PolicyLevelDetailsChangeControllerService;
+	
+	@PostMapping("/savenotes")
+	public PolicyServiceNotesTempEntity saveNotes(@RequestBody PolicyServiceNotesTempEntity policyServiceNotesTempEntity) {
+		return saveNotesDetails_PolicyLevelDetailsChangeControllerService.save(policyServiceNotesTempEntity);
+	}
+	
+	@GetMapping("/findnotes")
+	public PolicyServiceNotesTempEntity findByPolicyId(Integer policyId) {
+		return saveNotesDetails_PolicyLevelDetailsChangeControllerService.findByPolicyId(policyId);
+	}
 }

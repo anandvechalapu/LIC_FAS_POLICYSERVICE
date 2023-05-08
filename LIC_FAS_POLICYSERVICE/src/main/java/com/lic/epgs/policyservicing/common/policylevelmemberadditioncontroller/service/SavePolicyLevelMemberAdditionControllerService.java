d0783c5c49686dc3bec808db1618820d72058c11
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-08 17:11:09 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelmemberadditioncontroller/service/SavePolicyLevelMemberAdditionControllerService.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelmemberadditioncontroller/service/SavePolicyLevelMemberAdditionControllerService.java
@@ -1,62 +1,55 @@
 package com.lic.epgs.policyservicing.common.policylevelmemberadditioncontroller.service;
 
-import java.util.Map;
-
 import org.springframework.stereotype.Service;
+import com.lic.epgs.policyservicing.common.policylevelmemberadditioncontroller.repository.SavePolicyLevelMemberAdditionControllerRepository;
 
 @Service
 public class SavePolicyLevelMemberAdditionControllerService {
 
-  private final SavePolicyLevelMemberAdditionControllerRepository savePolicyLevelMemberAdditionControllerRepository;
+    private final SavePolicyLevelMemberAdditionControllerRepository savePolicyLevelMemberAdditionControllerRepository;
 
-  public SavePolicyLevelMemberAdditionControllerService(
-      SavePolicyLevelMemberAdditionControllerRepository savePolicyLevelMemberAdditionControllerRepository) {
-    this.savePolicyLevelMemberAdditionControllerRepository = savePolicyLevelMemberAdditionControllerRepository;
-  }
+    public SavePolicyLevelMemberAdditionControllerService(SavePolicyLevelMemberAdditionControllerRepository savePolicyLevelMemberAdditionControllerRepository) {
+        this.savePolicyLevelMemberAdditionControllerRepository = savePolicyLevelMemberAdditionControllerRepository;
+    }
 
-  // Save policy service member details
-  public void savePolicyServiceMemberDetails(Map<String, Object> policyServiceMemberDetails) {
-    savePolicyLevelMemberAdditionControllerRepository.savePolicyServiceMemberDetails(policyServiceMemberDetails);
-  }
+    public void savePolicyServiceMemberDetails(String personalDetails, String addressDetails, String bankDetails,
+                                               String nomineeDetails, String appointeeDetails) {
+        savePolicyLevelMemberAdditionControllerRepository.savePolicyServiceMemberDetails(personalDetails, addressDetails, bankDetails,
+                                                                                        nomineeDetails, appointeeDetails);
+    }
 
-  // Update policy service member details
-  public void updatePolicyServiceMemberDetails(Map<String, Object> policyServiceMemberDetails) {
-    savePolicyLevelMemberAdditionControllerRepository.updatePolicyServiceMemberDetails(policyServiceMemberDetails);
-  }
+    public void savePolicyServiceMemberDetailsAsDraft(String personalDetails, String addressDetails, String bankDetails,
+                                                      String nomineeDetails, String appointeeDetails, String memberAdditionID) {
+        savePolicyLevelMemberAdditionControllerRepository.savePolicyServiceMemberDetailsAsDraft(personalDetails, addressDetails, bankDetails,
+                                                                                               nomineeDetails, appointeeDetails, memberAdditionID);
+    }
 
-  // Save policy service member details as draft
-  public void savePolicyServiceMemberDetailsAsDraft(Map<String, Object> policyServiceMemberDetails) {
-    savePolicyLevelMemberAdditionControllerRepository.savePolicyServiceMemberDetailsAsDraft(policyServiceMemberDetails);
-  }
+    public void savePolicyServiceMemberDetailsAsActive(String personalDetails, String addressDetails, String bankDetails,
+                                                       String nomineeDetails, String appointeeDetails, String memberAdditionID) {
+        savePolicyLevelMemberAdditionControllerRepository.savePolicyServiceMemberDetailsAsActive(personalDetails, addressDetails, bankDetails,
+                                                                                                nomineeDetails, appointeeDetails, memberAdditionID);
+    }
 
-  // Save policy service member details as active
-  public void savePolicyServiceMemberDetailsAsActive(Map<String, Object> policyServiceMemberDetails) {
-    savePolicyLevelMemberAdditionControllerRepository.savePolicyServiceMemberDetailsAsActive(policyServiceMemberDetails);
-  }
+    public void modifyAndUpdatePolicyServiceMemberDetails(String personalDetails, String addressDetails, String bankDetails,
+                                                          String nomineeDetails, String appointeeDetails, String memberAdditionID) {
+        savePolicyLevelMemberAdditionControllerRepository.modifyAndUpdatePolicyServiceMemberDetails(personalDetails, addressDetails, bankDetails,
+                                                                                                   nomineeDetails, appointeeDetails, memberAdditionID);
+    }
 
-  // Set member addition status as Draft Status
-  public void setMemberAdditionStatusAsDraftStatus(String memberAdditionId) {
-    savePolicyLevelMemberAdditionControllerRepository.setMemberAdditionStatusAsDraftStatus(memberAdditionId);
-  }
+    public void saveUpdatedPolicyServiceMemberDetails() {
+        savePolicyLevelMemberAdditionControllerRepository.saveUpdatedPolicyServiceMemberDetails();
+    }
 
-  // Set member addition status as Active Status
-  public void setMemberAdditionStatusAsActiveStatus(String memberAdditionId) {
-    savePolicyLevelMemberAdditionControllerRepository.setMemberAdditionStatusAsActiveStatus(memberAdditionId);
-  }
+    public void viewUpdatedPolicyServiceMemberDetails() {
+        savePolicyLevelMemberAdditionControllerRepository.viewUpdatedPolicyServiceMemberDetails();
+    }
 
-  // Get updated policy service member details
-  public Map<String, Object> getUpdatedPolicyServiceMemberDetails(String memberAdditionId) {
-    return savePolicyLevelMemberAdditionControllerRepository.getUpdatedPolicyServiceMemberDetails(memberAdditionId);
-  }
+    public void handleErrorsAndExceptions() {
+        savePolicyLevelMemberAdditionControllerRepository.handleErrorsAndExceptions();
+    }
 
-  // Handle exceptions gracefully
-  public void handleExceptionsGracefully() {
-    savePolicyLevelMemberAdditionControllerRepository.handleExceptionsGracefully();
-  }
-
-  // Secure policy service member details
-  public void securePolicyServiceMemberDetails() {
-    savePolicyLevelMemberAdditionControllerRepository.securePolicyServiceMemberDetails();
-  }
+    public void securePolicyServiceMemberDetails() {
+        savePolicyLevelMemberAdditionControllerRepository.securePolicyServiceMemberDetails();
+    }
 
 }

a59e9a253809cbc2e3360929ed18d9a791c8fa96
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-08 18:36:00 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelmergercontroller/service/PolicyLevelMergerService.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelmergercontroller/service/PolicyLevelMergerService.java
@@ -1,19 +1,28 @@
 package com.lic.epgs.policyservicing.common.policylevelmergercontroller.service;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import com.lic.epgs.policyservicing.common.policylevelmergercontroller.model.PolicyLevelMergerTempEntity;
+import com.lic.epgs.policyservicing.common.policylevelmergercontroller.model.PolicyLevelMergerApiResponse;
+import com.lic.epgs.policyservicing.common.policylevelmergercontroller.model.PolicyLevelMergerDto;
+import com.lic.epgs.policyservicing.common.policylevelmergercontroller.model.PolicyServiceNotesDto;
 import com.lic.epgs.policyservicing.common.policylevelmergercontroller.repository.PolicyLevelMergerRepository;
 
 @Service
 public class PolicyLevelMergerService {
-    
+
     @Autowired
     PolicyLevelMergerRepository policyLevelMergerRepository;
-    
-    public PolicyLevelMergerTempEntity getPolicyMergebyMergeId_PolicyLevelMergerController(Long mergeId) {
-        return policyLevelMergerRepository.getPolicyMergebyMergeId_PolicyLevelMergerController(mergeId);
+
+    public PolicyLevelMergerApiResponse getMasterPolicyMergebyMergeId(Long mergeId){
+        return policyLevelMergerRepository.getMasterPolicyMergebyMergeId(mergeId);
     }
-    
+
+    public PolicyLevelMergerDto findByMergeId(Long mergeId){
+        return policyLevelMergerRepository.findByMergeId(mergeId);
+    }
+
+    public List<PolicyServiceNotesDto> findByMergeIdAndIsActive(Long mergeId, Boolean isActive){
+        return policyLevelMergerRepository.findByMergeIdAndIsActive(mergeId, isActive);
+    }
 }

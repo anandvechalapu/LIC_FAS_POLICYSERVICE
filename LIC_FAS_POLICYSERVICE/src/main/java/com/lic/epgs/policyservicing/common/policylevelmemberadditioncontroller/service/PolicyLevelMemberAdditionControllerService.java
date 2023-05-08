27a520054e12f3e092c4d326bfb1a85652c48bb8
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-08 17:11:30 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelmemberadditioncontroller/service/PolicyLevelMemberAdditionControllerService.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelmemberadditioncontroller/service/PolicyLevelMemberAdditionControllerService.java
@@ -1,32 +1,32 @@
 package com.lic.epgs.policyservicing.common.policylevelmemberadditioncontroller.service;
 
-import java.util.Date;
-
-import com.lic.epgs.policyservicing.common.policylevelmemberadditioncontroller.model.CommonResponseDto;
-import com.lic.epgs.policyservicing.common.policylevelmemberadditioncontroller.repository.PolicyLevelMemberAdditionControllerRepository;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.lic.epgs.policyservicing.common.policylevelmemberadditioncontroller.entity.PolicyServiceMemberAdditionEntity;
+import com.lic.epgs.policyservicing.common.policylevelmemberadditioncontroller.repository.SendToMaker_PolicyLevelMemberAdditionController;
 
 @Service
 public class PolicyLevelMemberAdditionControllerService {
 
-    @Autowired
-    private PolicyLevelMemberAdditionControllerRepository controllerRepository;
+	@Autowired
+	private SendToMaker_PolicyLevelMemberAdditionController sendToMaker_PolicyLevelMemberAdditionController;
 
-    /**
-    * Saves a note related to a Policy Service Member.
-    *
-    * @param memberAdditionId - A unique identifier for the member addition.
-    * @param note - A string containing the text of the note.
-    * @param createdBy - A string containing the username of the user who created the note.
-    * @param createdOn - A date and time indicating when the note was created.
-    *
-    * @return A CommonResponseDto with a SUCCESS status and an empty response data if the note was saved successfully.
-    *         A CommonResponseDto with a FAIL status and a transaction message indicating that the memberAdditionId
-    *         is invalid if the memberAdditionId is not associated with a Policy Service Member.
-    */
-    public CommonResponseDto saveNote(String memberAdditionId, String note, String createdBy, Date createdOn) {
-        return controllerRepository.saveNote(memberAdditionId, note, createdBy, createdOn);
-    }
+	@Transactional
+	public void sendToMakerOrChecker(String memberAdditionStatus, String modifiedBy, String modifiedOn, Long memberAdditionId,
+			String unitCode) {
+		sendToMaker_PolicyLevelMemberAdditionController.sendToMakerOrChecker(memberAdditionStatus, modifiedBy, modifiedOn, memberAdditionId, unitCode);
+	}
+	
+	public List<PolicyServiceMemberAdditionEntity> findAll(){
+		return sendToMaker_PolicyLevelMemberAdditionController.findAll();
+	}
+	
+	public PolicyServiceMemberAdditionEntity findById(Long id) {
+		return sendToMaker_PolicyLevelMemberAdditionController.findById(id).orElse(null);
+	}
+
 }

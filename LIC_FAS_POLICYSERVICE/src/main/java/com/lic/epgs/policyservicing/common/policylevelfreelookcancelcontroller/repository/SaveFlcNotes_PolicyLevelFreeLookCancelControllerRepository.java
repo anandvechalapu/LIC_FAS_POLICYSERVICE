7446f2c74844e7ac9eab8ad9101f9c985f110cfc
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-08 16:34:15 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelfreelookcancelcontroller/repository/SaveFlcNotes_PolicyLevelFreeLookCancelControllerRepository.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelfreelookcancelcontroller/repository/SaveFlcNotes_PolicyLevelFreeLookCancelControllerRepository.java
@@ -1,6 +1,21 @@
-@Repository
+package com.lic.epgs.policyservicing.common.policylevelfreelookcancelcontroller.repository;
+
+import com.lic.epgs.policyservicing.common.policylevelfreelookcancelcontroller.dto.FreeLookCancellationNotesDto;
+import com.lic.epgs.policyservicing.common.policylevelfreelookcancelcontroller.dto.FreeLookCancellationResponseDto;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public interface SaveFlcNotes_PolicyLevelFreeLookCancelControllerRepository {
 
+    Logger logger = LoggerFactory.getLogger(SaveFlcNotes_PolicyLevelFreeLookCancelControllerRepository.class);
+
     FreeLookCancellationResponseDto saveFlcNotes(Long freeLookId, Long policyId, Long serviceId, String notesText);
+
+    default void validateInputs(Long freeLookId, Long policyId, Long serviceId, String notesText){
+        if(freeLookId == null || policyId == null || serviceId == null || notesText == null){
+            throw new IllegalArgumentException("Missing one of the required parameters");
+        }
+    }
 
 }

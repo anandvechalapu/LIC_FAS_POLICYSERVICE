40c32b69c25f0865819625a085c7e40648c932cd
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-08 19:00:04 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelmergercontroller/repository/PolicyLevelMergerControllerRepository.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelmergercontroller/repository/PolicyLevelMergerControllerRepository.java
@@ -1,13 +1,12 @@
-package com.lic.epgs.policyservicing.common.policylevelmergercontroller.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.lic.epgs.policyservicing.common.policylevelmergercontroller.model.ExistingPolicy;
-
 @Repository
-public interface PolicyLevelMergerControllerRepository extends JpaRepository<ExistingPolicy, Long> {
-	
-	ExistingPolicy getExistingPolicyByPolicyNumber(String policyNumber);
-	
+public interface PolicyLevelMergerControllerRepository {
+ 
+    /**
+     * Get Document List for the given merge ID
+     *
+     * @param mergeId
+     * @return ResponseEntity containing a list of PolicyServiceDocumentDto objects associated with the given merge ID
+     */
+    ResponseEntity<List<PolicyServiceDocumentDto>> getDocumentList(String mergeId);
+ 
 }

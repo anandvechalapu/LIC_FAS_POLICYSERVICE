77c178d4c3127d68622b6ade74187f402f254a6e
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-08 19:03:20 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelmergercontroller/service/PolicyLevelMergerControllerService.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelmergercontroller/service/PolicyLevelMergerControllerService.java
@@ -1,23 +1,19 @@
 package com.lic.epgs.policyservicing.common.policylevelmergercontroller.service;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.lic.epgs.policyservicing.common.policylevelmergercontroller.model.ExistingPolicy;
 import com.lic.epgs.policyservicing.common.policylevelmergercontroller.repository.PolicyLevelMergerControllerRepository;
-import com.lic.epgs.policyservicing.common.policylevelmergercontroller.repository.PolicyLevelMergerSearchDto;
 
 @Service
 public class PolicyLevelMergerControllerService {
-
-    @Autowired
-    private PolicyLevelMergerControllerRepository policyLevelMergerControllerRepository;
-
-    public List<PolicyLevelMergerSearchDto> getCriteriaSearchPolicy(String policyNumber, String product,
-            String lineOfBusiness, String mergeStatus, String unitCode, String mphCode, String mphName) {
-        return policyLevelMergerControllerRepository.getCriteriaSearchPolicy(policyNumber, product, lineOfBusiness,
-                mergeStatus, unitCode, mphCode, mphName);
-    }
+	
+	@Autowired
+	private PolicyLevelMergerControllerRepository policyLevelMergerControllerRepository;
+	
+	public ExistingPolicy getExistingPolicyByPolicyNumber(String policyNumber) {
+		return policyLevelMergerControllerRepository.getExistingPolicyByPolicyNumber(policyNumber);
+	}
 
 }

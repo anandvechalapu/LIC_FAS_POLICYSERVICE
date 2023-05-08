43d5128c9b0d4522a64049aa4e28b7d10ab04cce
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-08 15:13:25 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policyleveldetailschangecontroller/service/SaveNotesDetails_PolicyLevelDetailsChangeControllerService.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policyleveldetailschangecontroller/service/SaveNotesDetails_PolicyLevelDetailsChangeControllerService.java
@@ -1,22 +1,22 @@
 package com.lic.epgs.policyservicing.common.policyleveldetailschangecontroller.service;
 
-import com.lic.epgs.policyservicing.common.policyleveldetailschangecontroller.model.PolicyServiceNotesTempEntity;
-import com.lic.epgs.policyservicing.common.policyleveldetailschangecontroller.repository.SaveNotesDetails_PolicyLevelDetailsChangeControllerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.lic.epgs.policyservicing.common.policyleveldetailschangecontroller.model.PolicyServiceNotesTempEntity;
+import com.lic.epgs.policyservicing.common.policyleveldetailschangecontroller.repository.SaveNotesDetails_PolicyLevelDetailsChangeControllerRepo;
 
 @Service
 public class SaveNotesDetails_PolicyLevelDetailsChangeControllerService {
 
-    @Autowired
-    SaveNotesDetails_PolicyLevelDetailsChangeControllerRepository saveNotesDetails_PolicyLevelDetailsChangeControllerRepository;
-
-    public void saveNotesDetails(PolicyServiceNotesTempEntity policyServiceNotesTempEntity) {
-        saveNotesDetails_PolicyLevelDetailsChangeControllerRepository.save(policyServiceNotesTempEntity);
-    }
-
-    public void setPolicyServiceNotesActive(Long policyId) {
-        saveNotesDetails_PolicyLevelDetailsChangeControllerRepository.setPolicyServiceNotesActive(policyId);
-    }
-
+	@Autowired
+	private SaveNotesDetails_PolicyLevelDetailsChangeControllerRepo  saveNotesDetails_PolicyLevelDetailsChangeControllerRepo;
+	
+	public PolicyServiceNotesTempEntity save(PolicyServiceNotesTempEntity policyServiceNotesTempEntity) {
+		return saveNotesDetails_PolicyLevelDetailsChangeControllerRepo.save(policyServiceNotesTempEntity);
+	}
+	
+	public PolicyServiceNotesTempEntity findByPolicyId(Integer policyId) {
+		return saveNotesDetails_PolicyLevelDetailsChangeControllerRepo.findByPolicyId(policyId);
+	}
 }

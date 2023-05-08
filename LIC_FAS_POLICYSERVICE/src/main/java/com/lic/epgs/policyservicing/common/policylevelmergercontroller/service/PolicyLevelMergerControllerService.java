72382f72b7df75c74b18103b48ad21174bde2960
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-08 18:49:52 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelmergercontroller/service/PolicyLevelMergerControllerService.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelmergercontroller/service/PolicyLevelMergerControllerService.java
@@ -1,25 +1,23 @@
 package com.lic.epgs.policyservicing.common.policylevelmergercontroller.service;
 
-import com.lic.epgs.policyservicing.common.policylevelmergercontroller.dto.PolicyLevelMergerApiResponse;
-import com.lic.epgs.policyservicing.common.policylevelmergercontroller.dto.PolicyLevelMergerSearchDto;
-import com.lic.epgs.policyservicing.common.policylevelmergercontroller.repository.PolicyLevelMergerControllerRepository;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import com.lic.epgs.policyservicing.common.policylevelmergercontroller.repository.PolicyLevelMergerControllerRepository;
+import com.lic.epgs.policyservicing.common.policylevelmergercontroller.repository.PolicyLevelMergerSearchDto;
 
 @Service
 public class PolicyLevelMergerControllerService {
 
     @Autowired
-    PolicyLevelMergerControllerRepository policyLevelMergerControllerRepository;
+    private PolicyLevelMergerControllerRepository policyLevelMergerControllerRepository;
 
-    public List<PolicyLevelMergerSearchDto> getCriteriaSearchPolicy(String policyNumber, String product, String lineOfBusiness, String mergeStatus, String unitCode, String mphCode, String mphName){
-        return policyLevelMergerControllerRepository.getCriteriaSearchPolicy(policyNumber, product, lineOfBusiness, mergeStatus, unitCode, mphCode, mphName);
-    }
-
-    public PolicyLevelMergerApiResponse save(PolicyLevelMergerApiResponse policyLevelMergerApiResponse){
-        return policyLevelMergerControllerRepository.save(policyLevelMergerApiResponse);
+    public List<PolicyLevelMergerSearchDto> getCriteriaSearchPolicy(String policyNumber, String product,
+            String lineOfBusiness, String mergeStatus, String unitCode, String mphCode, String mphName) {
+        return policyLevelMergerControllerRepository.getCriteriaSearchPolicy(policyNumber, product, lineOfBusiness,
+                mergeStatus, unitCode, mphCode, mphName);
     }
 
 }

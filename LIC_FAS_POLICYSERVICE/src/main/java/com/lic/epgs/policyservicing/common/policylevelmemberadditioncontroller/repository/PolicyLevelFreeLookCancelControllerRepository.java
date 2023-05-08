cf564e98f1b507d17acec31b40690d74bfe0f6f7
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-08 17:49:23 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelmemberadditioncontroller/repository/PolicyLevelFreeLookCancelControllerRepository.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelmemberadditioncontroller/repository/PolicyLevelFreeLookCancelControllerRepository.java
@@ -1,14 +1,4 @@
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.lic.epgs.policyservicing.common.policylevelmemberadditioncontroller.dto.PolicyServiceMemberAdditionDto;
-import com.lic.epgs.policyservicing.common.policylevelmemberadditioncontroller.dto.CommonResponseDto;
-import com.lic.epgs.policyservicing.common.policylevelmemberadditioncontroller.entity.PolicyServiceMemberEntity;
-import com.lic.epgs.policyservicing.common.policylevelmemberadditioncontroller.dto.PolicyServiceMbrDto;
-
-public interface PolicyLevelFreeLookCancelControllerRepository extends JpaRepository<PolicyServiceMemberAdditionDto, CommonResponseDto<Object>> {
-
-    CommonResponseDto<Object> getAllMembers(Long memberAdditionId);
-    
-    List<PolicyServiceMbrDto> findByMemberAdditionId(Long memberAdditionId);
-    
-    List<PolicyServiceMemberEntity> findByActiveAddressAndActiveNominee(Boolean activeAddress, Boolean activeNominee);
+@Repository
+public interface PolicyLevelFreeLookCancelControllerRepository {
+    CommonResponseDto<Object> getAllMembers(PolicyServiceMemberAdditionDto policyServiceMemberAdditionDto);
 }

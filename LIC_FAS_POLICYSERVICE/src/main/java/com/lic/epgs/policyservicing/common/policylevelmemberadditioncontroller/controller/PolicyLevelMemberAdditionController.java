c7f91793798c8d2994c249844b3a0b9be40e9202
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-08 17:20:14 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelmemberadditioncontroller/controller/PolicyLevelMemberAdditionController.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelmemberadditioncontroller/controller/PolicyLevelMemberAdditionController.java
@@ -3,39 +3,49 @@ package com.lic.epgs.policyservicing.common.policylevelmemberadditioncontroller.
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.lic.epgs.policyservicing.common.policylevelmemberadditioncontroller.model.PolicyServiceMemberAdditionModel;
+import com.lic.epgs.policyservicing.common.policylevelmemberadditioncontroller.dto.CommonResponseDto;
+import com.lic.epgs.policyservicing.common.policylevelmemberadditioncontroller.dto.PolicyServiceMemberAdditionDto;
+import com.lic.epgs.policyservicing.common.policylevelmemberadditioncontroller.dto.PolicyServiceMbrDto;
 import com.lic.epgs.policyservicing.common.policylevelmemberadditioncontroller.service.PolicyLevelMemberAdditionControllerService;
 
-@CrossOrigin(origins = "*")
 @RestController
-@RequestMapping("/memberaddition/policyService")
+@RequestMapping("/policyLevelMemberAdditionController")
 public class PolicyLevelMemberAdditionController {
-	
+
 	@Autowired
-	private PolicyLevelMemberAdditionControllerService policyLevelMemberAdditionControllerService;
-	
-	@PostMapping("/sendToMakerOrChecker")
-	public ResponseEntity<PolicyServiceMemberAdditionModel> sendToMakerOrChecker(@RequestBody PolicyServiceMemberAdditionModel policyServiceMemberAdditionModel){
-		return ResponseEntity.ok(policyLevelMemberAdditionControllerService.sendToMakerOrChecker(policyServiceMemberAdditionModel.getMemberAdditionStatus(), policyServiceMemberAdditionModel.getModifiedBy(), policyServiceMemberAdditionModel.getModifiedOn(), policyServiceMemberAdditionModel.getMemberAdditionId(), policyServiceMemberAdditionModel.getUnitCode()));
+	PolicyLevelMemberAdditionControllerService policyLevelMemberAdditionControllerService;
+
+	@GetMapping("/getall")
+	public ResponseEntity<CommonResponseDto<Object>> getAllPolicyLevelMemberAdditionController(
+			PolicyServiceMemberAdditionDto policyServiceMemberAdditionDto) {
+		CommonResponseDto<Object> response = policyLevelMemberAdditionControllerService
+				.getAllPolicyLevelMemberAdditionController(policyServiceMemberAdditionDto);
+		return new ResponseEntity<>(response, HttpStatus.OK);
 	}
-	
-	@GetMapping("/getAll")
-	public ResponseEntity<List<PolicyServiceMemberAdditionModel>> getAll(){
-		return ResponseEntity.ok(policyLevelMemberAdditionControllerService.findAll());
+
+	@GetMapping("/getmemberdetails")
+	public ResponseEntity<List<PolicyServiceMbrDto>> getMemberDetails() {
+		List<PolicyServiceMbrDto> response = policyLevelMemberAdditionControllerService.getMemberDetails();
+		return new ResponseEntity<>(response, HttpStatus.OK);
 	}
-	
-	@GetMapping("/getById/{id}")
-	public ResponseEntity<PolicyServiceMemberAdditionModel> getById(@PathVariable Long id){
-		return ResponseEntity.ok(policyLevelMemberAdditionControllerService.findById(id));
+
+	@GetMapping("/getmemberadditiondetails")
+	public ResponseEntity<List<PolicyServiceMemberAdditionDto>> getMemberAdditionDetails() {
+		List<PolicyServiceMemberAdditionDto> response = policyLevelMemberAdditionControllerService
+				.getMemberAdditionDetails();
+		return new ResponseEntity<>(response, HttpStatus.OK);
+	}
+
+	@GetMapping("/getnotes")
+	public ResponseEntity<List<String>> getNotes() {
+		List<String> response = policyLevelMemberAdditionControllerService.getNotes();
+		return new ResponseEntity<>(response, HttpStatus.OK);
 	}
 
 }

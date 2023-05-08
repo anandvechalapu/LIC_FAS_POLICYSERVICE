b4a142dfa3b1c877703037f15e1f631aeda6e1c5
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-08 18:40:42 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelmergercontroller/service/PolicyLevelMergerControllerService.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelmergercontroller/service/PolicyLevelMergerControllerService.java
@@ -1,27 +1,32 @@
 package com.lic.epgs.policyservicing.common.policylevelmergercontroller.service;
 
-import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.lic.epgs.policyservicing.common.policylevelmergercontroller.model.PolicyLevelMergerApiResponse;
-import com.lic.epgs.policyservicing.common.policylevelmergercontroller.model.PolicyServiceNotesDto;
+import com.lic.epgs.policyservicing.common.policylevelmergercontroller.model.PolicyLevelMergerTempEntity;
 import com.lic.epgs.policyservicing.common.policylevelmergercontroller.repository.PolicyLevelMergerControllerRepository;
 
 @Service
 public class PolicyLevelMergerControllerService {
 
-  @Autowired
-  private PolicyLevelMergerControllerRepository policyLevelMergerControllerRepository;
+	@Autowired
+	private PolicyLevelMergerControllerRepository policyLevelMergerControllerRepository;
 
-  public PolicyLevelMergerApiResponse getNoteList(long mergeId) {
-    List<PolicyServiceNotesDto> notes = policyLevelMergerControllerRepository.getNoteList(mergeId);
-    PolicyLevelMergerApiResponse response = new PolicyLevelMergerApiResponse();
-    response.setNotes(notes);
-    response.setStatus("SUCCESS");
-    response.setMessage("Notes retrieved successfully.");
-    return response;
-  }
+	public PolicyLevelMergerApiResponse getPolicyMergebyMergeId(Long mergeId) {
+		Optional<PolicyLevelMergerTempEntity> policyLevelMergerTempEntity = policyLevelMergerControllerRepository.findById(mergeId);
+		if (policyLevelMergerTempEntity.isPresent()) {
+			PolicyLevelMergerApiResponse policyLevelMergerApiResponse = new PolicyLevelMergerApiResponse();
+			PolicyLevelMergerTempEntity policyLevelMergerTempEntityData = policyLevelMergerTempEntity.get();
+			policyLevelMergerApiResponse.setMergeId(policyLevelMergerTempEntityData.getMergeId());
+			policyLevelMergerApiResponse.setResponseCode(policyLevelMergerTempEntityData.getResponseCode());
+			policyLevelMergerApiResponse.setResponseMessage(policyLevelMergerTempEntityData.getResponseMessage());
 
+			return policyLevelMergerApiResponse;
+		} else {
+			return null;
+		}
+	}
 }

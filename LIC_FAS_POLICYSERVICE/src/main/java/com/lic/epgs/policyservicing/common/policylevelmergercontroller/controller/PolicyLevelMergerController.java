bfa2bb53494235481b41686b57a997a1b28c4bb3
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-08 19:06:27 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelmergercontroller/controller/PolicyLevelMergerController.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelmergercontroller/controller/PolicyLevelMergerController.java
@@ -1,22 +1,24 @@
 package com.lic.epgs.policyservicing.common.policylevelmergercontroller.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.lic.epgs.policyservicing.common.policylevelmergercontroller.model.ExistingPolicy;
+import com.lic.epgs.policyservicing.common.policylevelmergercontroller.dto.PolicyServiceDocumentDto;
 import com.lic.epgs.policyservicing.common.policylevelmergercontroller.service.PolicyLevelMergerControllerService;
 
 @RestController
 public class PolicyLevelMergerController {
-	
-	@Autowired
-	private PolicyLevelMergerControllerService policyLevelMergerControllerService;
-	
-	@GetMapping("/existingpolicies/{policyNumber}")
-	public ExistingPolicy getExistingPolicyByPolicyNumber(@PathVariable("policyNumber") String policyNumber) {
-		return policyLevelMergerControllerService.getExistingPolicyByPolicyNumber(policyNumber);
-	}
-
+ 
+    @Autowired
+    private PolicyLevelMergerControllerService service;
+ 
+    @GetMapping("/document/{mergeId}")
+    public ResponseEntity<List<PolicyServiceDocumentDto>> getDocumentList(@PathVariable String mergeId) {
+        return service.getDocumentList(mergeId);
+    }
 }

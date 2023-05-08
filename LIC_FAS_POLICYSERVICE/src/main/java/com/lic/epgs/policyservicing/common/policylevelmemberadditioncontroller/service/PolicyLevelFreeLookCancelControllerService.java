b72fcda9482fb9d639223c1c35c29ff4a4c29eef
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-08 17:54:32 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelmemberadditioncontroller/service/PolicyLevelFreeLookCancelControllerService.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelmemberadditioncontroller/service/PolicyLevelFreeLookCancelControllerService.java
@@ -1,15 +1,11 @@
 package com.lic.epgs.policyservicing.common.policylevelmemberadditioncontroller.service;
 
-import java.util.List;
+import com.lic.epgs.policyservicing.common.policylevelmemberadditioncontroller.dto.CommonResponseDto;
+import com.lic.epgs.policyservicing.common.policylevelmemberadditioncontroller.dto.PolicyServiceMemberAdditionDto;
+import com.lic.epgs.policyservicing.common.policylevelmemberadditioncontroller.repository.PolicyLevelFreeLookCancelControllerRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import com.lic.epgs.policyservicing.common.policylevelmemberadditioncontroller.dto.PolicyServiceMemberAdditionDto;
-import com.lic.epgs.policyservicing.common.policylevelmemberadditioncontroller.dto.CommonResponseDto;
-import com.lic.epgs.policyservicing.common.policylevelmemberadditioncontroller.entity.PolicyServiceMemberEntity;
-import com.lic.epgs.policyservicing.common.policylevelmemberadditioncontroller.dto.PolicyServiceMbrDto;
-import com.lic.epgs.policyservicing.common.policylevelmemberadditioncontroller.repository.PolicyLevelFreeLookCancelControllerRepository;
 
 @Service
 public class PolicyLevelFreeLookCancelControllerService {
@@ -17,16 +13,8 @@ public class PolicyLevelFreeLookCancelControllerService {
     @Autowired
     private PolicyLevelFreeLookCancelControllerRepository policyLevelFreeLookCancelControllerRepository;
 
-    public CommonResponseDto<Object> getAllMembers(Long memberAdditionId){
-        return policyLevelFreeLookCancelControllerRepository.getAllMembers(memberAdditionId);
+    public CommonResponseDto<Object> getAllMembers(PolicyServiceMemberAdditionDto policyServiceMemberAdditionDto)
+    {
+        return policyLevelFreeLookCancelControllerRepository.getAllMembers(policyServiceMemberAdditionDto);
     }
-    
-    public List<PolicyServiceMbrDto> findByMemberAdditionId(Long memberAdditionId){
-        return policyLevelFreeLookCancelControllerRepository.findByMemberAdditionId(memberAdditionId);
-    }
-    
-    public List<PolicyServiceMemberEntity> findByActiveAddressAndActiveNominee(Boolean activeAddress, Boolean activeNominee){
-        return policyLevelFreeLookCancelControllerRepository.findByActiveAddressAndActiveNominee(activeAddress, activeNominee);
-    }
-
 }

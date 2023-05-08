a7187983659632128287c1eefbadd48a38342a4f
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-08 18:04:13 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelmergercontroller/repository/PolicyLevelMergerControllerRepository.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelmergercontroller/repository/PolicyLevelMergerControllerRepository.java
@@ -1,21 +1,15 @@
 @Repository
-public interface PolicyLevelMergerControllerRepository { 
- 
-    /**
-     * Saves or updates the policy level merger details.
-     *
-     * @param policyLevelMergerDto the policy level merger details
-     * @return the PolicyLevelMergerApiResponse object
-     */
-    PolicyLevelMergerApiResponse saveAndUpdatePolicyLevelMerger(PolicyLevelMergerDto policyLevelMergerDto);
+public interface PolicyLevelMergerControllerRepository extends JpaRepository<PolicyServiceDocumentDto, Long> {
 
-    /**
-     * Validates the policy numbers and types.
-     *
-     * @param policyNumber the policy number
-     * @param policyType the policy type
-     * @return true if valid, false otherwise
-     */
-    boolean validPolicyNumberAndType(String policyNumber, String policyType);
+	Optional<PolicyServiceDocumentDto> findByDocumentId(Long documentId);
+	
+	@Modifying
+	@Query("update PolicyServiceDocumentDto p set p.isActive = :isActive where p.documentId = :documentId")
+	void updateIsActive(@Param("documentId") Long documentId, @Param("isActive") Boolean isActive);
+	
+	@Transactional
+	@Modifying
+	@Query("delete from PolicyServiceDocumentDto p where p.documentId = :documentId")
+	void deleteByDocumentId(@Param("documentId") Long documentId);
 
 }

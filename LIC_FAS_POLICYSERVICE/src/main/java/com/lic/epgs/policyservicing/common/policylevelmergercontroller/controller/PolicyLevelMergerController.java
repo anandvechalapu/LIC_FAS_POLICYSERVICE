b638ba36a1d12b7a7d601f05b99348b33a45171a
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-08 18:23:57 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelmergercontroller/controller/PolicyLevelMergerController.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelmergercontroller/controller/PolicyLevelMergerController.java
@@ -1,61 +1,34 @@
-package com.lic.epgs.policyservicing.common.policylevelmergercontroller.controller;
+@Repository
+public interface PolicyLevelMergerControllerRepository extends JpaRepository<PolicyServiceDocumentDto, Long> {
 
-import java.util.List;
+    boolean existsByDocumentName(String documentName);
 
-import javax.validation.Valid;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.lic.epgs.policyservicing.common.policylevelmergercontroller.model.MemberMasterEntity;
-import com.lic.epgs.policyservicing.common.policylevelmergercontroller.model.PolicyLevelMergerApiResponse;
-import com.lic.epgs.policyservicing.common.policylevelmergercontroller.model.PolicyLevelMergerTempEntity;
-import com.lic.epgs.policyservicing.common.policylevelmergercontroller.model.PolicyMasterEntity;
-import com.lic.epgs.policyservicing.common.policylevelmergercontroller.model.PolicyServiceDocumentEntity;
-import com.lic.epgs.policyservicing.common.policylevelmergercontroller.model.PolicyServiceNotesEntity;
-import com.lic.epgs.policyservicing.common.policylevelmergercontroller.service.ApprovedAndRejectPolicyLevelMerger_PolicyLevelMergerControllerService;
+    void setActive(String documentName);
+}
 
 @RestController
+@RequestMapping("/policyLevelMergerController")
 public class PolicyLevelMergerController {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(PolicyLevelMergerController.class);
-    
     @Autowired
-    ApprovedAndRejectPolicyLevelMerger_PolicyLevelMergerControllerService approvedAndRejectPolicyLevelMerger_PolicyLevelMergerControllerService;
-    
-    @GetMapping("/getPolicyLevelMerger")
-    public List<PolicyLevelMergerTempEntity> findByMergeIdAndMergeStatus(@RequestParam Long mergeId, 
-                                                                        @RequestParam Integer mergeStatus) {
-        LOGGER.info("getPolicyLevelMerger with mergeId : {} and mergeStatus : {}", mergeId, mergeStatus);
-        return approvedAndRejectPolicyLevelMerger_PolicyLevelMergerControllerService.findByMergeIdAndMergeStatus(mergeId, mergeStatus);
+    private PolicyLevelMergerControllerService policyLevelMergerControllerService;
+
+    @PostMapping(value = "/save")
+    public ResponseEntity<Object> save(@RequestBody PolicyServiceDocumentDto policyServiceDocumentDto) {
+        PolicyServiceDocumentDto savedDocument = policyLevelMergerControllerService.save(policyServiceDocumentDto);
+        return new ResponseEntity<>(savedDocument, HttpStatus.CREATED);
     }
-    
-    @GetMapping("/getPolicyMaster")
-    public List<PolicyMasterEntity> findByMergeIdAndMergeStatusInPolicyMaster(@RequestParam Long mergeId, 
-                                                                             @RequestParam Integer mergeStatus) {
-        LOGGER.info("getPolicyMaster with mergeId : {} and mergeStatus : {}", mergeId, mergeStatus);
-        return approvedAndRejectPolicyLevelMerger_PolicyLevelMergerControllerService.findByMergeIdAndMergeStatus(mergeId, mergeStatus);
+
+    @GetMapping(value = "/existsByDocumentName")
+    public ResponseEntity<Object> existsByDocumentName(@RequestParam String documentName) {
+        Boolean isExists = policyLevelMergerControllerService.existsByDocumentName(documentName);
+        return new ResponseEntity<>(isExists, HttpStatus.OK);
     }
-    
-    @GetMapping("/getMemberMaster")
-    public List<MemberMasterEntity> findByMergeIdAndMergeStatusInMemberMaster(@RequestParam Long mergeId, 
-                                                                             @RequestParam Integer mergeStatus) {
-        LOGGER.info("getMemberMaster with mergeId : {} and mergeStatus : {}", mergeId, mergeStatus);
-        return approvedAndRejectPolicyLevelMerger_PolicyLevelMergerControllerService.findByMergeIdAndMergeStatus(mergeId, mergeStatus);
+
+    @PutMapping(value = "/setActive")
+    public ResponseEntity<Object> setActive(@RequestParam String documentName) {
+        policyLevelMergerControllerService.setActive(documentName);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
-    
-    @PutMapping("/savePolicyMasterEntity")
-    public void savePolicyMasterEntity(@RequestBody @Valid PolicyMasterEntity policyMasterEntity) {
-        LOGGER.info("savePolicyMasterEntity : {}", policyMasterEntity);
-        approvedAndRejectPolicyLevelMerger_PolicyLevelMergerControllerService.savePolicyMasterEntity(policyMasterEntity);
-    }
-    
-    @PutMapping("/savePolicyServiceDocumentEntity")
-    public void savePolicyServiceDocumentEntity(@RequestBody @Valid PolicyServiceDocumentEntity policyServiceDocumentEntity) {
-        LOGGER.info("savePolicyServiceDocumentEntity : {}", policyServiceDocumentEntity
+
+}

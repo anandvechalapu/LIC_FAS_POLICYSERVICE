95878b73453cd9be0c54c50f12454d0c2353f5cb
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-08 13:17:54 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelconversioncontroller/repository/PolicyLevelConversionControllerRepository.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelconversioncontroller/repository/PolicyLevelConversionControllerRepository.java
@@ -1,15 +1,9 @@
-package com.lic.epgs.policyservicing.common.policylevelconversioncontroller.repository;
-
-import com.lic.epgs.policyservicing.common.policylevelconversioncontroller.dto.CommonResponseDto;
-import com.lic.epgs.policyservicing.common.policylevelconversioncontroller.dto.PolicyLevelConversionDto;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface PolicyLevelConversionControllerRepository extends JpaRepository<PolicyLevelConversionDto, String> {
+import com.lic.epgs.policyservicing.common.policylevelconversioncontroller.model.Document;
 
-    @Query("SELECT p FROM PolicyLevelConversionDto p WHERE p.prevPolicyNo = :prevPolicyNo AND p.role = :role")
-    CommonResponseDto getPolicyConversionDetailsByPolicyNo(String prevPolicyNo, String role);
+public interface PolicyLevelConversionControllerRepository extends JpaRepository<Document, Long>{
+
+    public List<Document> getDocumentList(Long conversionId);
 
 }

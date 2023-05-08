42d53c5511fe09e3c15901b3913cb5a526de4796
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-08 17:43:43 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelmemberadditioncontroller/repository/PolicyLevelFreeLookCancelControllerRepository.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelmemberadditioncontroller/repository/PolicyLevelFreeLookCancelControllerRepository.java
@@ -1,40 +1,14 @@
-package com.lic.epgs.policyservicing.common.policylevelmemberadditioncontroller.repository;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import com.lic.epgs.policyservicing.common.policylevelmemberadditioncontroller.dto.PolicyServiceMemberAdditionDto;
+import com.lic.epgs.policyservicing.common.policylevelmemberadditioncontroller.dto.CommonResponseDto;
+import com.lic.epgs.policyservicing.common.policylevelmemberadditioncontroller.entity.PolicyServiceMemberEntity;
+import com.lic.epgs.policyservicing.common.policylevelmemberadditioncontroller.dto.PolicyServiceMbrDto;
 
-import com.lic.epgs.policyservicing.common.policylevelmemberadditioncontroller.model.PolicyServiceMemberAdditionDto;
+public interface PolicyLevelFreeLookCancelControllerRepository extends JpaRepository<PolicyServiceMemberAdditionDto, CommonResponseDto<Object>> {
 
-public interface PolicyLevelFreeLookCancelControllerRepository extends JpaRepository<PolicyServiceMemberAdditionDto, Long>, JpaSpecificationExecutor<PolicyServiceMemberAdditionDto> {
- 
-    //Method to search Policy Service Member Addition records based on MPH name
-    List<PolicyServiceMemberAdditionDto> findByMphNameIgnoreCase(String mphName);
-
-    //Method to search Policy Service Member Addition records based on MPH code
-    List<PolicyServiceMemberAdditionDto> findByMphCode(String mphCode);
-
-    //Method to search Policy Service Member Addition records based on product
-    List<PolicyServiceMemberAdditionDto> findByProduct(String product);
-
-    //Method to search Policy Service Member Addition records based on line of business
-    List<PolicyServiceMemberAdditionDto> findByLineOfBusiness(String lineOfBusiness);
-
-    //Method to search Policy Service Member Addition records based on member addition status
-    List<PolicyServiceMemberAdditionDto> findByMemberAdditionStatus(String memberAdditionStatus);
-
-    //Method to search Policy Service Member Addition records based on unit code
-    List<PolicyServiceMemberAdditionDto> findByUnitCode(String unitCode);
-
-    //Method to search Policy Service Member Addition records based on service number
-    List<PolicyServiceMemberAdditionDto> findByServiceNumber(String serviceNumber);
-
-    //Method to search Policy Service Member Addition records based on page name
-    List<PolicyServiceMemberAdditionDto> findByPageName(String pageName);
-
-    //Method to search Policy Service Member Addition records based on active status
-    List<PolicyServiceMemberAdditionDto> findByActiveStatus(Boolean activeStatus);
-
-    //Method to search Policy Service Member Addition records based on multiple criteria
-    List<PolicyServiceMemberAdditionDto> findByMphNameIgnoreCaseAndMphCodeAndProductAndLineOfBusinessAndMemberAdditionStatusAndUnitCodeAndServiceNumberAndPageNameAndActiveStatus(String mphName, String mphCode, String product, String lineOfBusiness, String memberAdditionStatus, String unitCode, String serviceNumber, String pageName, Boolean activeStatus);
-
+    CommonResponseDto<Object> getAllMembers(Long memberAdditionId);
+    
+    List<PolicyServiceMbrDto> findByMemberAdditionId(Long memberAdditionId);
+    
+    List<PolicyServiceMemberEntity> findByActiveAddressAndActiveNominee(Boolean activeAddress, Boolean activeNominee);
 }

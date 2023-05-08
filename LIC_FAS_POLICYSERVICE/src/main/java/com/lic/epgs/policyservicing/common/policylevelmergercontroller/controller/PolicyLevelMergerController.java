7b041f4704158dae4c71e69f59da6a090062720c
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-08 18:56:06 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelmergercontroller/controller/PolicyLevelMergerController.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelmergercontroller/controller/PolicyLevelMergerController.java
@@ -1,7 +1,8 @@
 package com.lic.epgs.policyservicing.common.policylevelmergercontroller.controller;
 
-import java.util.List;
-
+import com.lic.epgs.policyservicing.common.policylevelmergercontroller.dto.PolicyLevelMergerApiResponse;
+import com.lic.epgs.policyservicing.common.policylevelmergercontroller.dto.PolicyLevelMergerSearchDto;
+import com.lic.epgs.policyservicing.common.policylevelmergercontroller.service.PolicyLevelMergerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,22 +11,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.lic.epgs.policyservicing.common.policylevelmergercontroller.model.PolicyLevelMergerSearchDto;
-import com.lic.epgs.policyservicing.common.policylevelmergercontroller.service.PolicyLevelMergerControllerService;
-
 @RestController
-@RequestMapping("/policy-level-merger-controller")
+@RequestMapping("/policylevelmerger")
 public class PolicyLevelMergerController {
 
     @Autowired
-    private PolicyLevelMergerControllerService policyLevelMergerControllerService;
+    private PolicyLevelMergerService policyLevelMergerService;
 
-    @GetMapping("/search")
-    public ResponseEntity<List<PolicyLevelMergerSearchDto>> getCriteriaSearchPolicy(@RequestParam String policyNumber,
-            @RequestParam String product, @RequestParam String lineOfBusiness, @RequestParam String mergeStatus,
-            @RequestParam String unitCode, @RequestParam String mphCode, @RequestParam String mphName) {
-        List<PolicyLevelMergerSearchDto> policyLevelMergerSearchDtos = policyLevelMergerControllerService
-                .getCriteriaSearchPolicy(policyNumber, product, lineOfBusiness, mergeStatus, unitCode, mphCode, mphName);
-        return new ResponseEntity<>(policyLevelMergerSearchDtos, HttpStatus.OK);
+    @GetMapping("/criteriaSearch")
+    public ResponseEntity<PolicyLevelMergerApiResponse> getCriteriaSearchPolicy(
+            @RequestParam String policyNumber,
+            @RequestParam String product,
+            @RequestParam String lineOfBusiness,
+            @RequestParam String mergeStatus,
+            @RequestParam String unitCode,
+            @RequestParam String mphCode,
+            @RequestParam String mphName) {
+
+        PolicyLevelMergerApiResponse response = policyLevelMergerService.getCriteriaSearchPolicy(policyNumber, product, lineOfBusiness, mergeStatus, unitCode, mphCode, mphName);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }

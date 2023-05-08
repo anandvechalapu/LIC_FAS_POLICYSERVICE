5f79b9bf5b7757034e04091addf196bc972df4b4
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-08 16:36:22 +0530

    initial commit

---
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelfreelookcancelcontroller/model/FreeLookCancellationNotes.java
@@ -0,0 +1,73 @@
+package com.lic.epgs.policyservicing.common.policylevelfreelookcancelcontroller.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class FreeLookCancellationNotes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long freeLookId;
+
+    private Long policyId;
+
+    private Long serviceId;
+
+    private String notesText;
+
+    public FreeLookCancellationNotes() {
+    }
+
+    public FreeLookCancellationNotes(Long freeLookId, Long policyId, Long serviceId, String notesText) {
+        this.freeLookId = freeLookId;
+        this.policyId = policyId;
+        this.serviceId = serviceId;
+        this.notesText = notesText;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getFreeLookId() {
+        return freeLookId;
+    }
+
+    public void setFreeLookId(Long freeLookId) {
+        this.freeLookId = freeLookId;
+    }
+
+    public Long getPolicyId() {
+        return policyId;
+    }
+
+    public void setPolicyId(Long policyId) {
+        this.policyId = policyId;
+    }
+
+    public Long getServiceId() {
+        return serviceId;
+    }
+
+    public void setServiceId(Long serviceId) {
+        this.serviceId = serviceId;
+    }
+
+    public String getNotesText() {
+        return notesText;
+    }
+
+    public void setNotesText(String notesText) {
+        this.notesText = notesText;
+    }
+    
+}

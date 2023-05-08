884ab22383416940dcd200718924900093930909
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-08 16:11:06 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelfreelookcancelcontroller/controller/PolicyLevelFreeLookCancelController.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelfreelookcancelcontroller/controller/PolicyLevelFreeLookCancelController.java
@@ -1,41 +1,40 @@
+package com.lic.epgs.policyservicing.common.policylevelfreelookcancelcontroller.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.lic.epgs.policyservicing.common.policylevelfreelookcancelcontroller.model.NotesDetails;
+
+@Repository
+public interface GetnotesDetails_PolicyLevelFreeLookCancelControllerRepository extends JpaRepository<NotesDetails, Integer> {
+
+	List<NotesDetails> findByFreeLookId(int freeLookId);
+
+}
+
 package com.lic.epgs.policyservicing.common.policylevelfreelookcancelcontroller.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.lic.epgs.policyservicing.common.policylevelfreelookcancelcontroller.dto.freeLookCancellationDocumentDto;
-import com.lic.epgs.policyservicing.common.policylevelfreelookcancelcontroller.service.RemoveflcDocument_PolicyLevelFreeLookCancelControllerService;
-import com.lic.epgs.policyservicing.common.policylevelfreelookcancelcontroller.service.TransactionStatus;
+import com.lic.epgs.policyservicing.common.policylevelfreelookcancelcontroller.model.NotesDetails;
+import com.lic.epgs.policyservicing.common.policylevelfreelookcancelcontroller.service.GetnotesDetails_PolicyLevelFreeLookCancelControllerService;
 
 @RestController
-@RequestMapping("/flcDocument")
+@RequestMapping("/policylevelfreelookcancelcontroller")
 public class PolicyLevelFreeLookCancelController {
-
+	
 	@Autowired
-	private RemoveflcDocument_PolicyLevelFreeLookCancelControllerService 
-	removeflcDocument_PolicyLevelFreeLookCancelControllerService;
+	GetnotesDetails_PolicyLevelFreeLookCancelControllerService getnotesDetails_PolicyLevelFreeLookCancelControllerService;
 	
-	@DeleteMapping("/{documentId}/{freeLookId}/{modifiedBy}")
-	public ResponseEntity<freeLookCancellationDocumentDto> removeFLCDocument(
-			@PathVariable("documentId") String documentId, 
-			@PathVariable("freeLookId") String freeLookId,
-			@PathVariable("modifiedBy") String modifiedBy) {
-		freeLookCancellationDocumentDto flcDocumentDto = removeflcDocument_PolicyLevelFreeLookCancelControllerService.removeFLCDocument(documentId, freeLookId, modifiedBy);
-		return new ResponseEntity<>(flcDocumentDto, HttpStatus.OK);
+	@GetMapping("/getNotesDetailsByFreeLookId/{freeLookId}")
+	public List<NotesDetails> getNotesDetailsByFreeLookId(@PathVariable int freeLookId) {
+		return getnotesDetails_PolicyLevelFreeLookCancelControllerService.getNotesDetailsByFreeLookId(freeLookId);
 	}
-	
-	@DeleteMapping("/status/{documentId}/{freeLookId}")
-	public ResponseEntity<TransactionStatus> removeFlcDocumentStatus(
-			@PathVariable("documentId") String documentId, 
-			@PathVariable("freeLookId") String freeLookId) {
-		TransactionStatus transactionStatus = removeflcDocument_PolicyLevelFreeLookCancelControllerService.removeFlcDocumentStatus(documentId, freeLookId);
-		return new ResponseEntity<>(transactionStatus, HttpStatus.OK);
-	}
-	
 	
 }

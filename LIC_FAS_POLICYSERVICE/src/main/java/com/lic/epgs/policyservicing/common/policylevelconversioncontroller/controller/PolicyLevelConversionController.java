1b515d5fa3a77931285dc3cf0c02428dac9ce17c
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-08 13:21:05 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelconversioncontroller/controller/PolicyLevelConversionController.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelconversioncontroller/controller/PolicyLevelConversionController.java
@@ -1,30 +1,23 @@
-package com.lic.epgs.policyservicing.common.policylevelconversioncontroller.repository;
-
-import com.lic.epgs.policyservicing.common.policylevelconversioncontroller.dto.CommonResponseDto;
-import com.lic.epgs.policyservicing.common.policylevelconversioncontroller.dto.PolicyLevelConversionDto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PolicyLevelConversionControllerRepository extends JpaRepository<PolicyLevelConversionDto, Long> {
-    CommonResponseDto getPolicyConversionDetailsByPolicyNo(String prevPolicyNo, String role);
-}
-
 package com.lic.epgs.policyservicing.common.policylevelconversioncontroller.controller;
 
-import com.lic.epgs.policyservicing.common.policylevelconversioncontroller.dto.CommonResponseDto;
-import com.lic.epgs.policyservicing.common.policylevelconversioncontroller.dto.PolicyLevelConversionDto;
+import com.lic.epgs.policyservicing.common.policylevelconversioncontroller.model.Document;
 import com.lic.epgs.policyservicing.common.policylevelconversioncontroller.service.PolicyLevelConversionControllerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/policylevelconversioncontroller")
 public class PolicyLevelConversionController {
 
     @Autowired
-    private PolicyLevelConversionControllerService policyLevelConversionControllerService;
+    PolicyLevelConversionControllerService policyLevelConversionControllerService;
 
-    @GetMapping("/getpolicyconversiondetailsbypolicyno")
-    public CommonResponseDto getPolicyConversionDetailsByPolicyNo(@RequestParam String prevPolicyNo, @RequestParam String role) {
-        return policyLevelConversionControllerService.getPolicyConversionDetailsByPolicyNo(prevPolicyNo, role);
+    @GetMapping("/conversionId/{conversionId}")
+    public List<Document> getDocumentList(@PathVariable("conversionId") Long conversionId) {
+        return policyLevelConversionControllerService.getDocumentList(conversionId);
     }
+
 }

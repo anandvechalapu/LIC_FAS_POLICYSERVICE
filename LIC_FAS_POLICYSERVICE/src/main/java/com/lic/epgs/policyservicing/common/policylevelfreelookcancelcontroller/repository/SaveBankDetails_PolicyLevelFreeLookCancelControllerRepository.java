452b20a8ed26dd2b8971efc1b4c7b146c4b1c9aa
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-08 16:48:12 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelfreelookcancelcontroller/repository/SaveBankDetails_PolicyLevelFreeLookCancelControllerRepository.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelfreelookcancelcontroller/repository/SaveBankDetails_PolicyLevelFreeLookCancelControllerRepository.java
@@ -1,12 +1,21 @@
-@Repository
-public interface SaveBankDetails_PolicyLevelFreeLookCancelControllerRepository {
+package com.lic.epgs.policyservicing.common.policylevelfreelookcancelcontroller.repository;
 
-    /**
-     * Save or update the bank details for a policy in the free look cancellation feature.
-     *
-     * @param policyBankOldDto  The PolicyBankOldDto object containing the necessary information for saving bank details.
-     * @return FreeLookCancellationResponseDto object with the saved or updated bank details and the transaction details.
-     */
-    FreeLookCancellationResponseDto saveBankDetails(PolicyBankOldDto policyBankOldDto);
+import com.lic.epgs.policyservicing.common.policylevelfreelookcancelcontroller.dto.FreeLookCancellationResponseDto;
+import com.lic.epgs.policyservicing.common.policylevelfreelookcancelcontroller.dto.PolicyBankOldDto;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface SaveBankDetails_PolicyLevelFreeLookCancelControllerRepository extends JpaRepository<PolicyBankOldDto, Long> {
+
+	@Modifying
+	@Query("INSERT INTO PolicyBankOldDto (mphBankId, mphId, bankName, bankAddress, bankAccountNumber, accountType, bankIFSCCode) VALUES (:mphBankId, :mphId, :bankName, :bankAddress, :bankAccountNumber, :accountType, :bankIFSCCode)")
+	FreeLookCancellationResponseDto saveBankDetails(@Param("mphBankId") Long mphBankId, @Param("mphId") Long mphId, @Param("bankName") String bankName, @Param("bankAddress") String bankAddress, @Param("bankAccountNumber") String bankAccountNumber, @Param("accountType") String accountType, @Param("bankIFSCCode") String bankIFSCCode);
+
+	@Modifying
+	@Query("UPDATE PolicyBankOldDto SET mphBankId=:mphBankId, mphId=:mphId, bankName=:bankName, bankAddress=:bankAddress, bankAccountNumber=:bankAccountNumber, accountType=:accountType, bankIFSCCode=:bankIFSCCode WHERE mphBankId=:mphBankId")
+	FreeLookCancellationResponseDto updateBankDetails(@Param("mphBankId") Long mphBankId, @Param("mphId") Long mphId, @Param("bankName") String bankName, @Param("bankAddress") String bankAddress, @Param("bankAccountNumber") String bankAccountNumber, @Param("accountType") String accountType, @Param("bankIFSCCode") String bankIFSCCode);
 
 }

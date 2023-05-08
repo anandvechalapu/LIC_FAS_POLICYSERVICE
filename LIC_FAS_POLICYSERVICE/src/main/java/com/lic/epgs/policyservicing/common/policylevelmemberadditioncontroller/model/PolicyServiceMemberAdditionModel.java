1820936c44a6c810f504b5e135b9c1f8b9a75f6e
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-08 17:10:26 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelmemberadditioncontroller/model/PolicyServiceMemberAdditionModel.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelmemberadditioncontroller/model/PolicyServiceMemberAdditionModel.java
@@ -8,63 +8,40 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "PolicyServiceMemberAddition")
+@Table(name = "policy_service_member_addition")
 public class PolicyServiceMemberAdditionModel {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long memberAdditionId;
-    
+
     @Column(name = "unit_code")
     private String unitCode;
-    
-    @Column(name = "member_addition_status")
-    private String memberAdditionStatus;
-    
-    @Column(name = "modified_by")
-    private String modifiedBy;
-    
-    @Column(name = "modified_on")
-    private String modifiedOn;
-    
+
+    @Column(name = "member_addition_name")
+    private String memberAdditionName;
+
     public Long getMemberAdditionId() {
         return memberAdditionId;
     }
-    
+
     public void setMemberAdditionId(Long memberAdditionId) {
         this.memberAdditionId = memberAdditionId;
     }
-    
+
     public String getUnitCode() {
         return unitCode;
     }
-    
+
     public void setUnitCode(String unitCode) {
         this.unitCode = unitCode;
     }
-    
-    public String getMemberAdditionStatus() {
-        return memberAdditionStatus;
+
+    public String getMemberAdditionName() {
+        return memberAdditionName;
     }
-    
-    public void setMemberAdditionStatus(String memberAdditionStatus) {
-        this.memberAdditionStatus = memberAdditionStatus;
+
+    public void setMemberAdditionName(String memberAdditionName) {
+        this.memberAdditionName = memberAdditionName;
     }
-    
-    public String getModifiedBy() {
-        return modifiedBy;
-    }
-    
-    public void setModifiedBy(String modifiedBy) {
-        this.modifiedBy = modifiedBy;
-    }
-    
-    public String getModifiedOn() {
-        return modifiedOn;
-    }
-    
-    public void setModifiedOn(String modifiedOn) {
-        this.modifiedOn = modifiedOn;
-    }
-    
 }

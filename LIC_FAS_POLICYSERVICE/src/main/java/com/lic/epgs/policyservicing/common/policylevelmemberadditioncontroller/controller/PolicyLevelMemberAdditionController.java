ae66dc7bf9ef662c7a88311a85428f2ae0ac8ad2
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-08 17:13:49 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelmemberadditioncontroller/controller/PolicyLevelMemberAdditionController.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelmemberadditioncontroller/controller/PolicyLevelMemberAdditionController.java
@@ -1,25 +1,41 @@
 package com.lic.epgs.policyservicing.common.policylevelmemberadditioncontroller.controller;
 
-import java.util.Date;
-
-import com.lic.epgs.policyservicing.common.policylevelmemberadditioncontroller.model.CommonResponseDto;
-import com.lic.epgs.policyservicing.common.policylevelmemberadditioncontroller.service.PolicyLevelMemberAdditionControllerService;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.lic.epgs.policyservicing.common.policylevelmemberadditioncontroller.model.PolicyServiceMemberAdditionModel;
+import com.lic.epgs.policyservicing.common.policylevelmemberadditioncontroller.service.PolicyLevelMemberAdditionControllerService;
+
+@CrossOrigin(origins = "*")
 @RestController
+@RequestMapping("/memberaddition/policyService")
 public class PolicyLevelMemberAdditionController {
+	
+	@Autowired
+	private PolicyLevelMemberAdditionControllerService policyLevelMemberAdditionControllerService;
+	
+	@PostMapping("/sendToMakerOrChecker")
+	public ResponseEntity<PolicyServiceMemberAdditionModel> sendToMakerOrChecker(@RequestBody PolicyServiceMemberAdditionModel policyServiceMemberAdditionModel){
+		return ResponseEntity.ok(policyLevelMemberAdditionControllerService.sendToMakerOrChecker(policyServiceMemberAdditionModel.getMemberAdditionStatus(), policyServiceMemberAdditionModel.getModifiedBy(), policyServiceMemberAdditionModel.getModifiedOn(), policyServiceMemberAdditionModel.getMemberAdditionId(), policyServiceMemberAdditionModel.getUnitCode()));
+	}
+	
+	@GetMapping("/getAll")
+	public ResponseEntity<List<PolicyServiceMemberAdditionModel>> getAll(){
+		return ResponseEntity.ok(policyLevelMemberAdditionControllerService.findAll());
+	}
+	
+	@GetMapping("/getById/{id}")
+	public ResponseEntity<PolicyServiceMemberAdditionModel> getById(@PathVariable Long id){
+		return ResponseEntity.ok(policyLevelMemberAdditionControllerService.findById(id));
+	}
 
-    @Autowired
-    private PolicyLevelMemberAdditionControllerService service;
-
-    @PostMapping("/saveNote")
-    public CommonResponseDto saveNote(@RequestParam("memberAdditionId") String memberAdditionId,
-        @RequestParam("note") String note, @RequestParam("createdBy") String createdBy,
-        @RequestParam("createdOn") Date createdOn) {
-        return service.saveNote(memberAdditionId, note, createdBy, createdOn);
-    }
 }

294c29cc48b438842fa3a364fe2d403594a24afd
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-08 17:28:35 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelmemberadditioncontroller/repository/.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelmemberadditioncontroller/repository/.java
@@ -1,19 +1,38 @@
 package com.lic.epgs.policyservicing.common.policylevelmemberadditioncontroller.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+import com.lic.epgs.policyservicing.common.policylevelmemberadditioncontroller.dto.PolicyServiceMemberAdditionDto;
+import com.lic.epgs.policyservicing.common.policylevelmemberadditioncontroller.dto.CommonResponseDto;
 import org.springframework.stereotype.Repository;
 
-import com.lic.epgs.policyservicing.common.policylevelmemberadditioncontroller.entity.PolicyServiceMemberAdditionEntity;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import java.util.List;
 
 @Repository
-public interface SendToMaker_PolicyLevelMemberAdditionController extends JpaRepository<PolicyServiceMemberAdditionEntity, Long> {
+public class PolicyLevelFreeLookCancelControllerRepository {
 
-    @Modifying
-    @Query(value = "UPDATE PolicyServiceMemberAdditionEntity p SET p.memberAdditionStatus = :memberAdditionStatus, p.modifiedBy = :modifiedBy, p.modifiedOn = :modifiedOn WHERE p.memberAdditionId = :memberAdditionId AND p.unitCode = :unitCode")
-    void sendToMakerOrChecker(@Param("memberAdditionStatus") String memberAdditionStatus, @Param("modifiedBy") String modifiedBy, @Param("modifiedOn") String modifiedOn,
-            @Param("memberAdditionId") Long memberAdditionId, @Param("unitCode") String unitCode);
+    @PersistenceContext
+    private EntityManager entityManager;
 
+    public CommonResponseDto criteriaSearch(PolicyServiceMemberAdditionDto policyServiceMemberAdditionDto) {
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<PolicyServiceMemberAdditionDto> criteriaQuery = cb.createQuery(PolicyServiceMemberAdditionDto.class);
+
+        // Construct the search query using the CriteriaBuilder and CriteriaQuery classes
+        // Search should use case-insensitive matching for MPH name
+        // Search should return only the active records
+        // Search should return only the records that match all the provided criteria
+        // Search should return the MPH name, MPH code, product, line of business, member addition status, unit code, and service number fields
+
+        List<PolicyServiceMemberAdditionDto> results = entityManager.createQuery(criteriaQuery).getResultList();
+
+        CommonResponseDto response = new CommonResponseDto();
+        response.setData(results);
+        response.setTransactionMessage("Search successful");
+        response.setTransactionStatus("SUCCESS");
+
+        return response;
+    }
 }

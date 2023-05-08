61325c10c46867190d54fd37bf3f791dd3d75b66
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-08 16:35:32 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelfreelookcancelcontroller/repository/PolicyLevelFreeLookCancelControllerRepository.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelfreelookcancelcontroller/repository/PolicyLevelFreeLookCancelControllerRepository.java
@@ -1,4 +1,15 @@
-@Repository
-public interface PolicyLevelFreeLookCancelControllerRepository extends JpaRepository<FreeLookCancellationTempEntity, Long> {
-    FreeLookCancellationResponseDto getFreeLookCancellationDetails(Long freeLookCancellationId);
+package com.lic.epgs.policyservicing.common.policylevelfreelookcancelcontroller.repository;
+
+import com.lic.epgs.policyservicing.common.policylevelfreelookcancelcontroller.entity.PolicyServiceDocumentTempEntity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PolicyLevelFreeLookCancelControllerRepository extends JpaRepository<PolicyServiceDocumentTempEntity, Long> {
+
+    PolicyServiceDocumentTempEntity findByDocumentId(Long documentId);
+    
+    PolicyServiceDocumentTempEntity save(PolicyServiceDocumentTempEntity document);
+    
+    void deleteByDocumentId(Long documentId);
+    
 }

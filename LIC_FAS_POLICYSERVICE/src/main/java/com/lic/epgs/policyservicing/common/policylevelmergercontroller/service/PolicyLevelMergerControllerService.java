f8e68a17ca56dc52be87a4f44149a449b4482389
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-08 19:04:25 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelmergercontroller/service/PolicyLevelMergerControllerService.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelmergercontroller/service/PolicyLevelMergerControllerService.java
@@ -1,19 +1,21 @@
 package com.lic.epgs.policyservicing.common.policylevelmergercontroller.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import com.lic.epgs.policyservicing.common.policylevelmergercontroller.model.ExistingPolicy;
+import com.lic.epgs.policyservicing.common.policylevelmergercontroller.dto.PolicyServiceDocumentDto;
 import com.lic.epgs.policyservicing.common.policylevelmergercontroller.repository.PolicyLevelMergerControllerRepository;
 
 @Service
 public class PolicyLevelMergerControllerService {
-	
-	@Autowired
-	private PolicyLevelMergerControllerRepository policyLevelMergerControllerRepository;
-	
-	public ExistingPolicy getExistingPolicyByPolicyNumber(String policyNumber) {
-		return policyLevelMergerControllerRepository.getExistingPolicyByPolicyNumber(policyNumber);
-	}
-
+ 
+    @Autowired
+    private PolicyLevelMergerControllerRepository repository;
+ 
+    public ResponseEntity<List<PolicyServiceDocumentDto>> getDocumentList(String mergeId) {
+        return repository.getDocumentList(mergeId);
+    }
 }

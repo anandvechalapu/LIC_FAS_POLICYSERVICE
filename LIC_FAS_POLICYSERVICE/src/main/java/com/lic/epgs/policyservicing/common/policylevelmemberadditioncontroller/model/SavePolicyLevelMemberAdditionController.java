a9330ced3a4ab1cd4ae5463f85a1f135314c7e38
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-08 17:54:53 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelmemberadditioncontroller/model/SavePolicyLevelMemberAdditionController.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelmemberadditioncontroller/model/SavePolicyLevelMemberAdditionController.java
@@ -1,70 +1,42 @@
 package com.lic.epgs.policyservicing.common.policylevelmemberadditioncontroller.model;
 
-import java.util.Map;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
 public class SavePolicyLevelMemberAdditionController {
-    private String memberAdditionId;
-    private Map<String, Object> policyServiceMemberDetails;
-    private String memberAdditionStatus;
 
-    public String getMemberAdditionId() {
-        return memberAdditionId;
-    }
+   @Id
+   @GeneratedValue(strategy = GenerationType.AUTO)
+   private Long id;
 
-    public void setMemberAdditionId(String memberAdditionId) {
-        this.memberAdditionId = memberAdditionId;
-    }
+   private String componentName;
+   private String memberAdditionId;
 
-    public Map<String, Object> getPolicyServiceMemberDetails() {
-        return policyServiceMemberDetails;
-    }
+   public Long getId() {
+      return id;
+   }
 
-    public void setPolicyServiceMemberDetails(Map<String, Object> policyServiceMemberDetails) {
-        this.policyServiceMemberDetails = policyServiceMemberDetails;
-    }
+   public void setId(Long id) {
+      this.id = id;
+   }
 
-    public String getMemberAdditionStatus() {
-        return memberAdditionStatus;
-    }
+   public String getComponentName() {
+      return componentName;
+   }
 
-    public void setMemberAdditionStatus(String memberAdditionStatus) {
-        this.memberAdditionStatus = memberAdditionStatus;
-    }
+   public void setComponentName(String componentName) {
+      this.componentName = componentName;
+   }
 
-    public void savePolicyServiceMemberDetails(Map<String, Object> policyServiceMemberDetails) {
-        // Save policy service member details
-    }
+   public String getMemberAdditionId() {
+      return memberAdditionId;
+   }
 
-    public void updatePolicyServiceMemberDetails(Map<String, Object> policyServiceMemberDetails) {
-        // Update policy service member details
-    }
+   public void setMemberAdditionId(String memberAdditionId) {
+      this.memberAdditionId = memberAdditionId;
+   }
 
-    public void savePolicyServiceMemberDetailsAsDraft(Map<String, Object> policyServiceMemberDetails) {
-        // Save policy service member details as draft
-    }
-
-    public void savePolicyServiceMemberDetailsAsActive(Map<String, Object> policyServiceMemberDetails) {
-        // Save policy service member details as active
-    }
-
-    public void setMemberAdditionStatusAsDraftStatus(String memberAdditionId) {
-        // Set member addition status as Draft Status
-    }
-
-    public void setMemberAdditionStatusAsActiveStatus(String memberAdditionId) {
-        // Set member addition status as Active Status
-    }
-
-    public Map<String, Object> getUpdatedPolicyServiceMemberDetails(String memberAdditionId) {
-        // Get updated policy service member details
-        return null;
-    }
-
-    public void handleExceptionsGracefully() {
-        // Handle exceptions gracefully
-    }
-
-    public void securePolicyServiceMemberDetails() {
-        // Secure policy service member details
-    }
 }

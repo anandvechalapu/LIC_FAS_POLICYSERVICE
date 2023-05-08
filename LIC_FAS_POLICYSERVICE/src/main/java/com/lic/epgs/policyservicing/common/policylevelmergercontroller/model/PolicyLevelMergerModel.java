82ce9e8a2306862eb0d3dc63f26ec4e55f5e8b8d
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-08 19:04:12 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelmergercontroller/model/PolicyLevelMergerModel.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelmergercontroller/model/PolicyLevelMergerModel.java
@@ -1,95 +1,103 @@
 package com.lic.epgs.policyservicing.common.policylevelmergercontroller.model;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "policy_level_merger")
 public class PolicyLevelMergerModel {
+    
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String policyNumber;
     private String product;
     private String lineOfBusiness;
-    private String mergeStatus;
+    private Integer mergeStatus;
     private String unitCode;
     private String mphCode;
     private String mphName;
-    private String modifiedDate;
-
+    private Boolean isActive;
+    private Long modifiedOn;
+    
     public Long getId() {
         return id;
     }
-
+    
     public void setId(Long id) {
         this.id = id;
     }
-
+    
     public String getPolicyNumber() {
         return policyNumber;
     }
-
+    
     public void setPolicyNumber(String policyNumber) {
         this.policyNumber = policyNumber;
     }
-
+    
     public String getProduct() {
         return product;
     }
-
+    
     public void setProduct(String product) {
         this.product = product;
     }
-
+    
     public String getLineOfBusiness() {
         return lineOfBusiness;
     }
-
+    
     public void setLineOfBusiness(String lineOfBusiness) {
         this.lineOfBusiness = lineOfBusiness;
     }
-
-    public String getMergeStatus() {
+    
+    public Integer getMergeStatus() {
         return mergeStatus;
     }
-
-    public void setMergeStatus(String mergeStatus) {
+    
+    public void setMergeStatus(Integer mergeStatus) {
         this.mergeStatus = mergeStatus;
     }
-
+    
     public String getUnitCode() {
         return unitCode;
     }
-
+    
     public void setUnitCode(String unitCode) {
         this.unitCode = unitCode;
     }
-
+    
     public String getMphCode() {
         return mphCode;
     }
-
+    
     public void setMphCode(String mphCode) {
         this.mphCode = mphCode;
     }
-
+    
     public String getMphName() {
         return mphName;
     }
-
+    
     public void setMphName(String mphName) {
         this.mphName = mphName;
     }
-
-    public String getModifiedDate() {
-        return modifiedDate;
+    
+    public Boolean getIsActive() {
+        return isActive;
     }
-
-    public void setModifiedDate(String modifiedDate) {
-        this.modifiedDate = modifiedDate;
+    
+    public void setIsActive(Boolean isActive) {
+        this.isActive = isActive;
     }
+    
+    public Long getModifiedOn() {
+        return modifiedOn;
+    }
+    
+    public void setModifiedOn(Long modifiedOn) {
+        this.modifiedOn = modifiedOn;
+    }
+    
 }

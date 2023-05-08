7484c838a448fe2ca1366ecff60cdac57d591d49
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-08 16:15:21 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelfreelookcancelcontroller/repository/FreeLookCancellationRepository.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelfreelookcancelcontroller/repository/FreeLookCancellationRepository.java
@@ -1,18 +1,17 @@
 package com.lic.epgs.policyservicing.common.policylevelfreelookcancelcontroller.repository;
 
-import com.lic.epgs.policyservicing.common.policylevelfreelookcancelcontroller.dto.FreeLookCancellationResponseDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import com.lic.epgs.policyservicing.common.policylevelfreelookcancelcontroller.entity.FreeLookCancellationEntity;
+import com.lic.epgs.policyservicing.common.policylevelfreelookcancelcontroller.dto.MphBankDto;
+
 import java.util.List;
 
 @Repository
-public interface FreeLookCancellationRepository extends JpaRepository<FreeLookCancellationResponseDto, Long> {
+public interface FreeLookCancellationRepository extends JpaRepository<FreeLookCancellationEntity, Long> {
 
-    @Query("SELECT f FROM FreeLookCancellationResponseDto f WHERE f.freeLookCancellationId = ?1")
-    List<FreeLookCancellationResponseDto> getFlcDocumentDetails(Long freeLookCancellationId);
-    
-    @Query("INSERT INTO FreeLookCancellationResponseDto SELECT * FROM tempTable WHERE freeLookCancellationId = ?1")
-    void moveToFlcMainTable(Long freeLookCancellationId);
+    @Query("SELECT f FROM FreeLookCancellationEntity f LEFT JOIN FETCH f.policyMaster pm LEFT JOIN FETCH pm.mphBankDtos WHERE f.id = ?1")
+    FreeLookCancellationEntity getExistingFreeLookCancellationById(Long freeLookId);
 }

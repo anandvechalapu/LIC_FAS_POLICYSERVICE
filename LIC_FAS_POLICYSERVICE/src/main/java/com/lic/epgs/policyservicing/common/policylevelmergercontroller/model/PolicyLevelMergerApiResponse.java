e67b227a516a40f206c66c5ef30aa69733bb8309
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-08 18:41:28 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelmergercontroller/model/PolicyLevelMergerApiResponse.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelmergercontroller/model/PolicyLevelMergerApiResponse.java
@@ -4,17 +4,24 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "policy_level_merger_api_response")
 public class PolicyLevelMergerApiResponse {
-    
+
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Long mergeId;
+
     private String policyNumber;
-    private String displayName;
+    private String product;
+    private String lineOfBusiness;
     private String mergeStatus;
+    private String unitCode;
+    private String mphCode;
+    private String mphName;
+    private String modificationDate;
 
     public Long getId() {
         return id;
@@ -22,14 +29,6 @@ public class PolicyLevelMergerApiResponse {
 
     public void setId(Long id) {
         this.id = id;
-    }
-
-    public Long getMergeId() {
-        return mergeId;
-    }
-
-    public void setMergeId(Long mergeId) {
-        this.mergeId = mergeId;
     }
 
     public String getPolicyNumber() {
@@ -40,12 +39,20 @@ public class PolicyLevelMergerApiResponse {
         this.policyNumber = policyNumber;
     }
 
-    public String getDisplayName() {
-        return displayName;
+    public String getProduct() {
+        return product;
     }
 
-    public void setDisplayName(String displayName) {
-        this.displayName = displayName;
+    public void setProduct(String product) {
+        this.product = product;
+    }
+
+    public String getLineOfBusiness() {
+        return lineOfBusiness;
+    }
+
+    public void setLineOfBusiness(String lineOfBusiness) {
+        this.lineOfBusiness = lineOfBusiness;
     }
 
     public String getMergeStatus() {
@@ -56,111 +63,35 @@ public class PolicyLevelMergerApiResponse {
         this.mergeStatus = mergeStatus;
     }
 
+    public String getUnitCode() {
+        return unitCode;
+    }
+
+    public void setUnitCode(String unitCode) {
+        this.unitCode = unitCode;
+    }
+
+    public String getMphCode() {
+        return mphCode;
+    }
+
+    public void setMphCode(String mphCode) {
+        this.mphCode = mphCode;
+    }
+
+    public String getMphName() {
+        return mphName;
+    }
+
+    public void setMphName(String mphName) {
+        this.mphName = mphName;
+    }
+
+    public String getModificationDate() {
+        return modificationDate;
+    }
+
+    public void setModificationDate(String modificationDate) {
+        this.modificationDate = modificationDate;
+    }
 }
-
-package com.lic.epgs.policyservicing.common.policylevelmergercontroller.model;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
-@Entity
-public class PolicyLevelMergerDto {
-    
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-    private Long mergeId;
-    private String policyNumber;
-    private String displayName;
-    private String policyType;
-    private String policyTerm;
-    private String policyStatus;
-    private String policyHolderName;
-    private String policyHolderType;
-    private String policyPremium;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getMergeId() {
-        return mergeId;
-    }
-
-    public void setMergeId(Long mergeId) {
-        this.mergeId = mergeId;
-    }
-
-    public String getPolicyNumber() {
-        return policyNumber;
-    }
-
-    public void setPolicyNumber(String policyNumber) {
-        this.policyNumber = policyNumber;
-    }
-
-    public String getDisplayName() {
-        return displayName;
-    }
-
-    public void setDisplayName(String displayName) {
-        this.displayName = displayName;
-    }
-
-    public String getPolicyType() {
-        return policyType;
-    }
-
-    public void setPolicyType(String policyType) {
-        this.policyType = policyType;
-    }
-
-    public String getPolicyTerm() {
-        return policyTerm;
-    }
-
-    public void setPolicyTerm(String policyTerm) {
-        this.policyTerm = policyTerm;
-    }
-
-    public String getPolicyStatus() {
-        return policyStatus;
-    }
-
-    public void setPolicyStatus(String policyStatus) {
-        this.policyStatus = policyStatus;
-    }
-
-    public String getPolicyHolderName() {
-        return policyHolderName;
-    }
-
-    public void setPolicyHolderName(String policyHolderName) {
-        this.policyHolderName = policyHolderName;
-    }
-
-    public String getPolicyHolderType() {
-        return policyHolderType;
-    }
-
-    public void setPolicyHolderType(String policyHolderType) {
-        this.policyHolderType = policyHolderType;
-    }
-
-    public String getPolicyPremium() {
-        return policyPremium;
-    }
-
-    public void setPolicyPremium(String policyPremium) {
-        this.policyPremium = policyPremium;
-    }
-
-}
-
-package

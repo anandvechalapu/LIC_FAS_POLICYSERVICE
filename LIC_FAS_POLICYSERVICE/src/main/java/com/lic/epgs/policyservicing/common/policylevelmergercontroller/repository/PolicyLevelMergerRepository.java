eae25ad7df82d7fc871472fea498e7f5bea6c731
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-08 18:31:15 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelmergercontroller/repository/PolicyLevelMergerRepository.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelmergercontroller/repository/PolicyLevelMergerRepository.java
@@ -1,12 +1,12 @@
 package com.lic.epgs.policyservicing.common.policylevelmergercontroller.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import com.lic.epgs.policyservicing.common.policylevelmergercontroller.model.PolicyLevelMergerApiResponse;
+import com.lic.epgs.policyservicing.common.policylevelmergercontroller.model.PolicyLevelMergerDto;
+import com.lic.epgs.policyservicing.common.policylevelmergercontroller.model.PolicyServiceNotesDto;
 
-import com.lic.epgs.policyservicing.common.policylevelmergercontroller.model.PolicyLevelMergerTempEntity;
-
-@Repository
-public interface PolicyLevelMergerRepository extends JpaRepository<PolicyLevelMergerTempEntity, Long> {
- 
-    PolicyLevelMergerTempEntity getPolicyMergebyMergeId_PolicyLevelMergerController(Long mergeId);
+public interface PolicyLevelMergerRepository extends JpaRepository<PolicyLevelMergerApiResponse, Long>{
+    PolicyLevelMergerApiResponse getMasterPolicyMergebyMergeId(Long mergeId);
+    PolicyLevelMergerDto findByMergeId(Long mergeId);
+    PolicyServiceNotesDto findByMergeIdAndIsActive(Long mergeId, Boolean isActive);
 }

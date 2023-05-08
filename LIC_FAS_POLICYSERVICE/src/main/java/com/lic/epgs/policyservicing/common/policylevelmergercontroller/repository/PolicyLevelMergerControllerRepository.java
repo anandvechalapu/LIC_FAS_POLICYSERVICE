4c79401a64f764f468a1ed36fde29c384459652e
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-08 18:44:46 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelmergercontroller/repository/PolicyLevelMergerControllerRepository.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelmergercontroller/repository/PolicyLevelMergerControllerRepository.java
@@ -1,17 +1,24 @@
-package com.lic.epgs.policyservicing.common.policylevelmergercontroller.repository;
-
-import com.lic.epgs.policyservicing.common.policylevelmergercontroller.dto.PolicyLevelMergerApiResponse;
-import com.lic.epgs.policyservicing.common.policylevelmergercontroller.dto.PolicyLevelMergerSearchDto;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.query.Param;
+import com.lic.epgs.policyservicing.common.policylevelmergercontroller.repository.PolicyLevelMergerSearchDto;
 
-import java.util.List;
+public interface PolicyLevelMergerControllerRepository extends JpaRepository<PolicyLevelMergerSearchDto, Long> {
 
-@Repository
-public interface PolicyLevelMergerControllerRepository extends JpaRepository<PolicyLevelMergerApiResponse, Long>, JpaSpecificationExecutor<PolicyLevelMergerApiResponse> {
-
-    @Query("SELECT p FROM PolicyLevelMergerApiResponse p WHERE p.policyNumber = ?1 AND p.product = ?2 AND p.lineOfBusiness = ?3 AND p.mergeStatus = ?4 AND p.unitCode = ?5 AND p.mphCode = ?6 AND p.mphName = ?7 ORDER BY p.modificationDate DESC ")
-    List<PolicyLevelMergerSearchDto> getCriteriaSearchPolicy(String policyNumber, String product, String lineOfBusiness, String mergeStatus, String unitCode, String mphCode, String mphName);
+    @Query("SELECT p FROM PolicyLevelMergerSearchDto p WHERE "
+           + " (:policyNumber IS NULL OR p.policyNumber = :policyNumber)"
+           + " AND (:product IS NULL OR p.product = :product)"
+           + " AND (:lineOfBusiness IS NULL OR p.lineOfBusiness = :lineOfBusiness)"
+           + " AND (:mergeStatus IS NULL OR p.mergeStatus = :mergeStatus)"
+           + " AND (:unitCode IS NULL OR p.unitCode = :unitCode)"
+           + " AND (:mphCode IS NULL OR p.mphCode = :mphCode)"
+           + " AND (:mphName IS NULL OR p.mphName = :mphName)"
+           + " ORDER BY p.modificationDate DESC")
+    List<PolicyLevelMergerSearchDto> getCriteriaSearchPolicy(@Param("policyNumber") String policyNumber,
+                                                            @Param("product") String product,
+                                                            @Param("lineOfBusiness") String lineOfBusiness,
+                                                            @Param("mergeStatus") String mergeStatus,
+                                                            @Param("unitCode") String unitCode,
+                                                            @Param("mphCode") String mphCode,
+                                                            @Param("mphName") String mphName);
 }

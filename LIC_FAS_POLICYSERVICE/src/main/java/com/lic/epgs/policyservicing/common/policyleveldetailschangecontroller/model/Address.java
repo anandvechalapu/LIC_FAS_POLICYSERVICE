4de497395e91a72bb2a2c7a2c232dd56b0184cd1
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-08 15:44:18 +0530

    initial commit

---
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policyleveldetailschangecontroller/model/Address.java
@@ -0,0 +1,71 @@
+package com.lic.epgs.policyservicing.common.policyleveldetailschangecontroller.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Address {
+	
+	@Id
+	private Long id;
+	
+	private String mphId;
+	private String address1;
+	private String address2;
+	private String city;
+	private String state;
+	private String country;
+	private String zipCode;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getMphId() {
+		return mphId;
+	}
+	public void setMphId(String mphId) {
+		this.mphId = mphId;
+	}
+	public String getAddress1() {
+		return address1;
+	}
+	public void setAddress1(String address1) {
+		this.address1 = address1;
+	}
+	public String getAddress2() {
+		return address2;
+	}
+	public void setAddress2(String address2) {
+		this.address2 = address2;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getZipCode() {
+		return zipCode;
+	}
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+	
+	
+
+}

c3e379ef7b64e1ca1d2332f8b359ad903d7d824e
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-08 17:00:46 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelfreelookcancelcontroller/controller/PolicyLevelFreeLookCancelController.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelfreelookcancelcontroller/controller/PolicyLevelFreeLookCancelController.java
@@ -1,26 +1,31 @@
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.lic.epgs.policyservicing.common.policylevelfreelookcancelcontroller.dto.FreeLookCancellationResponseDto;
-import com.lic.epgs.policyservicing.common.policylevelfreelookcancelcontroller.dto.PolicyServiceDocumentDto;
-import com.lic.epgs.policyservicing.common.policylevelfreelookcancelcontroller.service.PolicyLevelFreeLookCancelControllerService;
-
 @RestController
-@RequestMapping("/uploadDocument")
+@RequestMapping("/api/policylevelfreelookcancel")
 public class PolicyLevelFreeLookCancelController {
-
+  
     @Autowired
-    private PolicyLevelFreeLookCancelControllerService policyLevelFreeLookCancelControllerService;
-
-    @PostMapping
-    public ResponseEntity<FreeLookCancellationResponseDto> uploadDocument(@RequestBody PolicyServiceDocumentDto policyServiceDocumentDto) {
-        FreeLookCancellationResponseDto freeLookCancellationResponseDto = policyLevelFreeLookCancelControllerService.uploadDocument(policyServiceDocumentDto);
-        return new ResponseEntity<>(freeLookCancellationResponseDto, HttpStatus.OK);
+    private PolicySearchForFreeLookCancellation_PolicyLevelFreeLookCancelControllerService service;
+    
+    @GetMapping("/policy/{policyNumber}")
+    public ResponseEntity<Object> getPolicyForFreeLookCancellation(@PathVariable String policyNumber) {
+        
+        Optional<PolicyDetails> policyDetails = service.findByPolicyNumber(policyNumber);
+        
+        if(policyDetails.isPresent()) {
+            return new ResponseEntity<>(policyDetails.get(), HttpStatus.OK);
+        }
+        
+        return new ResponseEntity<>("No policy details found for policy number: " + policyNumber, HttpStatus.NOT_FOUND);
     }
-
+    
+    @PostMapping("/process")
+    public ResponseEntity<Object> logProcess(@RequestParam Date startTime, @RequestParam Date endTime) {
+        
+        boolean isLogged = service.logProcess(startTime, endTime);
+        
+        if(isLogged) {
+            return new ResponseEntity<>("Successfully logged the process.", HttpStatus.OK);
+        }
+        
+        return new ResponseEntity<>("Failed to log the process.", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }

26454c9dcd977f074f29a960b2ba6189e99f9e0a
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-08 18:38:52 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelmergercontroller/repository/PolicyLevelMergerControllerRepository.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelmergercontroller/repository/PolicyLevelMergerControllerRepository.java
@@ -2,18 +2,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.lic.epgs.policyservicing.common.policylevelmergercontroller.model.PolicyLevelMergerApiResponse;
-import com.lic.epgs.policyservicing.common.policylevelmergercontroller.model.PolicyServiceNotesDto;
-import com.lic.epgs.policyservicing.common.policylevelmergercontroller.model.PolicyServiceNotesTempEntity;
+import com.lic.epgs.policyservicing.common.policylevelmergercontroller.model.PolicyLevelMergerTempEntity;
 
 @Repository
-public interface PolicyLevelMergerControllerRepository extends JpaRepository<PolicyServiceNotesTempEntity, Integer> {
-	
-	// Method to save the PolicyServiceNotesDto object to the database as PolicyServiceNotesTempEntity with isActive set to true.
-	public PolicyServiceNotesTempEntity saveNotedPolicyMeger(PolicyServiceNotesDto policyServiceNotesDto);
-	
-	// Method to validate that the mergeId is not null or equal to 0.
-	public PolicyLevelMergerApiResponse validateMergeId(int mergeId);
-	
-	// Method to check if there exists a policy level merger with the given mergeId and isActive true.
-	public PolicyLevelMergerApiResponse checkMergeIdExists(int mergeId);
+public interface PolicyLevelMergerControllerRepository extends JpaRepository<PolicyLevelMergerTempEntity, Long> {
+
+	PolicyLevelMergerApiResponse getPolicyMergebyMergeId(Long mergeId);
+
 }

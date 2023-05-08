dc9e71cdc4ce079176a3836250189890c0694053
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-08 18:25:29 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelmergercontroller/repository/PolicyLevelMergerControllerRepository.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelmergercontroller/repository/PolicyLevelMergerControllerRepository.java
@@ -1,13 +1,18 @@
-@Repository
-public interface PolicyLevelMergerControllerRepository extends JpaRepository<PolicyServiceDocumentDto, Long> {
+package com.lic.epgs.policyservicing.common.policylevelmergercontroller.repository;
 
-    PolicyServiceDocumentDto save(PolicyServiceDocumentDto policyServiceDocumentDto);
-    
-    boolean existsByDocumentName(String documentName);
-    
-    @Transactional
-    @Modifying
-    @Query("UPDATE PolicyServiceDocumentDto SET isActive = true WHERE documentName = :documentName")
-    void setActive(@Param("documentName") String documentName);
+import java.util.List;
+import com.lic.epgs.policyservicing.common.policylevelmergercontroller.model.PolicyLevelMergerApiResponse;
+import com.lic.epgs.policyservicing.common.policylevelmergercontroller.model.PolicyServiceNotesDto;
+import org.springframework.data.repository.Repository;
+
+public interface PolicyLevelMergerControllerRepository extends Repository<PolicyServiceNotesTempRepository, Long> {
+
+  /**
+   * This method retrieves the list of notes associated with a specific merge ID.
+   * 
+   * @param mergeId the merge ID of the notes to be retrieved.
+   * @return a PolicyLevelMergerApiResponse object with a list of PolicyServiceNotesDto objects containing the notes, as well as a transaction status and message.
+   */
+  PolicyLevelMergerApiResponse getNoteList(long mergeId);
 
 }

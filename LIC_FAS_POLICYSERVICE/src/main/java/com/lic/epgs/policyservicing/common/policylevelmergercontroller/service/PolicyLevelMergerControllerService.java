9fd1f7bbcdb66e3b5466bbea9d59d28534eb7c88
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-08 18:30:19 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelmergercontroller/service/PolicyLevelMergerControllerService.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelmergercontroller/service/PolicyLevelMergerControllerService.java
@@ -1,18 +1,27 @@
+package com.lic.epgs.policyservicing.common.policylevelmergercontroller.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lic.epgs.policyservicing.common.policylevelmergercontroller.model.PolicyLevelMergerApiResponse;
+import com.lic.epgs.policyservicing.common.policylevelmergercontroller.model.PolicyServiceNotesDto;
+import com.lic.epgs.policyservicing.common.policylevelmergercontroller.repository.PolicyLevelMergerControllerRepository;
+
 @Service
 public class PolicyLevelMergerControllerService {
 
-    @Autowired
-    private PolicyLevelMergerControllerRepository policyLevelMergerControllerRepository;
+  @Autowired
+  private PolicyLevelMergerControllerRepository policyLevelMergerControllerRepository;
 
-    public PolicyServiceDocumentDto save(PolicyServiceDocumentDto policyServiceDocumentDto) {
-        return policyLevelMergerControllerRepository.save(policyServiceDocumentDto);
-    }
+  public PolicyLevelMergerApiResponse getNoteList(long mergeId) {
+    List<PolicyServiceNotesDto> notes = policyLevelMergerControllerRepository.getNoteList(mergeId);
+    PolicyLevelMergerApiResponse response = new PolicyLevelMergerApiResponse();
+    response.setNotes(notes);
+    response.setStatus("SUCCESS");
+    response.setMessage("Notes retrieved successfully.");
+    return response;
+  }
 
-    public boolean existsByDocumentName(String documentName) {
-        return policyLevelMergerControllerRepository.existsByDocumentName(documentName);
-    }
-
-    public void setActive(String documentName) {
-        policyLevelMergerControllerRepository.setActive(documentName);
-    }
 }

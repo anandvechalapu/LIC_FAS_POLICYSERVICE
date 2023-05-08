400878dd5eb0fdeebe4d02f1fcfa20952c1a3b6f
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-08 18:27:26 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelmergercontroller/model/PolicyLevelMergerApiResponse.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelmergercontroller/model/PolicyLevelMergerApiResponse.java
@@ -1,58 +1,66 @@
 package com.lic.epgs.policyservicing.common.policylevelmergercontroller.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import java.util.List;
 
-@Entity
 public class PolicyLevelMergerApiResponse {
-    
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private Long documentId;
-    private Long mergeId;
-    private Boolean success;
-    private String message;
-    
-    public Long getId() {
-        return id;
-    }
- 
-    public void setId(Long id) {
-        this.id = id;
-    }
- 
-    public Long getDocumentId() {
-        return documentId;
-    }
- 
-    public void setDocumentId(Long documentId) {
-        this.documentId = documentId;
-    }
- 
-    public Long getMergeId() {
-        return mergeId;
-    }
- 
-    public void setMergeId(Long mergeId) {
-        this.mergeId = mergeId;
-    }
- 
-    public Boolean getSuccess() {
-        return success;
-    }
- 
-    public void setSuccess(Boolean success) {
-        this.success = success;
-    }
- 
-    public String getMessage() {
-        return message;
-    }
- 
-    public void setMessage(String message) {
-        this.message = message;
-    }
+
+  private List<PolicyServiceNotesDto> notes;
+  private String transactionStatus;
+  private String transactionMessage;
+
+  public List<PolicyServiceNotesDto> getNotes() {
+    return notes;
+  }
+
+  public void setNotes(List<PolicyServiceNotesDto> notes) {
+    this.notes = notes;
+  }
+
+  public String getTransactionStatus() {
+    return transactionStatus;
+  }
+
+  public void setTransactionStatus(String transactionStatus) {
+    this.transactionStatus = transactionStatus;
+  }
+
+  public String getTransactionMessage() {
+    return transactionMessage;
+  }
+
+  public void setTransactionMessage(String transactionMessage) {
+    this.transactionMessage = transactionMessage;
+  }
+}
+
+package com.lic.epgs.policyservicing.common.policylevelmergercontroller.model;
+
+public class PolicyServiceNotesDto {
+  private Long noteId;
+  private Long mergeId;
+  private String noteText;
+
+  public Long getNoteId() {
+    return noteId;
+  }
+
+  public void setNoteId(Long noteId) {
+    this.noteId = noteId;
+  }
+
+  public Long getMergeId() {
+    return mergeId;
+  }
+
+  public void setMergeId(Long mergeId) {
+    this.mergeId = mergeId;
+  }
+
+  public String getNoteText() {
+    return noteText;
+  }
+
+  public void setNoteText(String noteText) {
+    this.noteText = noteText;
+  }
 }

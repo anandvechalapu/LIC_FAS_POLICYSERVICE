3bc38e9856c54935d1b0a35b7a339b2850b8b032
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-08 18:16:10 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelmergercontroller/repository/PolicyLevelMergerControllerRepository.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelmergercontroller/repository/PolicyLevelMergerControllerRepository.java
@@ -1,15 +1,13 @@
 @Repository
 public interface PolicyLevelMergerControllerRepository extends JpaRepository<PolicyServiceDocumentDto, Long> {
 
-	Optional<PolicyServiceDocumentDto> findByDocumentId(Long documentId);
-	
-	@Modifying
-	@Query("update PolicyServiceDocumentDto p set p.isActive = :isActive where p.documentId = :documentId")
-	void updateIsActive(@Param("documentId") Long documentId, @Param("isActive") Boolean isActive);
-	
-	@Transactional
-	@Modifying
-	@Query("delete from PolicyServiceDocumentDto p where p.documentId = :documentId")
-	void deleteByDocumentId(@Param("documentId") Long documentId);
+    PolicyServiceDocumentDto save(PolicyServiceDocumentDto policyServiceDocumentDto);
+    
+    boolean existsByDocumentName(String documentName);
+    
+    @Transactional
+    @Modifying
+    @Query("UPDATE PolicyServiceDocumentDto SET isActive = true WHERE documentName = :documentName")
+    void setActive(@Param("documentName") String documentName);
 
 }

a15f649757d2b92ec6f15a087f7b7ecf4ce8694f
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-08 13:20:12 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelconversioncontroller/service/PolicyLevelConversionControllerService.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policylevelconversioncontroller/service/PolicyLevelConversionControllerService.java
@@ -1,18 +1,20 @@
 package com.lic.epgs.policyservicing.common.policylevelconversioncontroller.service;
 
-import com.lic.epgs.policyservicing.common.policylevelconversioncontroller.dto.CommonResponseDto;
-import com.lic.epgs.policyservicing.common.policylevelconversioncontroller.dto.PolicyLevelConversionDto;
+import com.lic.epgs.policyservicing.common.policylevelconversioncontroller.model.Document;
 import com.lic.epgs.policyservicing.common.policylevelconversioncontroller.repository.PolicyLevelConversionControllerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PolicyLevelConversionControllerService {
 
     @Autowired
-    private PolicyLevelConversionControllerRepository policyLevelConversionControllerRepository;
+    PolicyLevelConversionControllerRepository policyLevelConversionControllerRepository;
 
-    public CommonResponseDto getPolicyConversionDetailsByPolicyNo(String prevPolicyNo, String role) {
-        return policyLevelConversionControllerRepository.getPolicyConversionDetailsByPolicyNo(prevPolicyNo, role);
+    public List<Document> getDocumentList(Long conversionId){
+        return policyLevelConversionControllerRepository.getDocumentList(conversionId);
     }
+
 }

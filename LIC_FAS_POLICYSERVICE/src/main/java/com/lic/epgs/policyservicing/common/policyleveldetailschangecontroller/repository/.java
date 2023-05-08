5d56619c5d6a60f9bfe16ef4251606faa7395795
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-08 15:11:10 +0530

    Updated

--- LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policyleveldetailschangecontroller/repository/.java
+++ LIC_FAS_POLICYSERVICE/src/main/java/com/lic/epgs/policyservicing/common/policyleveldetailschangecontroller/repository/.java
@@ -1,53 +1,13 @@
 package com.lic.epgs.policyservicing.common.policyleveldetailschangecontroller.repository;
 
-import java.util.Date;
-import java.util.Optional;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.lic.epgs.policyservicing.common.policyleveldetailschangecontroller.model.PolicyDtlsResponseDto;
-import com.lic.epgs.policyservicing.common.policyleveldetailschangecontroller.model.PolicyDtlsChangeTempEntity;
-import com.lic.epgs.policyservicing.common.policyleveldetailschangecontroller.service.PolicyDetailsChangeService;
+import com.lic.epgs.policyservicing.common.policyleveldetailschangecontroller.entity.PolicyServiceNotesTempEntity;
 
 @Repository
-public class PolicyLevelDetailsChangeControllerRepository {
-	
-	private static final Logger logger = LoggerFactory.getLogger(PolicyLevelDetailsChangeControllerRepository.class);
-	
-	@Autowired
-	private PolicyDetailsChangeService policyDetailsChangeService;
-	
-	public PolicyDtlsResponseDto sendToMaker(Long policyDtlsId) {
-		logger.info("sendToMaker method started with policyDtlsId {}", policyDtlsId);
-		
-		Optional<PolicyDtlsChangeTempEntity> policyDtlsChangeTempEntity = policyDetailsChangeService.findById(policyDtlsId);
-		if(policyDtlsChangeTempEntity.isPresent()) {
-			policyDtlsChangeTempEntity.get().setStatus("CHECKER_NO");
-			policyDtlsChangeTempEntity.get().setModifiedOn(new Date());
-			
-			PolicyDtlsChangeTempEntity updatedPolicyDtlsChangeTempEntity = policyDetailsChangeService.save(policyDtlsChangeTempEntity.get());
-			
-			PolicyDtlsResponseDto response = new PolicyDtlsResponseDto();
-			response.setPolicyDtlsChangeTempEntity(updatedPolicyDtlsChangeTempEntity);
-			response.setStatus("SUCCESS");
-			response.setMessage("UPDATEMESSAGE");
-			
-			logger.info("sendToMaker method completed successfully");
-			
-			return response;
-		} else {
-			PolicyDtlsResponseDto response = new PolicyDtlsResponseDto();
-			response.setStatus("FAIL");
-			response.setMessage("ERROR");
-			
-			logger.info("sendToMaker method completed with failure");
-			
-			return response;
-		}
-		
-	}
-
+public interface PolicyServiceNotesTempRepo extends JpaRepository<PolicyServiceNotesTempEntity, Long> {
+  
+  public PolicyServiceNotesTempEntity findByPolicyIdAndServiceId(Long policyId, Long serviceId);
+  
 }
